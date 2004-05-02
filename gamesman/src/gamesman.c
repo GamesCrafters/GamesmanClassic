2f9f4b747145a7d2ef9cd9e850db861d2ad2b8a4
nizebulous <nizebulous@a668be5d-ef11-cb4c-8bfd-e08cbb24f2ba>
2004-05-02 08:02:40 +0000

    Fixed Scott's menu booboo.  -Damian Hites
    git-svn-id: https://gamescrafters.svn.sourceforge.net/svnroot/gamescrafters/GamesmanClassic/trunk@1162 a668be5d-ef11-cb4c-8bfd-e08cbb24f2ba

--- gamesman/src/gamesman.c
+++ gamesman/src/gamesman.c
@@ -376,7 +376,7 @@ void MenusBeforeEvaluation()
   if(kGameSpecificMenu)
     printf("\tg)\t(G)ame-specific options for %s\n",kGameName);
   printf("\t2)\tToggle (2)-bit solving (currently %s)\n", gTwoBits ? "ON" : "OFF");
-  printf("\t2)\tToggle (L)ow Mem solving (currently %s)\n", kZeroMemSolver ? "ON" : "OFF");
+  printf("\tl)\tToggle (L)ow Mem solving (currently %s)\n", kZeroMemSolver ? "ON" : "OFF");
 }
 
 void MenusEvaluated()
