efc2d1344a4b1c5ec1547b51a0c4edac4174c40c
reyjeffc <reyjeffc@a668be5d-ef11-cb4c-8bfd-e08cbb24f2ba>
2004-09-24 17:53:28 +0000

    Added Tcl Gold template files and stuff
    Jeffrey
    
    
    git-svn-id: https://gamescrafters.svn.sourceforge.net/svnroot/gamescrafters/GamesmanClassic/trunk@1276 a668be5d-ef11-cb4c-8bfd-e08cbb24f2ba

---
+++ gamesman/tcl/template/mblank.tcl
@@ -0,0 +1,191 @@
+
+####################################################
+# this is a template for tcl module creation
+#
+# created by Alex Kozlowski and Peterson Trethewey
+####################################################
+
+
+# GS_InitGameSpecific sets characteristics of the game that
+# are inherent to the game, unalterable.  You can use this fucntion
+# to initialize data structures, but not to present any graphics.
+# It is called FIRST, ONCE, only when the player
+# starts playing your game, and before the player hits "New Game"
+# At the very least, you must set the global variables kGameName
+# and gInitialPosition in this function.
+
+proc GS_InitGameSpecific {} {
+    
+    ### Set the name of the game
+    
+    global kGameName
+    set kGameName ""
+    
+    ### Set the initial position of the board
+
+    global gInitialPosition gPosition
+    set gInitialPosition 0
+    set gPosition $gInitialPosition
+}
+
+
+
+# GS_NameOfPieces should return a list of 2 strings that represent
+# your names for the "pieces".  If your game is some pathalogical game
+# with no concept of a "piece", give a name to the game's sides.
+# if the game is tic tac toe, this might be a single line: return [list x o]
+# This function is called FIRST, ONCE, only when the player
+# starts playing the game, and before he hits "New Game"
+
+proc GS_NameOfPieces {} {
+
+    ### FILL THIS IN
+
+}
+
+
+# GS_Initialize is where you can start drawing graphics.  
+# Its argument, c, is a canvas.  Please draw only in this canvas.
+# You could put an opening animation in this function that introduces the game
+# or just draw an empty board.
+# This function is called ONCE after GS_InitGameSpecific, and before the
+# player hits "New Game"
+
+proc GS_Initialize { c } {
+
+    # you may want to start by setting the size of the canvas; this line isn't cecessary
+    $c configure -width 300 -height 300
+
+} 
+
+
+
+# GS_DrawPosition this draws the board in an arbitrary position.
+# It's arguments are a canvas, c, where you should draw and the
+# (hashed) position.  For example, if your game is a rearranger,
+# here is where you would draw pieces on the board in their correct positions.
+# Imagine that this function is called when the player
+# loads a saved game, and you must quickly return the board to its saved
+# state.  It probably shouldn't animate, but it can if you want.
+
+# BY THE WAY: Before you go any further, I recommend writing a tcl function that 
+# UNhashes You'll thank yourself later.
+# Don't bother writing tcl that hashes, that's never necessary.
+
+
+proc GS_DrawPosition { c position } {
+    
+    # BTW too: don't make any assumptions about the state of the board.
+
+}
+
+
+# GS_NewGame should start playing the game. "let's play"  :)
+# It's arguments are a canvas, c, where you should draw and
+# the hashed starting position of the game.
+# This is called just when the player hits "New Game"
+# and before any moves are made.
+
+proc GS_NewGame { c position } {
+    # The default behavior of this funciton is just to draw the position
+    # but if you want you can add a special behaivior here like an animation
+    GS_DrawPosition $c $position
+}
+
+
+# GS_WhoseMove takes a position and returns whose move it is.
+# Your return value should be one of the items in the list returned
+# by GS_NameOfPieces.
+# This function is called just before every move.
+
+proc GS_WhoseMove { position } {
+    return ""    
+}
+
+
+# GS_HandleMove draws (animates) a move being made.
+# The user or the computer has just made a move, animate it or draw it
+# or whatever.  Draw the piece moving if your game is a rearranger, or
+# the piece appearing if it's a "dart board"
+
+# By the way, if you animate, a function that will be useful for you is
+# update idletasks.  You can call this to force the canvas to update if
+# you make changes before tcl enters the event loop again.
+
+proc GS_HandleMove { c oldPosition theMove newPosition } {
+    
+}
+
+
+# GS_ShowMoves draws the move indicator (be it an arrow or a dot, whatever the
+# player clicks to make the move)  It is also the function that handles coloring
+# of the moves according to value. It is called by gamesman just before the player
+# is prompted for a move.
+
+# Arguments:
+# c = the canvas to draw in as usual
+# moveType = a string which is either value, moves or best according to which radio button is down
+# position = the current hashed position
+# moveList = a list of available moves to the player.  These moves are represented as numbers (same as in C)
+
+# The code snippet herein may be helpful but is not necessary to do it that way.
+# We provide a procedure called MoveTypeToColor that takes in moveType and
+# returns the correct color.
+
+proc GS_ShowMoves { c moveType position moveList } {
+    set color MoveTypeToColor moveType
+}
+
+
+
+# GS_HideMoves erases the moves drawn by GS_ShowMoves.  It's arguments are the same as GS_ShowMoves.
+# You might not use all the arguments, and that's okay.
+
+proc GS_HideMoves { c moveType position moveList} {
+    
+}
+
+
+
+# GS_HandleUndo draws a move undoing itself sortof.
+# the names of the arguments explain themsleves but just to clarify...
+# The game was in position A, a player makes move M bringing the game to position B
+# then an undo is called
+# currentPosition is the B
+# theMoveToUndo is the M
+# positionAfterUndo is the A
+
+# By default this function just calls GS_DrawPosition, but you certainly don't need to keep that.
+
+proc GS_HandleUndo { c currentPosition theMoveToUndo positionAfterUndo} {
+    GS_DrawPosition c positionAfterUndo
+}
+
+
+
+
+
+# GS_GetGameSpecificOptions is not quite ready, don't worry about it .
+proc GS_GetGameSpecificOptions { } {
+}
+
+
+
+
+# GS_GameOver is called the moment the game is finished ( won, lost or tied)
+# you could use this function to draw the line striking out the winning row in tic tac toe for instance
+# or you could congratulate the winner or do nothing if you want.
+
+proc GS_GameOver { c position gameValue nameOfWinningPiece nameOfWinner } {
+}
+
+
+# GS_UndoGameOver is called then the player hits undo after the game is finished.
+# this is provided so that you may undo the drawing you did in GS_GameOver if you drew something.
+# for instance, if you drew a line crossing out the winning row in tic tac toe, this is where you sould delete the line.
+
+# note: GS_HandleUndo is called regardless of whether the move undoes the end of the game, so IF you choose to do nothing in
+# GS_GameOver, you needn't do anything here either.
+
+proc GS_UndoGameOver { c position } {
+}
