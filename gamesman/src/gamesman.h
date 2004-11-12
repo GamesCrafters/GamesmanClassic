4681f5726c1d8cdc56e4829acaa7810b88d3ae92
boundsyco <boundsyco@a668be5d-ef11-cb4c-8bfd-e08cbb24f2ba>
2004-11-12 22:11:11 +0000

    Reverted POSITIONS to unsigned long. ~SL
    git-svn-id: https://gamescrafters.svn.sourceforge.net/svnroot/gamescrafters/GamesmanClassic/trunk@1438 a668be5d-ef11-cb4c-8bfd-e08cbb24f2ba

--- gamesman/src/gamesman.h
+++ gamesman/src/gamesman.h
@@ -1,4 +1,4 @@
-#ifndef __GAMESMAN_H__
+SP#ifndef __GAMESMAN_H__
 #define __GAMESMAN_H__
 
 #ifndef NO_GRAPHICS
@@ -58,7 +58,7 @@ typedef int ClientData;
 #define  TRUE           1
 #endif
 
-typedef unsigned long long POSITION;      /* This used to be determined by mTempTypedef.h */
+typedef unsigned long POSITION;      /* This used to be determined by mTempTypedef.h */
 typedef int MOVE;           /* but I decided to hardcode it for now, so as  */
 typedef int MOVES;          /* to make it a leeeetle bit easier and cleaner */
 #define POSITION_FORMAT "%lld"
