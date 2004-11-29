bcbb7765dd737bef41633cc88de3996c9992ee26
liao_r <liao_r@a668be5d-ef11-cb4c-8bfd-e08cbb24f2ba>
2004-11-29 04:16:25 +0000

    Contains changes necessary for ANALYSIS typedef.
    git-svn-id: https://gamescrafters.svn.sourceforge.net/svnroot/gamescrafters/GamesmanClassic/trunk@1459 a668be5d-ef11-cb4c-8bfd-e08cbb24f2ba

--- gamesman/src/gamesman.h
+++ gamesman/src/gamesman.h
@@ -146,6 +146,25 @@ typedef struct valuemoves_item
 }
 VALUE_MOVES;
 
+/* Analysis Data */
+
+typedef struct analysis_info
+{
+	int HashEfficiency;
+	float AverageFanout;
+	POSITION NumberOfPositions;
+	POSITION TotalPositions;
+	unsigned int TotalMoves;
+	unsigned long WinCount;
+	unsigned long LoseCount;
+	unsigned long TieCount;
+	unsigned long UnknownCount;
+	unsigned long PrimitiveWins;
+	unsigned long PrimitiveLoses;
+	unsigned long PrimitiveTies;
+	unsigned int TimeToSolve;
+} ANALYSIS;
+
 typedef signed char MEX;           /* The max mex value is 31 */
 typedef unsigned long int MEXCALC; /* ...since we use a long int here */
 
