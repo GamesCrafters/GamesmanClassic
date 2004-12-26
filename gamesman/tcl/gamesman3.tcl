77f4097eb0249cafb346b5c41a62a19f21f7fa3c
scarr2508 <scarr2508@a668be5d-ef11-cb4c-8bfd-e08cbb24f2ba>
2004-12-26 21:02:21 +0000

    made oxyskin the default skin
    git-svn-id: https://gamescrafters.svn.sourceforge.net/svnroot/gamescrafters/GamesmanClassic/trunk@1504 a668be5d-ef11-cb4c-8bfd-e08cbb24f2ba

--- gamesman/tcl/gamesman3.tcl
+++ gamesman/tcl/gamesman3.tcl
@@ -728,7 +728,7 @@ proc main {kRootDir} {
     # Initialize generic top-level window
     source "$kRootDir/../tcl/InitWindow.tcl"
     InitGlobals
-    InitWindow $kRootDir LilySkin ppm
+    InitWindow $kRootDir OxySkin ppm
 
     # Initialize game-specific globals and frame
     GS_InitGameSpecific
