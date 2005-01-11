8e4f017304809123ff0d0f4fb7e72ccfed5226f9
jjjordan <jjjordan@a668be5d-ef11-cb4c-8bfd-e08cbb24f2ba>
2005-01-11 21:31:37 +0000

    Moved the bottom-up solver into the gamesman core and removed any files (besides gamesman.c) remaining in src/ that are now in the core. -JJ
    git-svn-id: https://gamescrafters.svn.sourceforge.net/svnroot/gamescrafters/GamesmanClassic/trunk@1536 a668be5d-ef11-cb4c-8bfd-e08cbb24f2ba

--- gamesman/src/loopygasolver.h
+++
@@ -1,8 +0,0 @@
-#ifndef __LOOPYGASOLVER_H__
-#define __LOOPYGASOLVER_H__
-
-#include "gamesman.h"
-
-VALUE lgas_DetermineValue(POSITION position);
-
-#endif
--- gamesman/src/loopyupsolver.h
+++
@@ -1,9 +0,0 @@
-#ifndef __LOOPYUPSOLVER_H__
-#define __LOOPYUPSOLVER_H__
-
-#include "gamesman.h"
-
-VALUE loopyup_DetermineValue(POSITION);
-POSITIONLIST* GenerateParents(POSITION);
-
-#endif
