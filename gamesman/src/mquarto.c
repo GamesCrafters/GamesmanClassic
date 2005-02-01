d91806d4c63529248189d16978a82add7a630d2a
yanpeichen <yanpeichen@a668be5d-ef11-cb4c-8bfd-e08cbb24f2ba>
2005-02-01 05:24:14 +0000

    *** empty log message ***
    git-svn-id: https://gamescrafters.svn.sourceforge.net/svnroot/gamescrafters/GamesmanClassic/trunk@1557 a668be5d-ef11-cb4c-8bfd-e08cbb24f2ba

---
+++ gamesman/src/mquarto.c
@@ -0,0 +1,592 @@
+// $id$
+// $log$
+
+/*
+ * The above lines will include the name and log of the last person
+ * to commit this file to CVS
+ */
+
+/************************************************************************
+**
+** NAME:        mquarto.c
+**
+** DESCRIPTION: Quarto
+**
+** AUTHORS:      Yanpei CHEN  <ychen@berkeley.edu>
+**               Amy Sueh     <amysueh@berkeley.edu>
+**               Mario TANEV  <mtanev@berkeley.edu>
+**
+** DATE:        Began Jan 2005; 
+**
+** UPDATE HIST: RECORD CHANGES YOU HAVE MADE SO THAT TEAMMATES KNOW
+**              30 Jan 2005 Yanpei: PrintPosition() coded
+**
+**************************************************************************/
+
+/*************************************************************************
+**
+** Everything below here must be in every game file
+**
+**************************************************************************/
+
+#include <stdio.h>
+#include "gamesman.h"
+#include <stdlib.h>
+#include <unistd.h>
+#include <limits.h>
+
+
+/*************************************************************************
+**
+** Game-specific constants
+**
+**************************************************************************/
+
+STRING   kGameName            = "QUARTO"; /* The name of your game */
+STRING   kAuthorName          = "Yanpei CHEN, Amy SUEH, Mario TANEV"; /* Your name(s) */
+STRING   kDBName              = ""; /* The name to store the database under */
+
+BOOLEAN  kPartizan            = FALSE ; /* A partizan game is a game where each player has different moves from the same board (chess - different pieces) */
+BOOLEAN  kGameSpecificMenu    = FALSE ; /* TRUE if there is a game specific menu. FALSE if there is not one. */
+BOOLEAN  kTieIsPossible       = FALSE ; /* TRUE if a tie is possible. FALSE if it is impossible.*/
+BOOLEAN  kLoopy               = FALSE ; /* TRUE if the game tree will have cycles (a rearranger style game). FALSE if it does not.*/
+
+BOOLEAN  kDebugMenu           = TRUE ; /* TRUE only when debugging. FALSE when on release. */
+BOOLEAN  kDebugDetermineValue = TRUE ; /* TRUE only when debugging. FALSE when on release. */
+
+POSITION gNumberOfPositions   =  0; /* The number of total possible positions | If you are using our hash, this is given by the hash_init() function*/
+POSITION gInitialPosition     =  0; /* The initial hashed position for your starting board */
+POSITION kBadPosition         = -1; /* A position that will never be used */
+
+/* 
+ * Help strings that are pretty self-explanatory
+ * Strings than span more than one line should have backslashes (\) at the end of the line.
+ */
+
+STRING kHelpGraphicInterface =
+"Not written yet";
+
+STRING   kHelpTextInterface    =
+""; 
+
+STRING   kHelpOnYourTurn =
+"";
+
+STRING   kHelpStandardObjective =
+"";
+
+STRING   kHelpReverseObjective =
+"";
+
+STRING   kHelpTieOccursWhen =
+"A tie occurs when ...";
+
+STRING   kHelpExample =
+"";
+
+
+/*************************************************************************
+**
+** #defines and structs
+**
+**************************************************************************/
+
+#define GAMEDIMENSION 4
+
+#define BOARDSIZE GAMEDIMENSION^2
+#define PIECESTATES BOARDSIZE+2
+#define NUMPIECES 2^GAMEDIMENSION
+
+typedef struct board_item {
+
+  int[BOARDSIZE] squares;
+  BOOLEAN usersTurn;
+
+} QTBOARD;
+
+
+/*************************************************************************
+**
+** Global Variables
+**
+*************************************************************************/
+
+
+/*************************************************************************
+**
+** Function Prototypes
+**
+*************************************************************************/
+
+/* External */
+extern GENERIC_PTR	SafeMalloc ();
+extern void		SafeFree ();
+
+
+/*************************************************************************
+**
+** Global Database Declaration
+**
+**************************************************************************/
+
+extern VALUE     *gDatabase;
+
+
+/************************************************************************
+**
+** NAME:        InitializeGame
+**
+** DESCRIPTION: Prepares the game for execution.
+**              Initializes required variables.
+**              Sets up gDatabase (if necessary).
+** 
+************************************************************************/
+
+void InitializeGame ()
+{
+    
+}
+
+
+/************************************************************************
+**
+** NAME:        GenerateMoves
+**
+** DESCRIPTION: Creates a linked list of every move that can be reached
+**              from this position. Returns a pointer to the head of the
+**              linked list.
+** 
+** INPUTS:      POSITION position : Current position for move
+**                                  generation.
+**
+** OUTPUTS:     (MOVELIST *)      : A pointer to the first item of
+**                                  the linked list of generated moves
+**
+** CALLS:       MOVELIST *CreateMovelistNode();
+**
+************************************************************************/
+
+MOVELIST *GenerateMoves (POSITION position)
+{
+    MOVELIST *moves = NULL;
+    
+    /* Use CreateMovelistNode(move, next) to 'cons' together a linked list */
+    
+    return moves;
+}
+
+
+/************************************************************************
+**
+** NAME:        DoMove
+**
+** DESCRIPTION: Applies the move to the position.
+** 
+** INPUTS:      POSITION position : The old position
+**              MOVE     move     : The move to apply to the position
+**
+** OUTPUTS:     (POSITION)        : The position that results from move
+**
+** CALLS:       Some Board Hash Function
+**              Some Board Unhash Function
+**
+*************************************************************************/
+
+POSITION DoMove (POSITION position, MOVE move)
+{
+    return 0;
+}
+
+
+/************************************************************************
+**
+** NAME:        Primitive
+**
+** DESCRIPTION: Returns the value of a position if it fulfills certain
+**              'primitive' constraints.
+**
+**              Example: Tic-tac-toe - Last piece already placed
+**
+**              Case                                  Return Value
+**              *********************************************************
+**              Current player sees three in a row    lose
+**              Entire board filled                   tie
+**              All other cases                       undecided
+** 
+** INPUTS:      POSITION position : The position to inspect.
+**
+** OUTPUTS:     (VALUE)           : one of
+**                                  (win, lose, tie, undecided)
+**
+** CALLS:       None              
+**
+************************************************************************/
+
+VALUE Primitive (POSITION position)
+{
+    return undecided;
+}
+
+
+/************************************************************************
+**
+** NAME:        PrintPosition
+**
+** DESCRIPTION: Prints the position in a pretty format, including the
+**              prediction of the game's outcome.
+** 
+** INPUTS:      POSITION position    : The position to pretty print.
+**              STRING   playersName : The name of the player.
+**              BOOLEAN  usersTurn   : TRUE <==> it's a user's turn.
+**
+** CALLS:       Unhash()
+**              GetPrediction()      : Returns the prediction of the game
+**
+************************************************************************/
+
+/************************************************************************
+**
+**  Position encoding: 
+**
+**  Encode each piece as having one of 18 states, 0-15 represent one of 16
+**  squares on the board, 16 represent awaiting placement as next piece, 17 
+**  represent piece unused. 
+**
+**              BOARD          NEXT PIECE
+**
+**  Piece    xxx0xxx1xxxx3xxx  5    
+**           ----------------
+**  Square   0123456789abcdef
+**
+**  For the above position, the pieces' states are: 
+**
+**    Piece   State
+**    0       3     (on square 3)
+**    1       7     (on square 7)
+**    2       17    (unused)
+**    3       c     (on square c)
+**    4       17    (unused)
+**    5       16    (awaiting placement)
+**    6       17    (unused)
+**    7       17    (unused)
+**    8       17    (unused)
+**    ...     ...   ...
+**    f       17    (unused)
+**
+**  Each position is converted to an int by
+**
+**    position = sum(ith piece * number of states + ith state),
+**
+**  leading to total of 16*18 = 288 positions for game dimension of 4. 
+**
+**
+************************************************************************/
+
+void PrintPosition (POSITION position, STRING playersName, BOOLEAN usersTurn)
+{
+  int currentPiece, currentSquare;
+
+  QTBOARD newBoard = SafeMalloc(sizeof(QTBOARD));
+
+  for (currentSquare=0; currentSquare<BOARDSIZE; currentSquare++) {
+    newBoard->squares[currentSquare] = 0;
+  }
+  newBoard->usersTurn = usersTurn;
+
+  for (currentPiece=0; currentPiece<NUMPIECES; currentPiece++) {
+    currentSquare = position - PIECESTATES * currentPiece;
+    if (1<=currentSquare && currentSquare<=BOARDSIZE) {
+      newBoard->squares[currentSquare] = currentPiece;
+    }
+  }
+
+  for (currentSquare=0; currentSquare<BOARDSIZE; currentSquare++) {
+    printf("%s    ", binStr((unsigned int) newBoard->squares[currentSquare]));
+    if (currentSquare % GAMEDIMENSION == 0) {
+      printf("\n\n");
+    }
+  }
+
+  printf("\n");
+
+}
+
+
+/************************************************************************
+**
+** NAME:        PrintComputersMove
+**
+** DESCRIPTION: Nicely formats the computers move.
+** 
+** INPUTS:      MOVE    computersMove : The computer's move. 
+**              STRING  computersName : The computer's name. 
+**
+************************************************************************/
+
+void PrintComputersMove (MOVE computersMove, STRING computersName)
+{
+    
+}
+
+
+/************************************************************************
+**
+** NAME:        PrintMove
+**
+** DESCRIPTION: Prints the move in a nice format.
+** 
+** INPUTS:      MOVE move         : The move to print. 
+**
+************************************************************************/
+
+void PrintMove (MOVE move)
+{
+    
+}
+
+
+/************************************************************************
+**
+** NAME:        GetAndPrintPlayersMove
+**
+** DESCRIPTION: Finds out if the player wishes to undo, abort, or use
+**              some other gamesman option. The gamesman core does
+**              most of the work here. 
+**
+** INPUTS:      POSITION position    : Current position
+**              MOVE     *move       : The move to fill with user's move. 
+**              STRING   playersName : Current Player's Name
+**
+** OUTPUTS:     USERINPUT          : One of
+**                                   (Undo, Abort, Continue)
+**
+** CALLS:       USERINPUT HandleDefaultTextInput(POSITION, MOVE*, STRING)
+**                                 : Gamesman Core Input Handling
+**
+************************************************************************/
+
+USERINPUT GetAndPrintPlayersMove (POSITION position, MOVE *move, STRING playersName)
+{
+    USERINPUT input;
+    USERINPUT HandleDefaultTextInput();
+    
+    for (;;) {
+        /***********************************************************
+         * CHANGE THE LINE BELOW TO MATCH YOUR MOVE FORMAT
+         ***********************************************************/
+	printf("%8s's move [(undo)/(MOVE FORMAT)] : ", playersName);
+	
+	input = HandleDefaultTextInput(position, move, playersName);
+	
+	if (input != Continue)
+		return input;
+    }
+
+    /* NOTREACHED */
+    return Continue;
+}
+
+
+/************************************************************************
+**
+** NAME:        ValidTextInput
+**
+** DESCRIPTION: Rudimentary check to check if input is in the move form
+**              you are expecting. Does not check if it is a valid move.
+**              Only checks if it fits the move form.
+**
+**              Reserved Input Characters - DO NOT USE THESE ONE CHARACTER
+**                                          COMMANDS IN YOUR GAME
+**              ?, s, u, r, h, a, c, q
+**                                          However, something like a3
+**                                          is okay.
+** 
+**              Example: Tic-tac-toe Move Format : Integer from 1 to 9
+**                       Only integers between 1 to 9 are accepted
+**                       regardless of board position.
+**                       Moves will be checked by the core.
+**
+** INPUTS:      STRING input : The string input the user typed.
+**
+** OUTPUTS:     BOOLEAN      : TRUE if the input is a valid text input.
+**
+************************************************************************/
+
+BOOLEAN ValidTextInput (STRING input)
+{
+    return FALSE;
+}
+
+
+/************************************************************************
+**
+** NAME:        ConvertTextInputToMove
+**
+** DESCRIPTION: Converts the string input your internal move representation.
+**              Gamesman already checked the move with ValidTextInput
+**              and ValidMove.
+** 
+** INPUTS:      STRING input : The VALID string input from the user.
+**
+** OUTPUTS:     MOVE         : Move converted from user input.
+**
+************************************************************************/
+
+MOVE ConvertTextInputToMove (STRING input)
+{
+    return 0;
+}
+
+
+/************************************************************************
+**
+** NAME:        GameSpecificMenu
+**
+** DESCRIPTION: Prints, receives, and sets game-specific parameters.
+**
+**              Examples
+**              Board Size, Board Type
+**
+**              If kGameSpecificMenu == FALSE
+**                   Gamesman will not enable GameSpecificMenu
+**                   Gamesman will not call this function
+** 
+**              Resets gNumberOfPositions if necessary
+**
+************************************************************************/
+
+void GameSpecificMenu ()
+{
+    
+}
+
+
+/************************************************************************
+**
+** NAME:        SetTclCGameSpecificOptions
+**
+** DESCRIPTION: Set the C game-specific options (called from Tcl)
+**              Ignore if you don't care about Tcl for now.
+** 
+************************************************************************/
+
+void SetTclCGameSpecificOptions (int options[])
+{
+    
+}
+  
+  
+/************************************************************************
+**
+** NAME:        GetInitialPosition
+**
+** DESCRIPTION: Called when the user wishes to change the initial
+**              position. Asks the user for an initial position.
+**              Sets new user defined gInitialPosition and resets
+**              gNumberOfPositions if necessary
+** 
+** OUTPUTS:     POSITION : New Initial Position
+**
+************************************************************************/
+
+POSITION GetInitialPosition ()
+{
+    return 0;
+}
+
+
+/************************************************************************
+**
+** NAME:        NumberOfOptions
+**
+** DESCRIPTION: Calculates and returns the number of variants
+**              your game supports.
+**
+** OUTPUTS:     int : Number of Game Variants
+**
+************************************************************************/
+
+int NumberOfOptions ()
+{
+    return 0;
+}
+
+
+/************************************************************************
+**
+** NAME:        getOption
+**
+** DESCRIPTION: A hash function that returns a number corresponding
+**              to the current variant of the game.
+**              Each set of variants needs to have a different number.
+**
+** OUTPUTS:     int : the number representation of the options.
+**
+************************************************************************/
+
+int getOption ()
+{
+    return 0;
+}
+
+
+/************************************************************************
+**
+** NAME:        setOption
+**
+** DESCRIPTION: The corresponding unhash function for game variants.
+**              Unhashes option and sets the necessary variants.
+**
+** INPUT:       int option : the number representation of the options.
+**
+************************************************************************/
+
+void setOption (int option)
+{
+    
+}
+
+
+/************************************************************************
+**
+** NAME:        DebugMenu
+**
+** DESCRIPTION: Game Specific Debug Menu (Gamesman comes with a default
+**              debug menu). Menu used to debug internal problems.
+**
+**              If kDebugMenu == FALSE
+**                   Gamesman will not display a debug menu option
+**                   Gamesman will not call this function
+** 
+************************************************************************/
+
+void DebugMenu ()
+{
+    
+}
+
+
+/************************************************************************
+**
+** Everything specific to this module goes below these lines.
+**
+** Things you want down here:
+** Move Hasher
+** Move Unhasher
+** Any other function you deem necessary to help the ones above.
+** 
+************************************************************************/
+
+STRING binStr(unsigned int x) {
+
+  int i;
+  char* toReturn = SafeMalloc(GAMEDIMENSION*sizeof(char));
+
+  for (i=0; i<GAMEDIMENSION; i++) {
+    toReturn[GAMEDIMENSION-1-i] = (x % 2) + '0';
+    x = x/2;
+  }
+
+  return toReturn;
+   
+}
