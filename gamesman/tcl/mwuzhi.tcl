45d92e712d73e833655e5009f87d367e1b29b370
dfang85 <dfang85@a668be5d-ef11-cb4c-8bfd-e08cbb24f2ba>
2005-03-14 01:52:38 +0000

    Wrote all the main functions (GS_Initialize, GS_PrintPosition, GS_ShowMoves, GS_HandleMoves, GS_HideMoves)
    git-svn-id: https://gamescrafters.svn.sourceforge.net/svnroot/gamescrafters/GamesmanClassic/trunk@1677 a668be5d-ef11-cb4c-8bfd-e08cbb24f2ba

---
+++ gamesman/tcl/mwuzhi.tcl
@@ -0,0 +1,517 @@
+
+
+####################################################
+# this is a template for tcl module creation
+#
+# created by Alex Kozlowski and Peterson Trethewey
+# Updated Fall 2004 by Jeffrey Chiang, and others
+####################################################
+
+#############################################################################
+# GS_InitGameSpecific sets characteristics of the game that
+# are inherent to the game, unalterable.  You can use this fucntion
+# to initialize data structures, but not to present any graphics.
+# It is called FIRST, ONCE, only when the player
+# starts playing your game, and before the player hits "New Game"
+# At the very least, you must set the global variables kGameName
+# and gInitialPosition in this function.
+############################################################################
+proc GS_InitGameSpecific {} {
+    
+    ### Set the name of the game
+    
+    global kGameName
+    set kGameName "Wuzhi"
+    
+    ### Set the initial position of the board (default 0)
+
+    global gInitialPosition gPosition
+    set gInitialPosition 0
+    set gPosition $gInitialPosition
+
+    ### Set the strings to be used in the Edit Rules
+
+    global kStandardString kMisereString
+    set kStandardString "First player to capture all but one of their opponents pieces WINS"
+    set kMisereString "First player to capture all but one of their opponents pieces LOSES"
+
+    ### Set the strings to tell the user how to move and what the goal is.
+    ### If you have more options, you will need to edit this section
+
+    global gMisereGame
+    if {!$gMisereGame} {
+	SetToWinString "To Win: Capture all but one of your opponenet's pieces. Capturing takes place when you move your piece into a capture position. A capture position is where two of your pieces are adjacent to each other and an opponent's piece is adjacent to one of those pieces all along the same line. You can also win if your opponent has no moves left."
+    } else {
+	SetToWinString "To Win: The Person with one piece left or the person with no moves left wins."
+    }
+    SetToMoveString "To Move: click on the arrow of the piece that you want to move in the direction that you want to move."
+	    
+    # Authors Info. Change if desired
+    global kRootDir
+    global kCAuthors kTclAuthors kGifAuthors
+    set kCAuthors "Diana Fang and Dan Yan"
+    set kTclAuthors "Dan Yan and Diana Fang"
+    set kGifAuthors "$kRootDir/../bitmaps/DanGarcia-310x232.gif"
+}
+
+
+#############################################################################
+# GS_NameOfPieces should return a list of 2 strings that represent
+# your names for the "pieces".  If your game is some pathalogical game
+# with no concept of a "piece", give a name to the game's sides.
+# if the game is tic tac toe, this might be a single line: return [list x o]
+# This function is called FIRST, ONCE, only when the player
+# starts playing the game, and before he hits "New Game"
+#############################################################################
+proc GS_NameOfPieces {} {
+
+    return [list w b]
+
+}
+
+
+#############################################################################
+# GS_ColorOfPlayers should return a list of two strings, 
+# each representing the color of a player.
+# If a specific color appears uniquely on one player's pieces,
+# it might be a good choice for that player's color.
+# In impartial games, both players may share the same color.
+# If the game is tic tac toe, this might be the line 
+# return [list blue red]
+# If the game is nim, this might be the line
+# return [list green green]
+# This function is called FIRST, ONCE, only when the player
+# starts playing the game, and before he clicks "New Game"
+# The left player's color should be the first item in the list.
+# The right player's color should be second.
+#############################################################################
+proc GS_ColorOfPlayers {} {
+
+    return [list blue red]
+    
+}
+
+
+#############################################################################
+# GS_SetupRulesFrame sets up the rules frame;
+# Adds widgets to the rules frame that will allow the user to 
+# select the variant of this game to play. The options 
+# selected by the user should be stored in a set of global
+# variables.
+# This procedure must initialize the global variables to some
+# valid game variant.
+# The rules frame must include a standard/misere setting.
+# Args: rulesFrame (Frame) - The rules frame to which widgets
+# should be added
+# Modifies: the rules frame and its global variables
+# Returns: nothing
+#############################################################################
+proc GS_SetupRulesFrame { rulesFrame } {
+
+    set standardRule \
+	[list \
+	     "What would you like your winning condition to be:" \
+	     "Standard" \
+	     "Misere" \
+	    ]
+
+    # List of all rules, in some order
+    set ruleset [list $standardRule]
+
+    # Declare and initialize rule globals
+    global gMisereGame
+    set gMisereGame 0
+
+    # List of all rule globals, in same order as rule list
+    set ruleSettingGlobalNames [list "gMisereGame"]
+
+    global kLabelFont
+    set ruleNum 0
+    foreach rule $ruleset {
+	frame $rulesFrame.rule$ruleNum -borderwidth 2 -relief raised
+	pack $rulesFrame.rule$ruleNum  -fill both -expand 1
+	message $rulesFrame.rule$ruleNum.label -text [lindex $rule 0] -font $kLabelFont
+	pack $rulesFrame.rule$ruleNum.label -side left
+	set rulePartNum 0
+	foreach rulePart [lrange $rule 1 end] {
+	    radiobutton $rulesFrame.rule$ruleNum.p$rulePartNum -text $rulePart -variable [lindex $ruleSettingGlobalNames $ruleNum] -value $rulePartNum -highlightthickness 0 -font $kLabelFont
+	    pack $rulesFrame.rule$ruleNum.p$rulePartNum -side left -expand 1 -fill both
+	    incr rulePartNum
+	}
+	incr ruleNum
+    } 
+}
+
+
+#############################################################################
+# GS_GetOption gets the game option specified by the rules frame
+# Returns the option of the variant of the game specified by the 
+# global variables used by the rules frame
+# Args: none
+# Modifies: nothing
+# Returns: option (Integer) - the option of the game as specified by 
+# getOption and setOption in the module's C code
+#############################################################################
+proc GS_GetOption { } {
+    # TODO: Needs to change with more variants
+    global gMisereGame
+    set option 1
+    set option [expr $option + (1-$gMisereGame)]
+    return $option
+}
+
+
+#############################################################################
+# GS_SetOption modifies the rules frame to match the given options
+# Modifies the global variables used by the rules frame to match the 
+# given game option.
+# This procedure only needs to support options that can be selected 
+# using the rules frame.
+# Args: option (Integer) -  the option of the game as specified by 
+# getOption and setOption in the module's C code
+# Modifies: the global variables used by the rules frame
+# Returns: nothing
+#############################################################################
+proc GS_SetOption { option } {
+    # TODO: Needs to change with more variants
+    global gMisereGame
+    set option [expr $option - 1]
+    set gMisereGame [expr 1-($option%2)]
+}
+
+
+#############################################################################
+# GS_Initialize is where you can start drawing graphics.  
+# Its argument, c, is a canvas.  Please draw only in this canvas.
+# You could put an opening animation in this function that introduces the game
+# or just draw an empty board.
+# This function is called ONCE after GS_InitGameSpecific, and before the
+# player hits "New Game"
+#############################################################################
+proc GS_Initialize { c } {
+
+    # you may want to start by setting the size of the canvas; this line isn't cecessary
+    $c configure -width 300 -height 300
+
+################# Drawing a dummy board for practice #######################
+set boardsize 4
+set dist 100 
+set boardwidth [expr $boardsize * $dist] 
+set numofBlue 0
+set numofRed 0
+#canvas .c
+$c configure -width $boardwidth -height $boardwidth
+pack $c
+$c create rectangle 0 0 $boardwidth $boardwidth -fill white -tags base
+# the board - can use for loop for variable board (currently 3 by 3)
+for {set i 0} {$i < [expr $boardsize - 1]} {set i [expr $i + 1]} {
+    for {set j 0} {$j < [expr $boardsize -1]} {set j [expr $j + 1]} {
+	$c create rectangle [expr $i * $dist + 50] [expr $j * $dist + 50] \
+	    [expr $i * $dist + 150] [expr $j * $dist + 150] -fill white -tags base
+    }
+}
+# place pieces
+for {set j 0} {$j< $boardsize} {set j [expr $j + 1]} {
+    for {set i 0} {$i < $boardsize} {set i [expr $i + 1]} {
+	drawArrows $c [expr $i *$dist +50] [expr $j *$dist +50] blue$numofBlue
+	drawPiece $c [expr $i *$dist +50] [expr $j *$dist +50] blue $numofBlue
+	$c lower blue$numofBlue 
+	set numofBlue [expr $numofBlue + 1]
+	drawArrows $c [expr $i *$dist +50] [expr $j *$dist +50] red$numofRed
+	drawPiece $c [expr $i *$dist +50] [expr $j *$dist +50] red $numofRed
+	$c lower red$numofRed
+	set numofRed [expr $numofRed + 1]
+    }
+}
+
+
+for {set b 0} {$b < $boardsize} {set b [expr $b + 1]} {
+    $c raise blue$b
+    $c lower arrowblue$b
+      
+}
+
+for {set r [expr $boardsize*$boardsize - $boardsize]} {$r < [expr $boardsize*$boardsize]} {set r [expr $r + 1]} {
+    $c raise red$r
+    $c lower arrowred$r
+     
+}
+
+
+##########################################################################
+
+    
+    ### TODO: fill this in
+
+} 
+
+
+#############################################################################
+# GS_Deinitialize deletes everything in the playing canvas.  I'm not sure why this
+# is here, so whoever put this here should update this.  -Jeff
+#############################################################################
+proc GS_Deinitialize { c } {
+    $c delete all
+}
+
+
+#############################################################################
+# GS_DrawPosition this draws the board in an arbitrary position.
+# It's arguments are a canvas, c, where you should draw and the
+# (hashed) position.  For example, if your game is a rearranger,
+# here is where you would draw pieces on the board in their correct positions.
+# Imagine that this function is called when the player
+# loads a saved game, and you must quickly return the board to its saved
+# state.  It probably shouldn't animate, but it can if you want.
+#
+# BY THE WAY: Before you go any further, I recommend writing a tcl function that 
+# UNhashes You'll thank yourself later.
+# Don't bother writing tcl that hashes, that's never necessary.
+#############################################################################
+proc GS_DrawPosition { c position } {
+    global boardsize boardstring
+    set pieceString [string range [C_GenericUnhash $position [expr $boardsize * $boardsize]] 0 [expr $boardsize*$boardsize-1]]
+    
+    # resets board
+    $c raise base
+
+    # raises appropriate pieces
+    for {set i 0} {$i < [expr $boardsize * $boardsize]} {incr $i} {
+	if {[string compare [string index $pieceString $i] "w"] == 0} {
+	    $c raise red$i 
+	    $c lower arrowred$i
+	} elseif {[string compare [string index $pieceString $i] "b"] == 0} {
+	    $c raise blue$i
+	    $c lower arrowblue$i
+	} else {}
+    }
+   
+
+    ### TODO: Fill this in
+
+}
+
+
+#############################################################################
+# GS_NewGame should start playing the game. 
+# It's arguments are a canvas, c, where you should draw 
+# the hashed starting position of the game.
+# This is called just when the player hits "New Game"
+# and before any moves are made.
+#############################################################################
+proc GS_NewGame { c position } {
+    # TODO: The default behavior of this funciton is just to draw the position
+    # but if you want you can add a special behaivior here like an animation
+    GS_DrawPosition $c $position
+}
+
+
+#############################################################################
+# GS_WhoseMove takes a position and returns whose move it is.
+# Your return value should be one of the items in the list returned
+# by GS_NameOfPieces.
+# This function is called just before every move.
+#############################################################################
+proc GS_WhoseMove { position } {
+    # Optional Procedure
+    return ""    
+}
+
+
+#############################################################################
+# GS_HandleMove draws (animates) a move being made.
+# The user or the computer has just made a move, animate it or draw it
+# or whatever.  Draw the piece moving if your game is a rearranger, or
+# the piece appearing if it's a "dart board"
+#
+# By the way, if you animate, a function that will be useful for you is
+# update idletasks.  You can call this to force the canvas to update if
+# you make changes before tcl enters the event loop again.
+#############################################################################
+proc GS_HandleMove { c oldPosition theMove newPosition } {
+
+    GS_DrawPosition $c $newPosition
+    
+}
+
+
+#############################################################################
+# GS_ShowMoves draws the move indicator (be it an arrow or a dot, whatever the
+# player clicks to make the move)  It is also the function that handles coloring
+# of the moves according to value. It is called by gamesman just before the player
+# is prompted for a move.
+#
+# Arguments:
+# c = the canvas to draw in as usual
+# moveType = a string which is either value, moves or best according to which radio button is down
+# position = the current hashed position
+# moveList = a list of lists.  Each list contains a move and its value.
+# These moves are represented as numbers (same as in C)
+# The value will be either "Win" "Lose" or "Tie"
+# Example:  moveList: { 73 Win } { 158 Lose } { 22 Tie } 
+#############################################################################
+proc GS_ShowMoves { c moveType position moveList } {
+ 
+    global boardsize boardstring
+    #set pieceString [string range [C_GenericUnhash $position [expr $boardsize * $boardsize]] 0 [expr $boardsize*$boardsize-1]]
+    
+    foreach item $moveList {
+	set move [lindex $item 0]
+	set value [lindex $item 1]
+	set color cyan
+
+	if {$moveType == "value"} {
+	    if {$value == "Tie"} {
+		set color yellow
+	    } elseif {$value == "Lose"} {
+		set color green
+	    } else {
+		set color red
+	    }
+	}
+
+	set arrayNum [getArraynum [GetXCoord move] [GetYCoord move]]
+	set dir [GetDirection move]
+
+	switch dir {
+	    0 {
+		$c raise arrowUPblue$arrayNum base
+		$c itemconfig arrowUPblue$arrayNum -fill $color
+	    }
+	    1 {
+		$c raise arrowRIGHTblue$arrayNum base
+		$c itemconfig arrowRIGHTblue$arrayNum -fill $color
+	    }
+	    2 {
+		$c raise arrowDOWNblue$arrayNum base
+		$c itemconfig arrowDOWNblue$arrayNum -fill $color
+	    }
+	    3 {
+		$c raise arrowLEFTblue$arrayNum base
+		$c itemconfig arrowLEFTblue$arrayNum -fill $color
+	    }
+	    default {}
+	}
+
+    }
+    update idletasks
+}
+
+
+#############################################################################
+# GS_HideMoves erases the moves drawn by GS_ShowMoves.  It's arguments are the 
+# same as GS_ShowMoves.
+# You might not use all the arguments, and that's okay.
+#############################################################################
+proc GS_HideMoves { c moveType position moveList} {
+    $c lower arrow base
+    update idletasks
+
+}
+
+
+#############################################################################
+# GS_HandleUndo handles undoing a move (possibly with animation)
+# Here's the undo logic
+# The game was in position A, a player makes move M bringing the game to position B
+# then an undo is called
+# currentPosition is the B
+# theMoveToUndo is the M
+# positionAfterUndo is the A
+#
+# By default this function just calls GS_DrawPosition, but you certainly don't 
+# need to keep that.
+#############################################################################
+proc GS_HandleUndo { c currentPosition theMoveToUndo positionAfterUndo} {
+
+    ### TODO if needed
+    GS_DrawPosition $c $positionAfterUndo
+}
+
+
+#############################################################################
+# GS_GetGameSpecificOptions is not quite ready, don't worry about it .
+#############################################################################
+proc GS_GetGameSpecificOptions { } {
+}
+
+
+#############################################################################
+# GS_GameOver is called the moment the game is finished ( won, lost or tied)
+# You could use this function to draw the line striking out the winning row in 
+# tic tac toe for instance.  Or, you could congratulate the winner.
+# Or, do nothing.
+#############################################################################
+proc GS_GameOver { c position gameValue nameOfWinningPiece nameOfWinner lastMove} {
+
+	### TODO if needed
+	
+}
+
+
+#############################################################################
+# GS_UndoGameOver is called when the player hits undo after the game is finished.
+# This is provided so that you may undo the drawing you did in GS_GameOver if you 
+# drew something.
+# For instance, if you drew a line crossing out the winning row in tic tac toe, 
+# this is where you sould delete the line.
+#
+# note: GS_HandleUndo is called regardless of whether the move undoes the end of the 
+# game, so IF you choose to do nothing in GS_GameOver, you needn't do anything here either.
+#############################################################################
+proc GS_UndoGameOver { c position } {
+
+	### TODO if needed
+
+}
+
+
+######################Helper Stuff###########################
+proc drawPiece {c x0 y0 color num} {
+ $c create oval [expr $x0 - 25] [expr $y0 - 25] \
+	[expr $x0 + 25] [expr $y0 + 25] \
+	-fill $color -tags $color$num
+
+}
+proc drawArrows {c x y piece} {
+set arrowLen 75
+    #up
+    $c create line $x $y $x [expr $y - $arrowLen] \
+    -width 15 -arrow last -arrowshape [list 30 30 15] -fill lightblue \
+    -tags [list arrowUP$piece arrow$piece $piece arrow]
+    $c bind arrowUP$piece <Enter> {$c itemconfig arrowUP$piece -fill black}
+    #down
+    $c create line $x $y $x [expr $y + $arrowLen] \
+    -width 15 -arrow last -arrowshape [list 30 30 15] -fill lightblue \
+    -tags [list arrowDOWN$piece arrow$piece $piece arrow]
+    $c bind arrowDOWN$piece <Enter> {$c itemconfig arrowDOWN$piece -fill black}
+    #left
+    $c create line $x $y [expr $x - $arrowLen] $y \
+    -width 15 -arrow last -arrowshape [list 30 30 15] -fill lightblue \
+    -tags [list arrowLEFT$piece arrow$piece $piece arrow]
+    $c bind arrowLEFT$piece <Enter> {$c itemconfig arrowLEFT$piece -fill black}
+    #right
+    $c create line $x $y [expr $x + $arrowLen] $y \
+    -width 15 -arrow last -arrowshape [list 30 30 15] -fill lightblue \
+    -tags [list arrowRIGHT$piece arrow$piece $piece arrow]
+    $c bind arrowRIGHT$piece <Enter> {$c itemconfig arrowRIGHT$piece -fill black}
+}
+
+
+# Move Stuff (Hashers, Unhashers, etc)
+proc GetDirection {theMove} {
+    return [expr $theMove >> 6]
+}
+
+proc GetXCoord {theMove} {
+    return [expr ($theMove >> 3) & 0x7]
+}
+
+proc GetYCoord {theMove} {
+    return [expr $theMove & 0x7]
+}
+
+
+proc getArraynum {xcoord ycoord} {
+    return [expr ($boardsize *($boardsize - $ycoord)) - ($boardsize - $xcoord)]
+}
