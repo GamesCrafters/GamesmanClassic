84988a18fe9932683b6e25f4edef5ee5c316d9f2
ciokita <ciokita@a668be5d-ef11-cb4c-8bfd-e08cbb24f2ba>
2005-05-04 17:16:21 +0000

    updated help
    git-svn-id: https://gamescrafters.svn.sourceforge.net/svnroot/gamescrafters/GamesmanClassic/trunk@1830 a668be5d-ef11-cb4c-8bfd-e08cbb24f2ba

--- gamesman/src/m1210.c
+++ gamesman/src/m1210.c
@@ -50,13 +50,10 @@ void*	 gGameSpecificTclInit = NULL;
 STRING   kHelpGraphicInterface = "";  /* empty since kSupportsGraphics == FALSE */
 
 STRING   kHelpTextInterface    =
-"On your turn, select the number 1 or 2 to raise the total sum. After selecting your move, the total\n\
-is displayed. The play then alternates to the second player, who also has the choice of raising the sum\n\
-by 1 or 2 points. The winner is the first person to raise the total sum to 10. If at any point you have \n\
-made a mistake, type u to revert back to your previous position.";
+"On your turn, select the number 1 or 2 to raise the total sum. After selecting\n your move, the total is displayed. The play then alternates to the second\n player, who also has the choice of raising the sum by 1 or 2 points. The winner\n is the first person to raise the total sum to 10. If at any point you have made\n a mistake, type u to revert back to your previous position.";
 
 STRING   kHelpOnYourTurn =
-"Select the number 1 or 2 to raise the total sum. You may also revert to your previous position by typing u.";
+"Select the number 1 or 2 to raise the total sum. You may also revert to your\n previous position by typing u.";
 
 STRING   kHelpStandardObjective =
 "To be the first player to raise the total to 10.";
