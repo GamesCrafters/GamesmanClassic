e0b0e1070d055d818e3257f5e8d6f6478aef75e6
scarr2508 <scarr2508@a668be5d-ef11-cb4c-8bfd-e08cbb24f2ba>
2005-10-20 07:03:47 +0000

    made game resizable and conform to animation speed preferences
    git-svn-id: https://gamescrafters.svn.sourceforge.net/svnroot/gamescrafters/GamesmanClassic/trunk@1956 a668be5d-ef11-cb4c-8bfd-e08cbb24f2ba

--- gamesman/tcl/mttt3.tcl
+++ gamesman/tcl/mttt3.tcl
@@ -13,97 +13,6 @@
 
 
 
-############   GLOBAL VARS #######################
-
-global dotgap dotmid
-global x1 x2 x3 y1 y2 y3
-global pieceSize pieceOutline xColor oColor pieceOffset
-global dotSize dotExpandAmount lineWidth lineColor baseColor base
-global dotx1 dotx2 dotx3 doty1 doty2 doty3
-global px1 px2 px3 py1 py2 py3
-global diagArrows horizArrows vertArrows slideDelay goDelay animQuality
-global canvasWidth canvasHeight
-
-# Authors Info
-global kRootDir
-global kCAuthors kTclAuthors kGifAuthors
-set kCAuthors "Jesse Phillips, Jennifer Lee"
-set kTclAuthors "Jesse Phillips, Jennifer Lee"
-set kGifAuthors "$kRootDir/../bitmaps/DanGarcia-310x232.gif"
-
-## IMPORTANT: These are variables used to change the board.
-# board size
-set canvasWidth  500; # 230 for first ever board
-set canvasHeight 500
-#coordinates:
-set dotgap [expr $canvasWidth / 3] 
-set dotmid [expr $dotgap / 2]
-set firstXCoord $dotmid
-set firstYCoord $dotmid
-## pieces
-set pieceSize [expr $dotmid - ($dotmid / 4)]
-set pieceOutline [expr $pieceSize / 14]
-set xColor blue4
-set oColor red2
-set pieceOffset $dotgap
-## dots
-set dotSize [expr $pieceSize.0 / 2.6]
-set dotExpandAmount [expr 3 * ( $dotSize / 4 )]
-## lines
-set lineWidth [ expr 3 * ( $pieceOutline / 2 ) ]
-set lineColor CadetBlue4
-## base
-set baseColor white
-## arrow lists
-set diagArrows  {list 15 51 26 62 48 84 59 95 24 42 35 53 57 75 68 86}
-set horizArrows {list 12 21 23 32 45 54 56 65 78 87 89 98}
-set vertArrows  {list 14 41 25 52 36 63 47 74 58 85 69 96}
-## animation delay
-set slideDelay 20000
-set goDelay 3000000
-set animQuality "low"
-# x and y position numbers
-set x1 $firstXCoord
-set x2 [expr $x1 + $dotgap]
-set x3 [expr $x2 + $dotgap]
-set x4 $x1
-set x5 $x2
-set x6 $x3
-set x7 $x1
-set x8 $x2
-set x9 $x3
-
-set y1 $firstYCoord
-set y2 $y1
-set y3 $y1
-set y4 [expr $y1 + $dotgap]
-set y5 $y4
-set y6 $y4
-set y7 [expr $y4 + $dotgap]
-set y8 $y7
-set y9 $y7
-
-
-
-set dotx1 [expr $x1 - [expr $dotSize / 2]];
-set dotx2 [expr $x2 - [expr $dotSize / 2]];
-set dotx3 [expr $x3 - [expr $dotSize / 2]];
-set doty1 [expr $y1 - [expr $dotSize / 2]];
-set doty2 [expr $y4 - [expr $dotSize / 2]];
-set doty3 [expr $y7 - [expr $dotSize / 2]];
-
-set px1 [expr $x1 - [expr $pieceSize / 2]];
-set px2 [expr $x2 - [expr $pieceSize / 2]];
-set px3 [expr $x3 - [expr $pieceSize / 2]];
-set py1 [expr $y1 - [expr $pieceSize / 2]];
-set py2 [expr $y4 - [expr $pieceSize / 2]];
-set py3 [expr $y7 - [expr $pieceSize / 2]];
-
-
-
-
-
-
 
 proc GS_InitGameSpecific {} {
     
@@ -134,6 +43,94 @@ proc GS_InitGameSpecific {} {
     SetToWinString [concat $toWin1 $toWin2]
 
     SetToMoveString  "To Move: Click near one of the dots in an empty square. This will place a piece for you"
+
+    ############   GLOBAL VARS #######################
+
+    global dotgap dotmid
+    global x1 x2 x3 y1 y2 y3
+    global pieceSize pieceOutline xColor oColor pieceOffset
+    global dotSize dotExpandAmount lineWidth lineColor baseColor base
+    global dotx1 dotx2 dotx3 doty1 doty2 doty3
+    global px1 px2 px3 py1 py2 py3
+    global diagArrows horizArrows vertArrows slideDelay goDelay animQuality
+    global gFrameWidth gFrameHeight
+    global boardSize squareSize leftBuffer topBuffer
+    # Authors Info
+    global kRootDir
+    global kCAuthors kTclAuthors kGifAuthors
+
+    set kCAuthors "Jesse Phillips, Jennifer Lee"
+    set kTclAuthors "Jesse Phillips, Jennifer Lee"
+    set kGifAuthors "$kRootDir/../bitmaps/DanGarcia-310x232.gif"
+
+    ## IMPORTANT: These are variables used to change the board.
+    # board size
+    set boardSize [min $gFrameWidth $gFrameHeight]
+    set squareSize [expr $boardSize / 3]
+    set leftBuffer [expr [expr $gFrameWidth - $boardSize] / 2]
+    set topBuffer [expr [expr $gFrameHeight - $boardSize] / 2]
+    #coordinates:
+    set dotgap [expr $boardSize / 3] 
+    set dotmid [expr $dotgap / 2]
+    set firstXCoord [expr $leftBuffer + $dotmid]
+    set firstYCoord [expr $topBuffer + $dotmid]
+    ## pieces
+    set pieceSize [expr $dotmid - ($dotmid / 4)]
+    set pieceOutline [expr $pieceSize / 14]
+    set xColor blue4
+    set oColor red2
+    set pieceOffset $dotgap
+    ## dots
+    set dotSize [expr $pieceSize.0 / 2.6]
+    set dotExpandAmount [expr 3 * ( $dotSize / 4 )]
+    ## lines
+    set lineWidth [ expr 3 * ( $pieceOutline / 2 ) ]
+    set lineColor CadetBlue4
+    ## base
+    set baseColor white
+    ## arrow lists
+    set diagArrows  {list 15 51 26 62 48 84 59 95 24 42 35 53 57 75 68 86}
+    set horizArrows {list 12 21 23 32 45 54 56 65 78 87 89 98}
+    set vertArrows  {list 14 41 25 52 36 63 47 74 58 85 69 96}
+    ## animation delay
+    set slideDelay 20000
+    set goDelay 3000000
+    set animQuality "low"
+    # x and y position numbers
+    set x1 $firstXCoord
+    set x2 [expr $x1 + $dotgap]
+    set x3 [expr $x2 + $dotgap]
+    set x4 $x1
+    set x5 $x2
+    set x6 $x3
+    set x7 $x1
+    set x8 $x2
+    set x9 $x3
+    
+    set y1 $firstYCoord
+    set y2 $y1
+    set y3 $y1
+    set y4 [expr $y1 + $dotgap]
+    set y5 $y4
+    set y6 $y4
+    set y7 [expr $y4 + $dotgap]
+    set y8 $y7
+    set y9 $y7
+        
+    set dotx1 [expr $x1 - [expr $dotSize / 2]];
+    set dotx2 [expr $x2 - [expr $dotSize / 2]];
+    set dotx3 [expr $x3 - [expr $dotSize / 2]];
+    set doty1 [expr $y1 - [expr $dotSize / 2]];
+    set doty2 [expr $y4 - [expr $dotSize / 2]];
+    set doty3 [expr $y7 - [expr $dotSize / 2]];
+    
+    set px1 [expr $x1 - [expr $pieceSize / 2]];
+    set px2 [expr $x2 - [expr $pieceSize / 2]];
+    set px3 [expr $x3 - [expr $pieceSize / 2]];
+    set py1 [expr $y1 - [expr $pieceSize / 2]];
+    set py2 [expr $y4 - [expr $pieceSize / 2]];
+    set py3 [expr $y7 - [expr $pieceSize / 2]];
+    
     
 }
 
@@ -194,17 +191,37 @@ proc GS_SetOption { option } {
     set option [expr $option - 1]
     set gMisereGame [expr $option%2]
 }
-
+proc min { num1 num2 } {
+    if { $num1 < $num2 } {
+	return $num1
+    } else {
+	return $num2
+    }
+}
 proc GS_Initialize { c } {
     #puts [format "GS_Initialize called on canvas %s" $c]
+    global boardSize squareSize leftBuffer topBuffer
+
     set counter 0
     for {set j 0} {$j < 3} {set j [expr $j + 1]} {
         
         for {set i 0} {$i < 3} {set i [expr $i + 1]} {
             
-            $c create rect [expr $i * 166] [expr $j * 166] [expr ($i+1) * 166] [expr ($j+1) * 166] -fill white -tag [list base base$counter]
+#            $c create rect [expr $i * 166] [expr $j * 166] [expr ($i+1) * 166] [expr ($j+1) * 166] -fill white -tag [list base base$counter]
+            $c create rect \
+		[expr $leftBuffer + [expr $i * $squareSize]] \
+		[expr $topBuffer + [expr $j * $squareSize]] \
+		[expr $leftBuffer + [expr ($i+1) * $squareSize]] \
+		[expr $topBuffer + [expr ($j+1) * $squareSize]] \
+		-fill white -tag [list base base$counter]
             
-            $c create oval [expr ($i+.4) * 166] [expr ($j+.4) * 166] [expr ($i+.6) * 166] [expr ($j+.6) * 166] -fill white -outline "" -tag [list moveindicators mi-$counter]
+#            $c create oval [expr ($i+.4) * 166] [expr ($j+.4) * 166] [expr ($i+.6) * 166] [expr ($j+.6) * 166] -fill white -outline "" -tag [list moveindicators mi-$counter]
+            $c create oval \
+		[expr $leftBuffer + [expr ($i+.4) * $squareSize]] \
+		[expr $topBuffer + [expr ($j+.4) * $squareSize]] \
+		[expr $leftBuffer + [expr ($i+.6) * $squareSize]] \
+		[expr $topBuffer + [expr ($j+.6) * $squareSize]] \
+		-fill white -outline "" -tag [list moveindicators mi-$counter]
             
             $c bind base$counter <ButtonRelease-1> "ReturnFromHumanMove $counter"
             
@@ -215,25 +232,27 @@ proc GS_Initialize { c } {
     
 
     #left-mid vertical border
-    $c create line 166 0 166 500 -width 2 -tag base
+#    $c create line 166 0 166 500 -width 2 -tag base
+    $c create line [expr $leftBuffer + $squareSize] $topBuffer [expr $leftBuffer + $squareSize] [expr $topBuffer + $boardSize] -width 2 -tag base
 
     #right-mid vertical border
-    $c create line 332 0 332 500 -width 2 -tag base
+    $c create line [expr $leftBuffer + [expr 2 * $squareSize]] $topBuffer [expr $leftBuffer + [expr 2 * $squareSize]] [expr $topBuffer + $boardSize] -width 2 -tag base
     
 
     #top-mid horizontal border
-    $c create line 0 166 500 166 -width 2 -tag base
+#    $c create line 0 166 500 166 -width 2 -tag base
+    $c create line $leftBuffer [expr $topBuffer + $squareSize] [expr $leftBuffer + $boardSize] [expr $topBuffer + $squareSize] -width 2 -tag base
     
     
     #bottom-mid horizontal border
-    $c create line 0 332 500 332 -width 2 -tag base
+#    $c create line 0 332 500 332 -width 2 -tag base
+    $c create line $leftBuffer [expr $topBuffer + [expr 2 * $squareSize]] [expr $leftBuffer + $boardSize] [expr $topBuffer + [expr 2 * $squareSize]] -width 2 -tag base
+
     MakePieces $c 0
     
     $c raise base
     NameFlash $c
     update idletasks
-
-
    
 }
 
@@ -244,27 +263,27 @@ proc GS_Deinitialize { c } {
 
 # fancy flashing of letters of the game (spelling the name of the game)
 proc NameFlash { c } {
-    global dotmid
+    global dotmid leftBuffer topBuffer
 
     #"T" in top left sq
     #$c create line 33 33 133 33 -width 10 -fill red
     #$c create line 83 33 83 133 -width 10 -fill red
 
     #"TIC"
-    $c create text [expr 1 * $dotmid] [expr 1 * $dotmid] -text "T" -font {Helvetica 100} -fill red -tags {ttic ttt}
-$c create text [expr 3 * $dotmid] [expr 1 * $dotmid] -text "I" -font {Helvetica 100} -fill red -tags {itic ttt}
-    $c create text [expr 5 * $dotmid] [expr 1 * $dotmid] -text "C" -font {Helvetica 100} -fill red -tags {ctic ttt}
+    $c create text [expr 1 * $dotmid + $leftBuffer] [expr 1 * $dotmid + $topBuffer] -text "T" -font {Helvetica 100} -fill red -tags {ttic ttt}
+    $c create text [expr 3 * $dotmid + $leftBuffer] [expr 1 * $dotmid + $topBuffer] -text "I" -font {Helvetica 100} -fill red -tags {itic ttt}
+    $c create text [expr 5 * $dotmid + $leftBuffer] [expr 1 * $dotmid + $topBuffer] -text "C" -font {Helvetica 100} -fill red -tags {ctic ttt}
     
     #"TAC"
-    $c create text [expr 1 * $dotmid] [expr 3 * $dotmid] -text "T" -font {Helvetica 100} -fill green -tags {ttac ttt}
-   $c create text [expr 3 * $dotmid] [expr 3 * $dotmid] -text "A" -font {Helvetica 100} -fill green -tags {atac ttt}
-    $c create text [expr 5 * $dotmid] [expr 3 * $dotmid] -text "C" -font {Helvetica 100} -fill green -tags {ctac ttt}
+    $c create text [expr 1 * $dotmid + $leftBuffer] [expr 3 * $dotmid + $topBuffer] -text "T" -font {Helvetica 100} -fill green -tags {ttac ttt}
+    $c create text [expr 3 * $dotmid + $leftBuffer] [expr 3 * $dotmid + $topBuffer] -text "A" -font {Helvetica 100} -fill green -tags {atac ttt}
+    $c create text [expr 5 * $dotmid + $leftBuffer] [expr 3 * $dotmid + $topBuffer] -text "C" -font {Helvetica 100} -fill green -tags {ctac ttt}
 
 
     #"TOE"
-$c create text [expr 1 * $dotmid] [expr 5 * $dotmid] -text "T" -font {Helvetica 100} -fill blue -tags {ttoe ttt}
-   $c create text [expr 3 * $dotmid] [expr 5 * $dotmid] -text "O" -font {Helvetica 100} -fill blue -tags {otoe ttt}
-    $c create text [expr 5 * $dotmid] [expr 5 * $dotmid] -text "E" -font {Helvetica 100} -fill blue -tags {etoe ttt}
+    $c create text [expr 1 * $dotmid + $leftBuffer] [expr 5 * $dotmid + $topBuffer] -text "T" -font {Helvetica 100} -fill blue -tags {ttoe ttt}
+    $c create text [expr 3 * $dotmid + $leftBuffer] [expr 5 * $dotmid + $topBuffer] -text "O" -font {Helvetica 100} -fill blue -tags {otoe ttt}
+    $c create text [expr 5 * $dotmid + $leftBuffer] [expr 5 * $dotmid + $topBuffer] -text "E" -font {Helvetica 100} -fill blue -tags {etoe ttt}
 
 
 $c lower ttt base
@@ -395,30 +414,15 @@ $c lower ttt base
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
 }
 global coordList 
 set coordList {}
 
 
-
 proc MakePieces { c num } {
-
   
     MakeX $c [expr $num % 3] [expr $num / 3] $num
     MakeO $c [expr $num % 3] [expr $num / 3] $num
-
 
     if { $num != 8 } {
         MakePieces $c [expr $num + 1]
@@ -428,16 +432,12 @@ proc MakePieces { c num } {
 
 #Changed to make it look more like lite-3 - Jeffrey Chiang
 proc MakeX { c x y tag } {
-    
-
+    global squareSize leftBuffer topBuffer
 
     #old way
     
-
-     set x [expr $x * 166]
-     set y [expr $y * 166]
-     
-
+     set x [expr $x * $squareSize + $leftBuffer]
+     set y [expr $y * $squareSize + $topBuffer]
  
      #full X
     #$c create line [expr $x  + 10] [expr $y + 10] [expr $x + 156] [expr $y + 156] -width 10 -fill blue -capstyle round -tag x-$tag 
@@ -446,7 +446,7 @@ proc MakeX { c x y tag } {
 
      #X dots
     $c create line [expr $x  + 10] [expr $y + 10] [expr $x + 10] [expr $y + 10] -width 24 -fill blue -tag x-$tag-1 
-    $c create line [expr $x  + 156] [expr $y + 10] [expr $x + 156] [expr $y + 10] -width 24 -fill blue -tag x-$tag-2
+    $c create line [expr $x  + [expr $squareSize - 10]] [expr $y + 10] [expr $x + [expr $squareSize - 10]] [expr $y + 10] -width 24 -fill blue -tag x-$tag-2
     $c lower x-$tag-1 base
     $c lower x-$tag-2 base
 
@@ -455,23 +455,21 @@ proc MakeX { c x y tag } {
 ###  (draw with mousebutton 1)
 
 #bind $c <B1-Motion> "$c create rectangle  %x %y %x %y -outline blue  -width 10"
-
-
-
     
 }
 
 # Changed to make it look more like Lite-3 - Jeffrey Chiang
 proc MakeO { c x y tag } {
-     set x [expr $x * 166]
-     set y [expr $y * 166]
+    global squareSize leftBuffer topBuffer
 
+    set x [expr $x * $squareSize + $leftBuffer]
+    set y [expr $y * $squareSize + $topBuffer]
 
 # for the red dot in the top corner of the square
 #    $c create oval $x $y [expr $x + 1] [expr $y + 1] -width 10 -outline red -tag o-$tag
 
-    $c create oval [expr $x  + 20] [expr $y + 20] [expr $x + 146] [expr $y + 146] -width 16 -outline red -tag actual-o-$tag
-    $c create arc [expr $x  + 20] [expr $y + 20] [expr $x + 146] [expr $y + 146] -start 90 -extent 0 -style arc -width 16 -outline red -tag o-$tag
+    $c create oval [expr $x  + 20] [expr $y + 20] [expr $x + [expr $squareSize - 20]] [expr $y + [expr $squareSize - 20]] -width 16 -outline red -tag actual-o-$tag
+    $c create arc [expr $x  + 20] [expr $y + 20] [expr $x + [expr $squareSize - 20]] [expr $y + [expr $squareSize - 20]] -start 90 -extent 0 -style arc -width 16 -outline red -tag o-$tag
     $c lower o-$tag base
 
   #new way
@@ -574,24 +572,32 @@ proc DrawX { c x y positionNum } {
 
 # Changed this to make it look more like Lite-3  - Jeffrey Chiang
 proc stretchX1 { c x y tag } {
-     for {set i 30} {$i < 137} {incr i 1} {
+    global squareSize gAnimationSpeed
+    set animSpeed [expr 5 * [expr $gAnimationSpeed + 6]]
+    for {set i 30} {$i < [expr $squareSize - 30]} {incr i $animSpeed} {
        $c coords $tag [expr $x + 30] [expr $y + 30] [expr $x + $i] [expr $y + $i]
 	 after 0 
 	 update idletasks
      }
+    $c coords $tag [expr $x + 30] [expr $y + 30] [expr $x + $squareSize - 30] [expr $y + $squareSize - 30]
 }
 
 # Changed this to make it look more like Lite-3  - Jeffrey Chiang
 proc stretchX2 { c x y tag } {
-     for {set i 30} {$i < 137} {incr i 1} {
-       $c coords $tag [expr $x + 136] [expr $y + 30] [expr $x + 166 - $i] [expr $y + $i]
+    global squareSize gAnimationSpeed
+    set animSpeed [expr 5 * [expr $gAnimationSpeed + 6]]
+    for {set i 30} {$i < [expr $squareSize - 30]} {incr i $animSpeed} {
+	$c coords $tag [expr $x + [expr $squareSize - 30]] [expr $y + 30] [expr $x + $squareSize - $i] [expr $y + $i]
 	 after 0 
 	 update idletasks
      }
+    $c coords $tag [expr $x + [expr $squareSize - 30]] [expr $y + 30] [expr $x + 30] [expr $y + $squareSize - 30]
 }
 
 proc stretchO { c tag } {
-    for {set i 0} {$i < 360} {incr i 1} {
+    global gAnimationSpeed
+    set animSpeed [expr 5 * [expr $gAnimationSpeed + 6]]
+    for {set i 0} {$i < 360} {incr i $animSpeed} {
 	$c itemconfigure $tag -extent [expr 0 - $i]
 	after 0
 	update idletasks
@@ -600,8 +606,10 @@ proc stretchO { c tag } {
 }
 
 proc GS_HandleMove { c oldPosition theMove newPosition } {
-    set x [expr [expr $theMove % 3] * 166]
-    set y [expr [expr $theMove / 3] * 166]
+    global squareSize leftBuffer topBuffer
+
+    set x [expr [expr $theMove % 3] * $squareSize + $leftBuffer]
+    set y [expr [expr $theMove / 3] * $squareSize + $topBuffer]
 
     #for getting position in which to draw the piece
 
@@ -680,7 +688,8 @@ proc GS_HideMoves { c moveType position moveList} {
 }
 
 proc GS_GameOver { c position gameValue nameOfWinningPiece nameOfWinner lastMove } {
-    
+    global squareSize leftBuffer topBuffer
+
     UnHashBoard $position a
     
     if { $gameValue != "Tie" } {
@@ -694,10 +703,10 @@ proc GS_GameOver { c position gameValue nameOfWinningPiece nameOfWinner lastMove
         if {$a(0) == $a(4) && $a(4) == $a(8)} {set p {0 8}}
         if {$a(2) == $a(4) && $a(4) == $a(6)} {set p {2 6}}
                 
-        set fromx [expr ([lindex $p 0] % 3 + .5)*166]
-        set fromy [expr ([lindex $p 0] / 3 + .5)*166]
-        set tox [expr ([lindex $p 1] % 3 + .5)*166]
-        set toy [expr ([lindex $p 1] / 3 + .5)*166]
+        set fromx [expr ([lindex $p 0] % 3 + .5)*$squareSize + $leftBuffer]
+        set fromy [expr ([lindex $p 0] / 3 + .5)*$squareSize + $topBuffer]
+        set tox [expr ([lindex $p 1] % 3 + .5)*$squareSize + $leftBuffer]
+        set toy [expr ([lindex $p 1] / 3 + .5)*$squareSize + $topBuffer]
         
         $c create line $fromx $fromy $tox $toy -width 15 -fill black -tag bigline -capstyle round
     }
