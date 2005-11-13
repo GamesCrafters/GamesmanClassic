f501cff0183b360a0acc64511b722398f4500827
hevanm <hevanm@a668be5d-ef11-cb4c-8bfd-e08cbb24f2ba>
2005-11-13 23:14:14 +0000

    Added licence and copyright notice to univdb and univht.
    git-svn-id: https://gamescrafters.svn.sourceforge.net/svnroot/gamescrafters/GamesmanClassic/trunk@2019 a668be5d-ef11-cb4c-8bfd-e08cbb24f2ba

--- gamesman/src/core/univdb.c
+++ gamesman/src/core/univdb.c
@@ -1,3 +1,34 @@
+/************************************************************************
+**
+** NAME:	univdb.c
+**
+** DESCRIPTION:	Collision Database - implementation
+**
+** AUTHOR:	GamesCrafters Research Group, UC Berkeley
+**		Supervised by Dan Garcia <ddgarcia@cs.berkeley.edu>
+**
+** DATE:	2005-11-12
+**
+** LICENSE:	This file is part of GAMESMAN,
+**		The Finite, Two-person Perfect-Information Game Generator
+**		Released under the GPL:
+**
+** This program is free software; you can redistribute it and/or modify
+** it under the terms of the GNU General Public License as published by
+** the Free Software Foundation; either version 2 of the License, or
+** (at your option) any later version.
+**
+** This program is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+** GNU General Public License for more details.
+**
+** You should have received a copy of the GNU General Public License
+** along with this program, in COPYING; if not, write to the Free Software
+** Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+**
+**************************************************************************/
+
 #include "gamesman.h"
 #include "univdb.h"
 #include "univht.h"
