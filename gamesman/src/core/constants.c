318d351fe7e82bac0147aad7cf21298aa3c832b6
ddgarcia <ddgarcia@a668be5d-ef11-cb4c-8bfd-e08cbb24f2ba>
2006-01-01 21:05:32 +0000

    Edited the intro text and the version number. Hopefully an improvement... -dan
    git-svn-id: https://gamescrafters.svn.sourceforge.net/svnroot/gamescrafters/GamesmanClassic/trunk@2139 a668be5d-ef11-cb4c-8bfd-e08cbb24f2ba

--- gamesman/src/core/constants.c
+++ gamesman/src/core/constants.c
@@ -38,17 +38,24 @@
 */
 
 STRING   kOpeningCredits =
-"\nWelcome to GAMESMAN, version %s, written by Dan Garcia.\n\n"
-"\t(G)ame-independent\n"
-"\t(A)utomatic\n"
-"\t(M)ove-tree\n"
-"\t(E)xhaustive\n"
-"\t(S)earch,\n"
-"\t(M)anipulation\n"
-"\t(A)nd\n"
-"\t(N)avigation\n\n"
-"\tThis program will determine the value of %s, and allow you to play\n"
-"\tthe computer or another human. Have fun!\n";
+"\n"
+"  ____               http://gamescrafters.berkeley.edu  : A finite,  two-person\n"
+" / ___| __ _ _ __ ___   ___  ___ _ __ ___   __ _ _ __   : complete  information\n"
+"| |  _ / _` | '_ ` _ \\ / _ \\/ __| '_ ` _ \\ / _` | '_ \\  : game generator.  More\n"
+"| |_| | (_| | | | | | |  __/\\__ \\ | | | | | (_| | | | | : information?  Contact\n"
+" \\____|\\__,_|_| |_| |_|\\___||___/_| |_| |_|\\__,_|_| |_| : ddgarcia@berkeley.edu\n"
+"...............................................................................\n"
+"\n"
+"Welcome to GAMESMAN, version %s. Originally       (G)ame-independent\n"
+"written by Dan Garcia, it has undergone a series of       (A)utomatic       \n"
+"exhancements from 2001-present by GamesCrafters, the      (M)ove-tree       \n"
+"UC Berkeley Undergraduate Game Theory Research Group.     (E)xhaustive      \n"
+"                                                          (S)earch,         \n"
+"This program will determine the value of your game,       (M)anipulation    \n"
+"perform analysis, & provide an interface to play it.      (A)nd             \n"
+"Usage: %37s --help       (N)avigation      \n"
+"\n"
+"You are playing %s, written by %s. Enjoy!\n";
 
 STRING   kHelpValueBeforeEvaluation =
 "At this point, the computer does not know the value of the game\n"
@@ -148,7 +155,6 @@ STRING  kHandleDefaultTextInputHelp =
 
 STRING kPrintMenu = 
 "\n"
-"\t---- Print Menu ----\n\n"
 "\tr)\t(R)eprint the position\n";
 
 
@@ -175,7 +181,7 @@ char *gValueString[] =
 
 char gValueLetter[] = { 'W', 'L', 'T', 'U' };
 
-STRING kSolveVersion = "2005.02.09" ;
+STRING kSolveVersion = "2006.01.01" ;
 
 STRING kCommandSyntaxHelp =
 "\nSyntax:\n"
