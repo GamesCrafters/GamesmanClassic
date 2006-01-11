fb2a21c64882d0163029df3a94831a54e2ca24b7
mtanev <mtanev@a668be5d-ef11-cb4c-8bfd-e08cbb24f2ba>
2006-01-11 06:14:40 +0000

    Multiple bugfixes, most importantly, ties are always scored 0, due to their impartiality
    git-svn-id: https://gamescrafters.svn.sourceforge.net/svnroot/gamescrafters/GamesmanClassic/trunk@2149 a668be5d-ef11-cb4c-8bfd-e08cbb24f2ba

--- gamesman/src/core/solveweakab.c
+++ gamesman/src/core/solveweakab.c
@@ -1,6 +1,6 @@
 #include "solveweakab.h"
 
-#define INFINITY (2*gMaxRemoteness + 1)
+#define INFINITY (gMaxRemoteness + 1)
 
 /*
  Function: invert_score
@@ -15,16 +15,14 @@ int ctra = 0;
 
 SCORE invert_score(SCORE score, REMOTENESS max_remoteness) {
 
-  /* If score is in the tie range, retain it */
-  if (score >= -max_remoteness && score <= max_remoteness) {
-    return score;
-  }
-  /* Otherwise, negate it */
-  else {
-    return -score;
-  }
+  return -score;
 
 }
+
+
+/* PROBLEM:
+   ALL DRAWS MUST BE 0 as there are no score symmetries
+*/
 
 SCORE generate_score(VALUE value, REMOTENESS remoteness, REMOTENESS min_remoteness, REMOTENESS max_remoteness) {
 
@@ -38,14 +36,27 @@ SCORE generate_score(VALUE value, REMOTENESS remoteness, REMOTENESS min_remotene
 
   case win:
     /* If value is win, subtract remoteness from maximum score and add min_remoteness */
-    return 2*max_remoteness + min_remoteness - remoteness;
+    score = max_remoteness + min_remoteness - remoteness;
+    break;
   case lose:
     /* If value is lose, add remoteness to minimum score and subtract min_remoteness */
-    return -2*max_remoteness - min_remoteness + remoteness;
+    score = -max_remoteness - min_remoteness + remoteness;
+    break;
   case tie:
-    /* If value is tie, subtract remoteness from maximum tie score and add min_remoteness */
-    return max_remoteness + min_remoteness - remoteness;
+    /* If value is tie, score is 0 */
+    score = 0;
+    break;
+  case undecided:
+    fprintf(stderr, "ERROR: generate_score invoked with undecided game value\n");
+    break;
+  default:    
+    printf("Invalid game value: %d\n", value);
   }
+
+  if (abs(score) >= INFINITY) {
+    fprintf(stderr, "SCORE %d out of bounds, value = %d, remoteness = %d\n", score, value, remoteness);
+  }
+  return score;
 
 }
 
@@ -57,8 +68,17 @@ SCORE alpha_beta(POSITION position, SCORE alpha, SCORE beta, REMOTENESS min_remo
   REMOTENESS remoteness;
   MOVELIST *moves_list, *move_node;
   POSITION child, best_child;
-  SCORE score;
-  
+  SCORE score, best_score;
+  BOOLEAN set = FALSE;
+  BOOLEAN run = FALSE;
+
+  if (alpha>=beta) {
+
+    fprintf(stderr, "alpha_beta invoked with alpha=%d, beta=%d\n", alpha, beta);
+
+  }
+
+  best_score = -INFINITY;
   ctra++;
   if (!(ctra & 0xFFFF)) {
     printf("evaluated %d positions\n", ctra);
@@ -67,8 +87,15 @@ SCORE alpha_beta(POSITION position, SCORE alpha, SCORE beta, REMOTENESS min_remo
   /* First examine if the game value of position is known already */
   value = GetValueOfPosition(position);
   
-  /* If the game value of this position is not known yet */
-  if (value == undecided) {
+  /* If game value of position is known already */
+  if (value != undecided) {
+    
+    /* Extract remoteness of position */
+    remoteness = Remoteness(position);
+    
+  }
+  /* If game value of position is not known yet */
+  else {
     
     /* Check if the position is terminal and extract value */
     value = Primitive(position);
@@ -85,12 +112,18 @@ SCORE alpha_beta(POSITION position, SCORE alpha, SCORE beta, REMOTENESS min_remo
       
       /* Generate possible moves from this position */
       moves_list = GenerateMoves(position);
+
+      if (moves_list == NULL) {
+	fprintf(stderr,"ERROR: empty move list\n");
+      }
       
       /* For every possible move until alpha >= beta */
       for (move_node = moves_list;
 	   move_node != NULL && alpha < beta;
 	   move_node = move_node->next)
 	{
+	  SCORE tempa, tempb;
+	  run = TRUE;
 	  
 	  /* Obtain position resulting from application of move */
 	  child = DoMove(position, move_node->move);
@@ -117,16 +150,21 @@ SCORE alpha_beta(POSITION position, SCORE alpha, SCORE beta, REMOTENESS min_remo
 				       invert_score(alpha, max_remoteness),
 				       min_remoteness - 1,
 				       max_remoteness),
-				max_remoteness);
-	  
-	  /* If inverse of min score exceeds alpha */
-	  if (score > alpha) {
+				       max_remoteness);
+
+	  if (score > best_score) {
 	    
-	    /* Replace alpha with inverse of min score */
-	    alpha = score;
-	    
-	    /* Record child as best child */
+	    best_score = score;
 	    best_child = child;
+	    set = TRUE;
+	    
+	    /* If inverse of min score exceeds alpha */
+	    if (best_score > alpha) {
+	      
+	      /* Replace alpha with inverse of min score */
+	      alpha = best_score;
+	      
+	    }
 	    
 	  }
 	  
@@ -154,12 +192,14 @@ SCORE alpha_beta(POSITION position, SCORE alpha, SCORE beta, REMOTENESS min_remo
 	/* If child's value is tie, this board's value is tie */
 	value = tie;
 	break;
+      case undecided:
+	break;
 	
       }
       
       /* Set remoteness of position value to one more than that of chosen child */
-      remoteness = Remoteness(child) + 1;
-      
+      remoteness = Remoteness(best_child) + 1;
+
     }
     
     /* Store value of position in database */
@@ -181,6 +221,7 @@ SCORE MTD(POSITION position, SCORE score) {
 
   upperbound = +INFINITY;
   lowerbound = -INFINITY;
+  int counter = 1;
   
   /* Repeat until zeroed in on score */
   do {  
@@ -191,8 +232,12 @@ SCORE MTD(POSITION position, SCORE score) {
     */
     beta = (lowerbound == score) ? score + 1 : score;
     
+    printf("Alpha-beta run #%d alpha=%d, beta=%d\n", counter, beta - 1, beta);
+    
     /* Run the alpha-beta pruning minimax search with alpha = beta - 1 */
     score = alpha_beta(position, beta - 1, beta, gMinRemoteness, gMaxRemoteness);
+    
+    printf("Score obtained in alpha-beta run #%d is %d\n", counter++, score);
     
     /* If score is less than beta, change upperbound to equal score */
     if (score < beta) {
