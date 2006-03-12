10d8fccd42224e5a328a5660e1e51a5f0d29d3ae
djvicdaquik <djvicdaquik@a668be5d-ef11-cb4c-8bfd-e08cbb24f2ba>
2006-03-12 22:37:06 +0000

    commonly used header types and values -vperez
    git-svn-id: https://gamescrafters.svn.sourceforge.net/svnroot/gamescrafters/GamesmanClassic/trunk@2302 a668be5d-ef11-cb4c-8bfd-e08cbb24f2ba

---
+++ gamesman/java/src/edu/berkeley/gamesman/server/registration/Macros.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package edu.berkeley.gamesman.server.registration;
+
+/**
+ * @author vperez
+ *	Commonly used header types and values
+ */
+public final class Macros {
+
+	public static final String STATUS = "STATUS";
+	public static final String ERROR_CODE = "ERROR_CODE";
+	public static final String SECRET_KEY = "SECRET_KEY";
+	public static final String ACK = "ACK";
+	public static final String DENY = "DENY";
+	public static final String ONLINE_USERS = "ONLINE_USERS";
+}
