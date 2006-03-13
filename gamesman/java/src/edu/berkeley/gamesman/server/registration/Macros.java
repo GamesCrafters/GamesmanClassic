73155e08b63c7b044640d9833e68ec3645b639b8
djvicdaquik <djvicdaquik@a668be5d-ef11-cb4c-8bfd-e08cbb24f2ba>
2006-03-13 00:44:23 +0000

    added header and requestType macros -vperez
    git-svn-id: https://gamescrafters.svn.sourceforge.net/svnroot/gamescrafters/GamesmanClassic/trunk@2303 a668be5d-ef11-cb4c-8bfd-e08cbb24f2ba

--- gamesman/java/src/edu/berkeley/gamesman/server/registration/Macros.java
+++ gamesman/java/src/edu/berkeley/gamesman/server/registration/Macros.java
@@ -10,9 +10,31 @@ package edu.berkeley.gamesman.server.registration;
 public final class Macros {
 
 	public static final String STATUS = "STATUS";
-	public static final String ERROR_CODE = "ERROR_CODE";
-	public static final String SECRET_KEY = "SECRET_KEY";
 	public static final String ACK = "ACK";
 	public static final String DENY = "DENY";
 	public static final String ONLINE_USERS = "ONLINE_USERS";
+	public static final int VALID = 0;
+	
+	//Headers
+	public static final String TYPE = "TYPE";
+	public static final String NAME = "NAME";
+	public static final String GAME = "GAME";
+	public static final String SECRET_KEY = "SECRETKEY";
+	public static final String ERROR_CODE = "ERRORCODE";
+	public static final String VARIATION = "VARIATION";
+	public static final String GAME_MESSAGE = "GAMEMESSAGE";
+	public static final String GAME_ID = "GAMEID";
+	public static final String OPPONENT_USERNAME = "OPPONENTUSERNAME";
+	public static final String ACCEPTED = "ACCEPTED";
+	
+	//Request Types
+	public static final String REG_MOD_REGISTER_USER = "RegModRegisterUser";
+	public static final String REG_MOD_GET_USERS_ONLINE = "RegModGetUsersOnline";
+	public static final String REG_MOD_REGISTER_NEW_GAME = "RegModRegisterNewGame";
+	public static final String REG_MOD_UNREGISTER_GAME = "RegModUnRegisterGame";
+	public static final String REG_MOD_JOIN_GAME_NUMBER = "RegModJoinGameNumber";
+	public static final String REG_MOD_JOIN_GAME_USER = "RegModJoinGameUser";
+	public static final String REG_MOD_REFRESH_STATUS = "RegModRefreshStatus";
+	public static final String REG_MOD_ACCEPT_CHALLENGE = "RegModAcceptChallenge";
+	public static final String REG_MOD_UNREGISTER_USER = "RegModUnRegisterUser";
 }
