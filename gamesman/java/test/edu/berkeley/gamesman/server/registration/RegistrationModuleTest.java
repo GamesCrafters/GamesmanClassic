5e598bd544427ed8aa80ffa8f36a8aef9e02f05e
djvicdaquik <djvicdaquik@a668be5d-ef11-cb4c-8bfd-e08cbb24f2ba>
2006-03-14 03:15:28 +0000

    added RegostrationModuleTest -vperez
    git-svn-id: https://gamescrafters.svn.sourceforge.net/svnroot/gamescrafters/GamesmanClassic/trunk@2319 a668be5d-ef11-cb4c-8bfd-e08cbb24f2ba

---
+++ gamesman/java/test/edu/berkeley/gamesman/server/registration/RegistrationModuleTest.java
@@ -0,0 +1,11 @@
+package edu.berkeley.gamesman.server.registration;
+
+import junit.framework.TestCase;
+
+public class RegistrationModuleTest extends TestCase {
+
+	public void test01()
+	{
+		
+	}
+}
