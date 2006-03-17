935b51431ad92048fd95961528c3ab964dfaffe1
ramesh_s <ramesh_s@a668be5d-ef11-cb4c-8bfd-e08cbb24f2ba>
2006-03-17 03:55:51 +0000

    CVS---------------------------------------------------------------------
    git-svn-id: https://gamescrafters.svn.sourceforge.net/svnroot/gamescrafters/GamesmanClassic/trunk@2332 a668be5d-ef11-cb4c-8bfd-e08cbb24f2ba

---
+++ gamesman/java/src/edu/berkeley/gamesman/server/p2p/P2PMacros.java
@@ -0,0 +1,6 @@
+package edu.berkeley.gamesman.server.p2p;
+
+public final class P2PMacros {
+	public static final int NO_SUCH_GAME = 1;
+	public static final int THREAD_INTERRUPTED = 2;
+}
--- gamesman/java/src/edu/berkeley/gamesman/server/p2p/P2PModule.java
+++ gamesman/java/src/edu/berkeley/gamesman/server/p2p/P2PModule.java
@@ -4,26 +4,135 @@ import edu.berkeley.gamesman.server.IModule;
 import edu.berkeley.gamesman.server.IModuleRequest;
 import edu.berkeley.gamesman.server.IModuleResponse;
 import edu.berkeley.gamesman.server.ModuleException;
+import java.util.Hashtable;
+import java.util.LinkedList;
 
+/**
+ * Handles messages passed between two gamesman clients.
+ * 
+ * @author Ramesh Sridharan
+ */
 public class P2PModule implements IModule
 {
-
-	public P2PModule()
+	public static final boolean DEBUGGING = false;
+	private static Hashtable theGames;
+		
+	/**
+	 * Wrapper class holding 2 strings.
+	 * 
+	 * @author ramesh
+	 */
+	static class ActiveGame {
+		private String player1;
+		private String player2;
+		//LinkedList moduleQueue; // stores 2 active instances for this game
+		ActiveGame(String p1, String p2) {
+			player1=p1;
+			player2=p2;
+			//moduleQueue = new LinkedList();
+		}
+		
+		public boolean equals(Object o) {
+			if(o.getClass() != this.getClass())  {
+				return false;
+			} else {
+				ActiveGame otherGame = (ActiveGame) o;
+				boolean sameOrder = this.player1.equals(otherGame.player1) &&
+									this.player2.equals(otherGame.player2);
+				boolean oppositeOrder = this.player1.equals(otherGame.player2) &&
+										this.player2.equals(otherGame.player1);
+				return sameOrder || oppositeOrder;
+				
+			}
+		}
+		
+		public String toString() {
+			return player1+"==with=="+player2;
+		}
+		
+		public int hashCode() {
+			return this.toString().hashCode();
+		}
+	}
+		
+	static void debugprint(String s) {
+		if(DEBUGGING) {
+			System.out.println(s);
+		}
+	}
+	
+	/**
+	 * Creates a game between user1 and user2 and stores it.
+	 * 
+	 * @param user1 First player
+	 * @param user2 Second player
+	 */
+	static void registerNewGame(String user1, String user2) 
+	{
+		LinkedList gameQueue = new LinkedList();
+		theGames.put(new ActiveGame(user1, user2), gameQueue);
+		debugprint("Added a game between "+user1+" and "+user2);
+		return;
+	}
+	
+	private IModuleResponse pendingPlayerResponse;
+	
+	public P2PModule() 
 	{
 		super();
-		// TODO Auto-generated constructor stub
+		
+		// TODO Fill this in if necessary?
 	}
-
+	
+	
 	public boolean typeSupported(String requestTypeName)
 	{
-		// TODO Auto-generated method stub
-		return false;
+		// TODO Fill in any other possible types?
+		return (requestTypeName == "SendMove");
 	}
 
-	public void handleRequest(IModuleRequest req, IModuleResponse res) throws ModuleException
+	public void handleRequest(IModuleRequest req, IModuleResponse res) throws ModuleException {
+		String sourcePlayer=req.getHeader("SourcePlayerName");
+		String destPlayer = req.getHeader("DestPlayerName");
+		ActiveGame theirGame = new ActiveGame(sourcePlayer, destPlayer);
+		
+		// TODO add cases for handling start and end of game
+		if(req.getHeader("Move")==null) 
+			;
+		
+		if(!theGames.containsKey(theirGame)) {
+			throw new ModuleException(P2PMacros.NO_SUCH_GAME, "No game between these players exists");
+			
+		} else {
+			//if(req.getHeader("Move")!=null) {
+				// Extract the player to send the info to
+				LinkedList gameQueue = (LinkedList) theGames.get(theirGame);
+				P2PModule pendingModule = (P2PModule) gameQueue.removeLast();
+				IModuleResponse pendingResponse = pendingModule.pendingPlayerResponse;
+			
+			// Set the response headers and send the info
+			pendingResponse.setHeader("SourcePlayerName", sourcePlayer);
+			pendingResponse.setHeader("DestPlayerName", destPlayer);
+			pendingResponse.setHeader("Move", req.getHeader("Move"));
+			debugprint("Send headers have been set...notifying module");
+			pendingModule.notify();
+			
+			// store this game, then wait until the next player is ready
+			gameQueue.addFirst(this);
+			this.pendingPlayerResponse=res;
+			try {
+				this.wait();
+			} catch(Exception e) {
+				throw new ModuleException(P2PMacros.THREAD_INTERRUPTED, "Game was interrupted", e);
+			}
+			
+			// Headers/etc have already been set, so terminate 
+		}
+	}
+	
+	static void finishGame(ActiveGame theGame)
 	{
-		// TODO Auto-generated method stub
-
+		theGames.remove(theGame);
+		debugprint("Game between "+theGame.player1+" and "+theGame.player2+" finished.");
 	}
-
 }
