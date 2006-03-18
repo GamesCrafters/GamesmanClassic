0d71180f70839bc0f11480a6e2d043bc7225e151
amitmatani <amitmatani@a668be5d-ef11-cb4c-8bfd-e08cbb24f2ba>
2006-03-18 18:33:09 +0000

    Changed gamesman servlet to add initialization functionality
    git-svn-id: https://gamescrafters.svn.sourceforge.net/svnroot/gamescrafters/GamesmanClassic/trunk@2340 a668be5d-ef11-cb4c-8bfd-e08cbb24f2ba

--- gamesman/java/src/edu/berkeley/gamesman/server/GamesmanServlet.java
+++ gamesman/java/src/edu/berkeley/gamesman/server/GamesmanServlet.java
@@ -5,106 +5,122 @@ import javax.servlet.*;
 import javax.servlet.http.*;
 
 /**
- * The Servlet that handles the requests from the client
- * and calls specific modules to implement them
- *
+ * The Servlet that handles the requests from the client and calls specific
+ * modules to implement them
+ * 
  * @author Amit Matani
  */
 
 public class GamesmanServlet extends HttpServlet {
-	
+
 	private IModule modulesArray[];
-	
-	//this is unneeded but kept just in case
-	private int IModuleLength;
-	
+
+	private boolean isServerAlive;
+
 	/**
-	 * Called only once when tomcat initializes the servlet.
-	 * The ServletConfig holds the parameter values set in the web.xml file
-	 * This will instantiate all of the modules and place them in the
-	 * modules array.  The class throws a servlet exception because
-	 * it deals with the web.xml file and not the clients
+	 * Called only once when tomcat initializes the servlet. The ServletConfig
+	 * holds the parameter values set in the web.xml file This will instantiate
+	 * all of the modules and place them in the modules array. The class throws
+	 * a servlet exception because it deals with the web.xml file and not the
+	 * clients
 	 * 
-	 * The web.xml file must have the following init-params:
-	 * imodules.lenghth
+	 * The web.xml file must have the following init-params: imodules.lenghth
 	 * imodules.list
-	 *
-	 * @param config config object of the web.xml file
-	 * @throws ServletException 
+	 * 
+	 * @param config
+	 *            config object of the web.xml file
+	 * @throws ServletException
 	 */
-	public void init(ServletConfig config)
-		throws ServletException 
-	{
-		String IModuleList;
-		String IModuleNameArray[];
-		
+	public void init(ServletConfig config) throws ServletException {
+		String iModuleName;
+		String iModuleArgs;
+		String iModuleArgsList[];
+		int iModuleLength;
+
 		try {
-			IModuleLength = Integer.parseInt(
-					config.getInitParameter("imodules.length"));
-		} catch(Exception e) {
+			iModuleLength = Integer.parseInt(config
+					.getInitParameter("imodules.length"));
+		} catch (Exception e) {
 			throw new ServletException("Bad imodules.length value");
 		}
-		if ((IModuleList = 
-			config.getInitParameter("imodules.list")) == null) {
-			throw new ServletException("imodules.list is unspecified");
+
+		modulesArray = new IModule[iModuleLength];
+
+		for (int i = 0; i < iModuleLength; i++) {
+			if ((iModuleName = config.getInitParameter("imodules." + i
+					+ ".classname")) == null)
+				throw new ServletException(
+						"imodule.length and list do not agree");
+			else {
+				try {
+					Class newClass = Class.forName(iModuleName);
+					modulesArray[i] = (IModule) newClass.newInstance();
+				} catch (Exception e) {
+					throw new ServletException("Bad Module Name");
+				}
+				if ((iModuleArgs = config.getInitParameter("imodules." + i
+						+ ".args")) != null) {
+					iModuleArgsList = iModuleArgs.split(",");
+				} else {
+					iModuleArgsList = new String[0];
+				}
+				try {
+					modulesArray[i].initialize(iModuleArgsList);
+				} catch (ModuleInitializationException e) {
+					System.out.println("Module Initialization Error:"
+							+ modulesArray.getClass().getName());
+				}
+			}
 		}
-		
-		IModuleNameArray = IModuleList.split("\\s+");
-		
-		modulesArray = new IModule[IModuleNameArray.length]; 
-		
-		//Instantiate Modules and place in object array
-		for (int i = 0; i < IModuleNameArray.length; i++) {
-			try {
-				Class newClass = Class.forName(IModuleNameArray[i]);
-				modulesArray[i] = (IModule) newClass.newInstance();
-			} catch (Exception e) {
-				throw new ServletException("Bad Module Name");
-			}	
-		}
-		
+		isServerAlive = true;
 	}
+
 	/**
-	 * The doPost function will respond to a post request from the
-	 * client.  The client will package the request and reponse in a
-	 * ModuleRequest and ModuleResponse and calls the appropriate module
-	 * to handle it.  It also handles servlet exceptions thrown from 
-	 * any of the modules and throws them back to the client
+	 * The doPost function will respond to a post request from the client. The
+	 * client will package the request and reponse in a ModuleRequest and
+	 * ModuleResponse and calls the appropriate module to handle it. It also
+	 * handles servlet exceptions thrown from any of the modules and throws them
+	 * back to the client
 	 * 
-	 * @param config config object of the web.xml file
-	 * @throws ServletException 
+	 * @param config
+	 *            config object of the web.xml file
+	 * @throws ServletException
 	 */
-	public void doPost(HttpServletRequest request,
-                      HttpServletResponse response)
-        throws IOException, ServletException
-    {
-    		ModuleRequest mRequest = new ModuleRequest(request);
-    		ModuleResponse mResponse = new ModuleResponse(response);
-    		int counter = 0;
-    		
-    		while (counter < modulesArray.length && !modulesArray[counter].typeSupported(
-    				mRequest.getHeader("type"))) {
-    			counter++;
-    		}
-    		if (counter < modulesArray.length) {
-    			try {
-    				modulesArray[counter].handleRequest(mRequest, mResponse);
-    			} catch (ModuleException e) {
-    				//send back error code
-    				mResponse.setHeader("error_code", String.valueOf(e.getCode()));
-    				mResponse.setHeader("error_message", e.getMessage());
-    			}
-    		}
-    		else {
-    			mResponse.setHeader("error_code", 
-    					String.valueOf(ModuleException.BAD_REQUEST_TYPE));
-    			mResponse.setHeader("error_message", "Bad Request Type");
-    		}
-    }
-	public void doGet (HttpServletRequest request,
-					HttpServletResponse response)
-		throws IOException, ServletException {
-		
+	public void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws IOException, ServletException {
+		ModuleRequest mRequest = new ModuleRequest(request);
+		ModuleResponse mResponse = new ModuleResponse(response);
+		int counter = 0;
+		if (isServerAlive) {
+			while (counter < modulesArray.length
+					&& !modulesArray[counter].typeSupported(mRequest
+							.getHeader("type"))) {
+				counter++;
+			}
+			if (counter < modulesArray.length) {
+				try {
+					modulesArray[counter].handleRequest(mRequest, mResponse);
+				} catch (ModuleException e) {
+					// send back error code
+					mResponse.setHeader("error_code", String.valueOf(e
+							.getCode()));
+					mResponse.setHeader("error_message", e.getMessage());
+				}
+			} else {
+				mResponse.setHeader("error_code", String
+						.valueOf(ModuleException.BAD_REQUEST_TYPE));
+				mResponse.setHeader("error_message", "Bad Request Type");
+			}
+		} else {
+			mResponse.setHeader("error_code", String
+					.valueOf(ModuleException.BAD_REQUEST_TYPE));
+			mResponse.setHeader("error_message", "Server is Down");
+		}
+	}
+
+	public void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws IOException, ServletException {
+
 		return;
 	}
 }
