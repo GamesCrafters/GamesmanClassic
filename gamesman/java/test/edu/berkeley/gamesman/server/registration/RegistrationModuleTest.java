ae1b4f33a89cce45bc7a8222933167d77017545a
djvicdaquik <djvicdaquik@a668be5d-ef11-cb4c-8bfd-e08cbb24f2ba>
2006-03-18 12:13:17 +0000

    Begining Testing suite. (Register User) -vperez
    git-svn-id: https://gamescrafters.svn.sourceforge.net/svnroot/gamescrafters/GamesmanClassic/trunk@2338 a668be5d-ef11-cb4c-8bfd-e08cbb24f2ba

--- gamesman/java/test/edu/berkeley/gamesman/server/registration/RegistrationModuleTest.java
+++ gamesman/java/test/edu/berkeley/gamesman/server/registration/RegistrationModuleTest.java
@@ -1,11 +1,55 @@
 package edu.berkeley.gamesman.server.registration;
-
+import edu.berkeley.gamesman.server.ModuleException;
 import junit.framework.TestCase;
+import edu.berkeley.gamesman.server.*;
 
+/**
+ * 
+ * @author vperez
+ *
+ */
 public class RegistrationModuleTest extends TestCase {
-
-	public void test01()
-	{
+	private RegistrationModule regMod;
+	private TestModuleRequest tReq;
+	private TestModuleResponse tRes;
+	
+	private byte [] input;
+	private int INPUT_BYTE_ARR_SIZE = 80;
+	private int OUTPUT_SIZE = 1024;
+	
+	private String [] headerNames, headerValues;
+	private int STRING_ARR_SIZE = 20;
+	
+	private String user = "user";
+	private String game = "mancala";
+	
+	private void initialize() {
+		input = new byte[INPUT_BYTE_ARR_SIZE];
+		headerNames = new String[STRING_ARR_SIZE];
+		headerValues = new String[STRING_ARR_SIZE];
+	}
+	
+	/**
+	 * 
+	 * @throws ModuleException
+	 */
+	public void test01() throws ModuleException {
+		regMod = new RegistrationModule();
 		
+		String [] headerNames  = {Macros.TYPE, Macros.NAME, Macros.GAME};
+		String [] headerValues = {Macros.REG_MOD_REGISTER_USER, user, game};
+		//don't actually need to use input stream
+		input = new byte[0];
+		
+		tReq = new TestModuleRequest(input,headerNames,headerValues);
+		tRes = new TestModuleResponse(OUTPUT_SIZE);
+		
+		regMod.registerUser(tReq, tRes);
+		
+		
+	}
+	
+	public void test02() {
+		System.out.println("another test");
 	}
 }
