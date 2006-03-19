ceecb80e3fb2b874c1832ae38f9461266ea545f3
albertchae <albertchae@a668be5d-ef11-cb4c-8bfd-e08cbb24f2ba>
2006-03-19 06:21:27 +0000

    DoMove() works somewhat. Able to play human v human except for a bug with alternating player's move and challenger's move.
    git-svn-id: https://gamescrafters.svn.sourceforge.net/svnroot/gamescrafters/GamesmanClassic/trunk@2349 a668be5d-ef11-cb4c-8bfd-e08cbb24f2ba

--- gamesman/src/mcambio.c
+++ gamesman/src/mcambio.c
@@ -1,4 +1,4 @@
-// $Id: mcambio.c,v 1.17 2006-03-18 10:36:35 albertchae Exp $
+// $Id: mcambio.c,v 1.18 2006-03-19 06:21:27 albertchae Exp $
 
 /*
  * The above lines will include the name and log of the last person
@@ -25,7 +25,8 @@
 **                                    Removed blanks from the game.
 **             3/14/2006 - Reducing game to 4x4 to see if that fixes a hash problem.
 **             3/16/2006 - Changed board printout. Made GetInitialPosition check inputs.
-**             3/18/2006 - Added GenerateMove() and PrintMove()
+**             3/18/2006 - Added GenerateMove() and PrintMove() and ValidTextInput() and ConvertTextInputToMove().
+**                                    Also added DoMove(). Some bugs left to work out with involving the alternation of moves etc.
 **
 **************************************************************************/
 
@@ -312,11 +313,12 @@ MOVELIST *GenerateMoves (POSITION position)
 		/* left column moves */
 		for(i = 0; i < rowcount; i++)
 		{
-			if(gBoard[((i+1)*colcount) - 1] != opposymbol) // should be 15, 11, 7, 3
+			if(gBoard[((colcount - i)*colcount) - 1] != opposymbol) // should be 15, 11, 7, 3
 				moves = CreateMovelistNode(i + boardSize + colcount + rowcount + colcount, moves); // should be 28-31 for 4x4
 		}
 	}
 	
+	free(gBoard);
     return moves;
 }
 
@@ -339,7 +341,95 @@ MOVELIST *GenerateMoves (POSITION position)
 
 POSITION DoMove (POSITION position, MOVE move)
 {
-    return 0;
+	char symbol, opposymbol;
+	char *gBoard = (char *) malloc(boardSize*sizeof(char));
+	int turn, countA, countB, i;
+	int pushon, pushoff;
+	POSITION positionAfterMove;
+	
+	generic_unhash(position, gBoard);
+	turn = whoseMove(position);
+	countA = 0;
+	countB = 0;
+    
+	/* Assign pieces for each player */
+	if(turn == playerA) {
+		symbol = aPiece;
+		opposymbol = bPiece;
+	}
+	else {
+		symbol = bPiece;
+		opposymbol = aPiece;
+	}
+	
+	/* count the number of pieces for each player */
+	for(i = 0; i < boardSize; i++)
+	{
+		if(gBoard[i] == aPiece)
+			countA++;
+		else if(gBoard[i] == bPiece)
+			countB++;
+	}
+	
+	if(move < boardSize)
+		gBoard[move] = opposymbol;
+	else 
+	{
+		if (move < boardSize + colcount)
+		{
+			pushon = (move - boardSize);
+			pushoff = (move - colcount);
+			gBoard[pushoff] = gBoard[pushoff-colcount];
+			gBoard[pushoff-colcount] = gBoard[pushoff-colcount*2];
+			gBoard[pushoff-colcount*2] = gBoard[pushon];
+			gBoard[pushon] = symbol;
+		}
+		else if (move < boardSize + colcount + rowcount)
+		{
+			pushon = (move - boardSize - colcount + 1)*colcount - 1;
+			pushoff = (move - boardSize - colcount)*colcount;
+			gBoard[pushoff] = gBoard[pushoff+1];
+			gBoard[pushoff+1] = gBoard[pushoff+2];
+			gBoard[pushoff+2] = gBoard[pushon];
+			gBoard[pushon] = symbol;
+		}
+		else if (move < boardSize + colcount + rowcount + colcount)
+		{
+			pushon = boardSize - (move - boardSize - colcount - rowcount + 1);
+			pushoff = colcount - (move - boardSize - colcount - rowcount) - 1;
+			gBoard[pushoff] = gBoard[pushoff+colcount];
+			gBoard[pushoff+colcount] = gBoard[pushoff+colcount*2];
+			gBoard[pushoff+colcount*2] = gBoard[pushon];
+			gBoard[pushon] = symbol;
+		}
+		else if (move < boardSize + colcount + rowcount + colcount + rowcount)
+		{
+			pushon = boardSize - (move - boardSize - colcount*2 - rowcount + 1)*colcount;
+			pushoff = boardSize - (move - boardSize - colcount*2 - rowcount + 1)*colcount + colcount - 1;
+			gBoard[pushoff] = gBoard[pushoff-1];
+			gBoard[pushoff-1] = gBoard[pushoff-2];
+			gBoard[pushoff-2] = gBoard[pushon];
+			gBoard[pushon] = symbol;
+		}	
+	}	
+
+	/* change turns */
+	if(countB == 3)
+		turn = playerB;
+	else if(countA == 2)
+		turn = playerA;
+	else if((countB>=4) && (countA>=3))
+	{
+		if(turn == playerA)
+			turn = playerB;
+		else if(turn == playerB)
+			turn = playerA;
+	}
+	
+	positionAfterMove = generic_hash(gBoard, turn);
+	free(gBoard);
+	
+	return positionAfterMove;
 }
 
 
@@ -428,14 +518,15 @@ void PrintPosition (POSITION position, STRING playersName, BOOLEAN usersTurn)
   generic_unhash(position, gBoard);
   
   /***********************LINE 1**************************/
-  printf("       ---------\n");
+  printf("       ---------");
+  printf("            16 17 18 19\n");
 
   /***********************LINE 2**************************/
   printf("       |");
   for (; i < colcount; i++) {
     printf("%c|", gBoard[i]);
   }
-  printf("          ( 0  1  2  3 )\n");
+  printf("       31 ( 0  1  2  3 ) 20\n");
 
   /***********************LINE 3**************************/
   printf("       ---------\n"); 
@@ -445,7 +536,7 @@ void PrintPosition (POSITION position, STRING playersName, BOOLEAN usersTurn)
   for (; i < colcount*2; i++) {
     printf("%c|", gBoard[i]);
   }
-  printf("          ( 4  5  6  7 )\n");
+  printf("       30 ( 4  5  6  7 ) 21\n");
 
   /***********************LINE 5**************************/
   printf("       ---------\n");                
@@ -456,7 +547,7 @@ void PrintPosition (POSITION position, STRING playersName, BOOLEAN usersTurn)
   for (; i < colcount*3; i++) {
     printf("%c|", gBoard[i]);
   }
-  printf("  LEGEND: ( 8  9  10 11)\n");
+  printf("LEGEND:29 ( 8  9  10 11) 22\n");
 
   /***********************LINE 7**************************/
   printf("       ---------\n"); 
@@ -466,11 +557,12 @@ void PrintPosition (POSITION position, STRING playersName, BOOLEAN usersTurn)
   for (; i < colcount*4; i++) {
     printf("%c|", gBoard[i]);
   }
-  printf("          ( 12 13 14 15)\n");
+  printf("       28 ( 12 13 14 15) 23\n");
 
   /***********************LINE 9**************************/
-  printf("       ---------\n");
-
+  printf("       ---------");
+  printf("            27 26 25 24\n");
+  
   /***********************LINE 10**************************/
   printf("                      A's Symbol = %c\n", aPiece);
   printf("                      B's Symbol = %c\n", bPiece);
@@ -510,16 +602,29 @@ void PrintComputersMove (MOVE computersMove, STRING computersName)
 void PrintMove (MOVE move)
 /* must check all the math used for general case, such as 5x5 and on */
 {
-    if(move < boardSize)
-		printf("Place a piece at %i.", move);
+/*    if(move < boardSize)
+		printf("(%i) Place a piece at %i.", move, move);
 	else if (move < boardSize + colcount)
-		printf("Push on %i and discard %i", (move - boardSize), (move - colcount));
+		printf("(%i) Push on %i and discard %i", move, (move - boardSize), (move - colcount));
 	else if (move < boardSize + colcount + rowcount)
-		printf("Push on %i and discard %i", (move - boardSize - colcount + 1)*colcount - 1, (move - boardSize - colcount)*colcount);
+		printf("(%i) Push on %i and discard %i", move, (move - boardSize - colcount + 1)*colcount - 1, (move - boardSize - colcount)*colcount);
 	else if (move < boardSize + colcount + rowcount + colcount)
-		printf("Push on %i and discard %i", boardSize - (move - boardSize - colcount - rowcount + 1), colcount - (move - boardSize - colcount - rowcount) - 1);
+		printf("(%i) Push on %i and discard %i", move, boardSize - (move - boardSize - colcount - rowcount + 1), colcount - (move - boardSize - colcount - rowcount) - 1);
 	else if (move < boardSize + colcount + rowcount + colcount + rowcount)
-		printf("Push on %i and discard %i", boardSize - (move - boardSize - colcount*2 - rowcount + 1)*colcount, boardSize - (move - boardSize - colcount*2 - rowcount + 1)*colcount + colcount - 1);
+		printf("(%i) Push on %i and discard %i", move, boardSize - (move - boardSize - colcount*2 - rowcount + 1)*colcount, boardSize - (move - boardSize - colcount*2 - rowcount + 1)*colcount + colcount - 1);
+*/
+	if(move < boardSize)
+		printf("%i", move);
+	else if (move < boardSize + colcount)
+		printf("%i", move);
+	else if (move < boardSize + colcount + rowcount)
+		printf("%i", move);
+	else if (move < boardSize + colcount + rowcount + colcount)
+		printf("%i", move);
+	else if (move < boardSize + colcount + rowcount + colcount + rowcount)
+		printf("%i", move);
+		
+
 }
 
 
@@ -552,7 +657,7 @@ USERINPUT GetAndPrintPlayersMove (POSITION position, MOVE *move, STRING playersN
         /***********************************************************
          * CHANGE THE LINE BELOW TO MATCH YOUR MOVE FORMAT
          ***********************************************************/
-	printf("%8s's move [(undo)/(MOVE FORMAT)] : ", playersName);
+	printf("%8s's move [(undo)/1-31] : ", playersName);
 	
 	input = HandleDefaultTextInput(position, move, playersName);
 	
@@ -592,7 +697,27 @@ USERINPUT GetAndPrintPlayersMove (POSITION position, MOVE *move, STRING playersN
 
 BOOLEAN ValidTextInput (STRING input)
 {
-    return FALSE;
+	int i;
+	
+	if(strlen(input) > 2)
+		return FALSE;
+	
+	for(i = 0; i < strlen(input); i++)
+	{
+		if(!((input[i] == '0') ||
+		     (input[i] == '1') ||
+		     (input[i] == '2') ||
+		     (input[i] == '3') ||
+		     (input[i] == '4') ||
+		     (input[i] == '5') ||
+		     (input[i] == '6') ||
+		     (input[i] == '7') ||
+		     (input[i] == '8') ||
+		     (input[i] == '9')))
+		     return FALSE;
+	}
+	
+    return TRUE;
 }
 
 
@@ -612,7 +737,20 @@ BOOLEAN ValidTextInput (STRING input)
 
 MOVE ConvertTextInputToMove (STRING input)
 {
-    return 0;
+	int move, i, limit; // limit is part of a really ugly hack. fix this for reals later.
+	
+	move = 0;
+	
+	if(strlen(input) == 1)
+		limit = 1;
+	else limit = strlen(input) - 1;
+	
+	for(i = 0; i < limit; i++) //for some reason the last character gets duplicated??
+	{	
+		move = move * 10;
+		move = move + atoi(&input[i]);		
+	}
+    return (MOVE) move;
 }
 
 
@@ -809,6 +947,11 @@ BOOLEAN FourInARow(char *board, char symbol)
 
 
 // $Log: not supported by cvs2svn $
+// Revision 1.17  2006/03/18 10:36:35  albertchae
+// Wrote the code for GenerateMoves and PrintMoves. Tried to keep
+// the logic general in case we have variant board sizes. Still needs some
+// testing.
+//
 // Revision 1.16  2006/03/14 12:38:29  albertchae
 // Minor edit. Changed help strings to reflect 4x4. Added some ideas for
 // generate move.
