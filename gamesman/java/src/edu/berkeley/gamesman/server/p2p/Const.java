ac807b9d0fb17e4de9649017804d213023b8bbed
ramesh_s <ramesh_s@a668be5d-ef11-cb4c-8bfd-e08cbb24f2ba>
2006-03-20 00:52:23 +0000

    Added Files:  	Const.java
    git-svn-id: https://gamescrafters.svn.sourceforge.net/svnroot/gamescrafters/GamesmanClassic/trunk@2362 a668be5d-ef11-cb4c-8bfd-e08cbb24f2ba

---
+++ gamesman/java/src/edu/berkeley/gamesman/server/p2p/Const.java
@@ -0,0 +1,13 @@
+package edu.berkeley.gamesman.server.p2p;
+
+public final class Const {
+	public static final int NO_SUCH_GAME = 1;
+	public static final int THREAD_INTERRUPTED = 2;
+	public static final String SEND_MOVE = "SendMove";
+	public static final String SOURCE_PLAYER = "SrcPlayer";
+	public static final String DESTINATION_PLAYER = "DestPlayer";
+	public static final String MOVE_VALUE = "Move";
+	public static final String END_OF_GAME = "GameOver";
+	
+	public static final boolean DEBUGGING = true;
+}
