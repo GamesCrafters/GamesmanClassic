31364eb99a8c72ae9be3c8be23440737ff3dd87e
dmchan <dmchan@a668be5d-ef11-cb4c-8bfd-e08cbb24f2ba>
2006-05-22 22:48:09 +0000

    added comments
    git-svn-id: https://gamescrafters.svn.sourceforge.net/svnroot/gamescrafters/GamesmanClassic/trunk@2645 a668be5d-ef11-cb4c-8bfd-e08cbb24f2ba

--- gamesman/tcl/msim.tcl
+++ gamesman/tcl/msim.tcl
@@ -17,10 +17,13 @@
 ## 03-19-06 David Chan
 ##                 - added scaling, code shamelessly ripped from Eudean
 ##                   and game over screen, from Eudean
+## 05-23-06 David Chan
+##                 - comments added
 #############################################################################
 
 
 # global variables
+# power of 3s used to unhash
 set g3Array [list 1 3 9 27 81 243 729 2187 6561 19683 59049 177147 531441 1594323 4782969]
 
 set edges 15
@@ -34,11 +37,19 @@ set vertices 6
 set CANVAS_HEIGHT 1
 set CANVAS_WIDTH 1
 set aval(0) false
+#not sure if I need some of these variables, will clean up later
+#dont want to break code
 set gameover true
 set prevColor $hColor
 set dashSize 1
 set lastMove 0
 
+##############################################################################
+# given a board position
+# unhashes the board
+# places the correct pieces into an
+# array to be handled
+#
 ##############################################################################
 proc unhash {pos array} {
     global edges
@@ -67,8 +78,16 @@ proc min { a b } {
     return $b
 }
 
-
 #############################################################################
+# GS_InitGameSpecific sets characteristics of the game that
+# are inherent to the game, unalterable.  You can use this fucntion
+# to initialize data structures, but not to present any graphics.
+# It is called FIRST, ONCE, only when the player
+# starts playing your game, and before the player hits "New Game"
+# At the very least, you must set the global variables kGameName
+# and gInitialPosition in this function.
+#############################################################################
+
 proc GS_InitGameSpecific {} {
     
     ### Set the name of the game
@@ -114,6 +133,14 @@ proc GS_InitGameSpecific {} {
 }
 
 #############################################################################
+# GS_NameOfPieces should return a list of 2 strings that represent
+# your names for the "pieces".  If your game is some pathalogical game
+# with no concept of a "piece", give a name to the game's sides.
+# if the game is tic tac toe, this might be a single line: return [list x o]
+# This function is called FIRST, ONCE, only when the player
+# starts playing the game, and before he hits "New Game"
+#############################################################################
+
 proc GS_NameOfPieces {} {
 
     return [list x o]
@@ -121,13 +148,42 @@ proc GS_NameOfPieces {} {
 }
 
 #############################################################################
-proc GS_ColorOfPlayers {} {
+# GS_ColorOfPlayers should return a list of two strings, 
+# each representing the color of a player.
+# If a specific color appears uniquely on one player's pieces,
+# it might be a good choice for that player's color.
+# In impartial games, both players may share the same color.
+# If the game is tic tac toe, this might be the line 
+# return [list blue red]
+# If the game is nim, this might be the line
+# return [list green green]
+# This function is called FIRST, ONCE, only when the player
+# starts playing the game, and before he clicks "New Game"
+# The left player's color should be the first item in the list.
+# The right player's color should be second.
+#############################################################################
 
-    return [list blue red]
+proc GS_ColorOfPlayers {} {
+    global p1Color, p2Color
+    return [list $p1Color $p2Color]
     
 }
 
 #############################################################################
+# GS_SetupRulesFrame sets up the rules frame;
+# Adds widgets to the rules frame that will allow the user to 
+# select the variant of this game to play. The options 
+# selected by the user should be stored in a set of global
+# variables.
+# This procedure must initialize the global variables to some
+# valid game variant.
+# The rules frame must include a standard/misere setting.
+# Args: rulesFrame (Frame) - The rules frame to which widgets
+# should be added
+# Modifies: the rules frame and its global variables
+# Returns: nothing
+#############################################################################
+
 proc GS_SetupRulesFrame { rulesFrame } {
 
     set standardRule \
@@ -165,6 +221,15 @@ proc GS_SetupRulesFrame { rulesFrame } {
 }
 
 #############################################################################
+# GS_GetOption gets the game option specified by the rules frame
+# Returns the option of the variant of the game specified by the 
+# global variables used by the rules frame
+# Args: none
+# Modifies: nothing
+# Returns: option (Integer) - the option of the game as specified by 
+# getOption and setOption in the module's C code
+#############################################################################
+
 proc GS_GetOption { } {
     # TODO: Needs to change with more variants
     global gMisereGame
@@ -174,6 +239,17 @@ proc GS_GetOption { } {
 }
 
 #############################################################################
+# GS_SetOption modifies the rules frame to match the given options
+# Modifies the global variables used by the rules frame to match the 
+# given game option.
+# This procedure only needs to support options that can be selected 
+# using the rules frame.
+# Args: option (Integer) -  the option of the game as specified by 
+# getOption and setOption in the module's C code
+# Modifies: the global variables used by the rules frame
+# Returns: nothing
+#############################################################################
+
 proc GS_SetOption { option } {
     # TODO: Needs to change with more variants
     global gMisereGame
@@ -182,6 +258,14 @@ proc GS_SetOption { option } {
 }
 
 #############################################################################
+# GS_Initialize is where you can start drawing graphics.  
+# Its argument, c, is a canvas.  Please draw only in this canvas.
+# You could put an opening animation in this function that introduces the game
+# or just draw an empty board.
+# This function is called ONCE after GS_InitGameSpecific, and before the
+# player hits "New Game"
+#############################################################################
+
 proc GS_Initialize { c } {
 
     # you may want to start by setting the size of the canvas; this line isn't cecessary
@@ -227,11 +311,28 @@ proc GS_Initialize { c } {
 
 
 #############################################################################
+# GS_Deinitialize deletes everything in the playing canvas.  I'm not sure why this
+# is here, so whoever put this here should update this.  -Jeff
+#############################################################################
+
 proc GS_Deinitialize { c } {
     $c delete all
 }
 
 #############################################################################
+# GS_DrawPosition this draws the board in an arbitrary position.
+# It's arguments are a canvas, c, where you should draw and the
+# (hashed) position.  For example, if your game is a rearranger,
+# here is where you would draw pieces on the board in their correct positions.
+# Imagine that this function is called when the player
+# loads a saved game, and you must quickly return the board to its saved
+# state.  It probably shouldn't animate, but it can if you want.
+#
+# BY THE WAY: Before you go any further, I recommend writing a tcl function that 
+# UNhashes You'll thank yourself later.
+# Don't bother writing tcl that hashes, that's never necessary.
+#############################################################################
+
 proc GS_DrawPosition { c position } {
     global edges
     global lineColor
@@ -257,6 +358,13 @@ proc GS_DrawPosition { c position } {
 }
 
 #############################################################################
+# GS_NewGame should start playing the game. 
+# It's arguments are a canvas, c, where you should draw 
+# the hashed starting position of the game.
+# This is called just when the player hits "New Game"
+# and before any moves are made.
+#############################################################################
+
 proc GS_NewGame { c position } {
     global gameover
     if { $gameover == true } {
@@ -267,6 +375,12 @@ proc GS_NewGame { c position } {
 }
 
 #############################################################################
+# GS_WhoseMove takes a position and returns whose move it is.
+# Your return value should be one of the items in the list returned
+# by GS_NameOfPieces.
+# This function is called just before every move.
+#############################################################################
+
 proc GS_WhoseMove { position } {
     # code copied from mttt3.tcl
     global edges
@@ -287,6 +401,11 @@ proc GS_WhoseMove { position } {
 	return $val
 }
 
+#########################################################################
+# used for animating lines, incomplete as of 5-22-06
+#
+#
+########################################################################
 proc animateLine { c  i } {
     global CANVAS_WIDTH
     global vertices
@@ -320,6 +439,16 @@ proc animateLine { c  i } {
 }
 
 #############################################################################
+# GS_HandleMove draws (animates) a move being made.
+# The user or the computer has just made a move, animate it or draw it
+# or whatever.  Draw the piece moving if your game is a rearranger, or
+# the piece appearing if it's a "dart board"
+#
+# By the way, if you animate, a function that will be useful for you is
+# update idletasks.  You can call this to force the canvas to update if
+# you make changes before tcl enters the event loop again.
+#############################################################################
+
 proc GS_HandleMove { c oldPosition theMove newPosition } {
     global edges
     global p1Color
@@ -334,9 +463,11 @@ proc GS_HandleMove { c oldPosition theMove newPosition } {
     unhash $oldPosition b
     unhash $newPosition a
 
+    # hack used to show last move done
     $c itemconfigure base$lastMove -state normal
     set lastMove $theMove
 
+    #iterate through the piece array setting the pieces
     for {set i 0} {$i < $edges} {incr i} {
 	if { $a($i) != $b($i) }  {
 	    set aval($i) false
@@ -351,6 +482,15 @@ proc GS_HandleMove { c oldPosition theMove newPosition } {
 	    }
 	}
     }
+
+    # toggle state of the recent move
+    # if recent move is now a blank spot, we had an undo
+    # else we highlight the move by enabling the disabled dash
+    # note that on an undo we do not know what was the last made move
+    # so we can not highlight it... fix if you know how
+    # there has to be an array of moves that gamesman uses for undos
+    # but I do not know what it is
+
     if {[$c itemcget base$lastMove -fill] == $lineColor} {
 	$c itemconfigure base$lastMove -state normal
     } else {
@@ -359,6 +499,21 @@ proc GS_HandleMove { c oldPosition theMove newPosition } {
 }
 
 #############################################################################
+# GS_ShowMoves draws the move indicator (be it an arrow or a dot, whatever the
+# player clicks to make the move)  It is also the function that handles coloring
+# of the moves according to value. It is called by gamesman just before the player
+# is prompted for a move.
+#
+# Arguments:
+# c = the canvas to draw in as usual
+# moveType = a string which is either value, moves or best according to which radio button is down
+# position = the current hashed position
+# moveList = a list of lists.  Each list contains a move and its value.
+# These moves are represented as numbers (same as in C)
+# The value will be either "Win" "Lose" or "Tie"
+# Example:  moveList: { 73 Win } { 158 Lose } { 22 Tie } 
+#############################################################################
+
 proc GS_ShowMoves { c moveType position moveList } {
     foreach item $moveList {
 	set move [lindex $item 0]
@@ -379,20 +534,46 @@ proc GS_ShowMoves { c moveType position moveList } {
 }
 
 #############################################################################
+# GS_HideMoves erases the moves drawn by GS_ShowMoves.  It's arguments are the 
+# same as GS_ShowMoves.
+# You might not use all the arguments, and that's okay.
+#############################################################################
+
 proc GS_HideMoves { c moveType position moveList} {
     GS_DrawPosition $c $position
 }
 
 #############################################################################
+# GS_HandleUndo handles undoing a move (possibly with animation)
+# Here's the undo logic
+# The game was in position A, a player makes move M bringing the game to position B
+# then an undo is called
+# currentPosition is the B
+# theMoveToUndo is the M
+# positionAfterUndo is the A
+#
+# By default this function just calls GS_DrawPosition, but you certainly don't 
+# need to keep that.
+#############################################################################
+
 proc GS_HandleUndo { c currentPosition theMoveToUndo positionAfterUndo} {
     GS_HandleMove $c $currentPosition $theMoveToUndo $positionAfterUndo
 }
 
 #############################################################################
+# GS_GetGameSpecificOptions is not quite ready, don't worry about it .
+#############################################################################
+
 proc GS_GetGameSpecificOptions { } {
 }
 
 #############################################################################
+# GS_GameOver is called the moment the game is finished ( won, lost or tied)
+# You could use this function to draw the line striking out the winning row in 
+# tic tac toe for instance.  Or, you could congratulate the winner.
+# Or, do nothing.
+#############################################################################
+
 proc GS_GameOver { c position gameValue nameOfWinningPiece nameOfWinner lastMove} {
     global CANVAS_WIDTH
     set size $CANVAS_WIDTH
@@ -407,6 +588,16 @@ proc GS_GameOver { c position gameValue nameOfWinningPiece nameOfWinner lastMove
 }
 
 #############################################################################
+# GS_UndoGameOver is called when the player hits undo after the game is finished.
+# This is provided so that you may undo the drawing you did in GS_GameOver if you 
+# drew something.
+# For instance, if you drew a line crossing out the winning row in tic tac toe, 
+# this is where you sould delete the line.
+#
+# note: GS_HandleUndo is called regardless of whether the move undoes the end of the 
+# game, so IF you choose to do nothing in GS_GameOver, you needn't do anything here either.
+#############################################################################
+
 proc GS_UndoGameOver { c position } {
     global gameover
     set gameover false
