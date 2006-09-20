9a9205d7f550d7d9205e0b61c5ab2ada16b1590c
mgreenbaum <mgreenbaum@a668be5d-ef11-cb4c-8bfd-e08cbb24f2ba>
2006-09-20 02:41:36 +0000

    *** empty log message ***
    git-svn-id: https://gamescrafters.svn.sourceforge.net/svnroot/gamescrafters/GamesmanClassic/trunk@2776 a668be5d-ef11-cb4c-8bfd-e08cbb24f2ba

--- gamesman/src/core/seval.c
+++ gamesman/src/core/seval.c
@@ -10,6 +10,31 @@
 ** DATE:        Start: 19 September 2006
 **
 ** UPDATE HIST: 
-**              9/19/06- Initial drafts of seval.
+**              9/19/06- Initial draft of seval.
 **              
 **************************************************************************/
+#include <stdio.h>
+#include gamesman.h
+#include string.h
+
+/*lBoard will contain board processing data. Requires call
+  of LibInitialize by the module. For information see mlib.c and mlib.h.*/
+#include mlib.h
+
+/************************************************************************
+**
+** Implementation of traits starts here.
+**
+** Current list:
+**
+** -Number of pieces
+** -Connections (how many pieces "see" each other)
+** -NinaRow, AlmostNinaRow.
+** -Side locality.
+** -Grouping, clustering.
+** -Weighting of pieces for above traits.
+** -Piece centric traits- distance to piece, connections to piece
+** -Slot centric traits- distance, connections, number on slots.
+** -Remaining moves (requires module info)
+** 
+************************************************************************/
