6a92462872345354778dad0f583445233f129fab
mgreenbaum <mgreenbaum@a668be5d-ef11-cb4c-8bfd-e08cbb24f2ba>
2006-09-20 00:22:53 +0000

    *** empty log message ***
    git-svn-id: https://gamescrafters.svn.sourceforge.net/svnroot/gamescrafters/GamesmanClassic/trunk@2775 a668be5d-ef11-cb4c-8bfd-e08cbb24f2ba

---
+++ gamesman/src/core/seval.c
@@ -0,0 +1,15 @@
+ /************************************************************************
+**
+** NAME:        seval.c
+**
+** DESCRIPTION: Static evaluator to be used with gamesman modules.
+**
+** AUTHOR:      Michael Greenbaum
+**              Brian Zimmer
+**
+** DATE:        Start: 19 September 2006
+**
+** UPDATE HIST: 
+**              9/19/06- Initial drafts of seval.
+**              
+**************************************************************************/
