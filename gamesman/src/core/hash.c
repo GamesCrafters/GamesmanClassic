17e3a5e316a583ec9a14ac231fc9cf795d47c446
max817 <max817@a668be5d-ef11-cb4c-8bfd-e08cbb24f2ba>
2006-10-17 23:49:19 +0000

    *** empty log message ***
    git-svn-id: https://gamescrafters.svn.sourceforge.net/svnroot/gamescrafters/GamesmanClassic/trunk@2875 a668be5d-ef11-cb4c-8bfd-e08cbb24f2ba

--- gamesman/src/core/hash.c
+++ gamesman/src/core/hash.c
@@ -642,7 +642,7 @@ void generic_hash_custom_contexts_mode(BOOLEAN on)
 void generic_hash_set_context(int context)
 {
 	if (context < 0)
-		ErrorStageRightErrorString("Attempting to set a negative custom context");
+		ExitStageRightErrorString("Attempting to set a negative custom context");
 	cCon->contextNumber = context;
 }
 
