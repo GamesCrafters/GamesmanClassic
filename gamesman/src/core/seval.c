d44b7fd14ed9720057b03f53e0dfa15bd628084a
mgreenbaum <mgreenbaum@a668be5d-ef11-cb4c-8bfd-e08cbb24f2ba>
2006-11-28 11:21:50 +0000

    *** empty log message ***
    git-svn-id: https://gamescrafters.svn.sourceforge.net/svnroot/gamescrafters/GamesmanClassic/trunk@2929 a668be5d-ef11-cb4c-8bfd-e08cbb24f2ba

--- gamesman/src/core/seval.c
+++ gamesman/src/core/seval.c
@@ -12,11 +12,14 @@
 ** UPDATE HIST: 
 **              9/19/06- Initial draft of seval.
 **              10/8/06- (Michael) Implemented 4 trait functions.
+**              11/10/06- (Michael) Implemented skeleton of data structures
+**                        and core functions.
 **              
 **************************************************************************/
 #include <stdio.h>
+#include <string.h>
+#include <math.h>
 #include gamesman.h
-#include string.h
 
 /*lBoard will contain board processing data. Requires call
   of LibInitialize by the module. For information see mlib.c and mlib.h.*/
@@ -29,6 +32,97 @@
 #define getRow(slot) (slot/lBoard.numCols)
 #define getCol(slot) (slot%lBoard.numRows)
 #define min(a,b) (a<=b ? a : b)
+
+#define NUMTRAITS 4
+#define ERROR -2
+
+int fuck = 1;
+
+int AdditionalParamList[NUMTRAITS] = {1, 2, 1, 3};
+char* TraitNames[NUMTRAITS] = {"Number of Pieces","Localization","Clustering","Connections"};
+
+/**************************************************************************
+**
+** Core functions
+**              
+**************************************************************************/
+
+void SEvalInitialize() {
+  
+}
+
+//Applies the given static evaluator instance to the given board
+int Evaluate(EVALUATOR e, void* board) {
+  double sum=0.0;
+  double raw, scaled;
+  int i,a,b,weightSum=0;
+  TRAITLIST traits = e->myTraits;
+  TRAIT current;
+
+  if (e->numTraits==0) {
+    printf("ERROR, static evaluator has no defined traits");
+    return ERROR;
+  }
+  
+  for(i=0;i< e->numTraits ,i++) {
+    current = traits;
+
+    //This part should be implemented later with function pointers
+    switch(current->id) {
+    case 0:
+      raw = (double) numPieces(board,(current->additionalParams)[0]);
+      break;
+    case 1:
+      raw = (double) numFromEdge(board,(current->additionalParams)[0],(current->additionalParams)[1]);
+      break;
+    case 2:
+      raw = clustering(board,(current->additionalParams)[0]);
+      break;
+    case 3:
+      raw = connections(board,(current->additionalParams)[0],(current->additionalParams)[1],(current->additionalParams)[2]);
+      break;
+    default:
+      printf("ERROR, unrecognized trait");
+      return ERROR;
+    }
+
+    switch(current->scaleType) {
+    case Linear:
+      a = current->scaleParams[0];
+      b = current->scaleParams[1];
+
+      if (raw < current->scaleParams[0]) {
+	scaled = -1;
+      } else if (raw > current->scaleParams[1]) {
+	scaled = 1;
+      } else {
+	scaled = (2 / (b-a)) * (raw - a);
+      }
+	
+      break;
+    case Logistic:
+      shift = current->scaleParams[0];
+      scale = current->scaleParams[1];
+
+      scaled = (1 / (1 + Math.exp(-scale(raw - shift) ) ) );
+      
+      break;
+    default:
+      printf("ERROR, unrecognized scaling type");
+    }
+    
+    weightSum += current->weight;
+    sum += scaled * current->weight;
+    current = current->next;
+  }
+
+  sum /= weightSum;
+  
+  return sum;
+}
+
+void 
+
 
 /************************************************************************
 **
@@ -53,7 +147,7 @@
 ************************************************************************/
 
 //Returns the number of the given piece on the board.
-int numPieces(void* board,void* piece) {
+int NumPieces(void* board,void* piece) {
   int count=0;i;
   for(i=0;i<lBoard.size;i++) {
     if (compare(i,piece)) {
@@ -66,7 +160,7 @@ int numPieces(void* board,void* piece) {
 /*Returns the sum of the distance of each piece from the given edge of
 **the board. 0 = top, 1 = right, 2 = bottom, 3 = left.
 */
-int numFromEdge(void* board,void* piece,int edge) {
+int NumFromEdge(void* board,void* piece,int edge) {
   int count=0,i;
   for(i=0;i<lBoard.size;i++) {
     if(compare(i,piece)) {
@@ -90,7 +184,7 @@ int numFromEdge(void* board,void* piece,int edge) {
 **calculates the centroid, then measures the mean squared distance of all
 **pieces from the centroid.
 */
-double clustering(void* board,void* piece) {
+double Clustering(void* board,void* piece) {
   int rowAverage=0,colAverage=0,numPieces=0,i=0;
   double value=0;
   for(i=0;i<lBoard.size;i++) {
@@ -117,7 +211,7 @@ double clustering(void* board,void* piece) {
 **A connection is defined as a string of blank spaces, possibly
 **diagonal, between two of the given piece.
 */
-int connections(void* board,void* piece,void* blank,boolean diagonal) {
+int Connections(void* board,void* piece,void* blank,boolean diagonal) {
   int count=0,i,j,pathLength,loc;
   for(i=0;i<lBoard.size;i++) {
     if (compare(i,piece)) {
@@ -144,7 +238,6 @@ int connections(void* board,void* piece,void* blank,boolean diagonal) {
 	    break;
 	  }
 	}
-	
       }
     }
   }
--- gamesman/src/core/seval.h
+++ gamesman/src/core/seval.h
@@ -8,5 +8,31 @@ int numFromEdge(void*,void*,int);
 double clustering(void*,void*);
 int connections(void*,void*,void*,boolean);
 
+//structs and enums
+
+typedef enum scaling_enum {
+  Linear, Logistic;
+} SCALING
+
+//encapsulates parameters associated with a trait
+typedef struct trait {
+  int id;
+  int weight;
+  SCALING scaleType;
+  int scaleParams[2];
+  int additionalParams[4];
+} TRAIT;
+
+//trait linked list
+typedef struct traitlist_item {
+  TRAIT item;
+  struct traitlist_item* next;
+} *TRAITLIST;
+
+//static evaluator instance
+typedef struct seval_struct {
+  String name;
+  TRAITLIST traits;
+} EVALUATOR;
 
 #endif
