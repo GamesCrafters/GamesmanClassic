75a06a32f11a73a9e793891c83acd0849d62c301
brianzimmer <brianzimmer@a668be5d-ef11-cb4c-8bfd-e08cbb24f2ba>
2006-11-29 00:00:16 +0000

    Prototypes for the xml parsing and evaluating functions, featureEvaluator and scalingFunction function pointer types, and feature list type
    git-svn-id: https://gamescrafters.svn.sourceforge.net/svnroot/gamescrafters/GamesmanClassic/trunk@2932 a668be5d-ef11-cb4c-8bfd-e08cbb24f2ba

--- gamesman/src/core/seval.h
+++ gamesman/src/core/seval.h
@@ -1,38 +1,36 @@
-ifndef _SEVAL_H_
+#ifndef _SEVAL_H_
 #define _SEVAL_H_
 
-//function prototypes
+// Type Declarations
+
+typedef float(*scalingFunction)(float);
+
+typedef float(*featureEvaluator)(POSITION);
+
+typedef struct fNode{
+  STRING name;
+  float weight;
+  scalingFunction scale;
+  BOOLEAN perfect;
+  featureEvaluator evalFn;
+  struct fNode * next;
+} *fList;
+
+// Function Prototypes
 
 int numPieces(void*,void*);
 int numFromEdge(void*,void*,int);
 double clustering(void*,void*);
-int connections(void*,void*,void*,boolean);
-
-//structs and enums
-
-typedef enum scaling_enum {
-  Linear, Logistic;
-} SCALING
-
-//encapsulates parameters associated with a trait
-typedef struct trait {
-  int id;
-  int weight;
-  SCALING scaleType;
-  int scaleParams[2];
-  int additionalParams[4];
-} TRAIT;
-
-//trait linked list
-typedef struct traitlist_item {
-  TRAIT item;
-  struct traitlist_item* next;
-} *TRAITLIST;
-
-//static evaluator instance
-typedef struct seval_struct {
-  String name;
-  TRAITLIST traits;
-} EVALUATOR;
+int connections(void*,void*,void*,BOOLEAN);
+fList loadDataFromXML(STRING);
+fList parse_element(scew_element* , fList);
+featureEvaluator getSEvalLibFnPtr(STRING);
+featureEvaluator getSEvalCustomFnPtr(STRING);
+BOOLEAN initializeStaticEvaluator(STRING);
+VALUE evaluatePosition(POSITION p);
+float linear(float);
+float logarithmic(float);
+float logistic(float);
+float quadratic(float);
 
 #endif
