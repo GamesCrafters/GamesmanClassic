c24de3ab143c6ef8c9407acaebdad19271593a1d
dmchan <dmchan@a668be5d-ef11-cb4c-8bfd-e08cbb24f2ba>
2007-02-28 07:25:28 +0000

    fixed missing {}
    git-svn-id: https://gamescrafters.svn.sourceforge.net/svnroot/gamescrafters/GamesmanClassic/trunk@3190 a668be5d-ef11-cb4c-8bfd-e08cbb24f2ba

--- gamesman/src/core/main.c
+++ gamesman/src/core/main.c
@@ -259,10 +259,11 @@ void HandleArguments (int argc, char *argv[])
                         gLoadDatabase = FALSE;
                 } else if(!strcasecmp(argv[i], "--newdb"))
                         gLoadDatabase = FALSE;
-                else if(!strcasecmp(argv[i], "--filedb"))
-                		gFileDB = TRUE;
+                else if(!strcasecmp(argv[i], "--filedb")) {
+                	gFileDB = TRUE;
                         gBitPerfectDB = FALSE;
                         gBitPerfectDBSolver = FALSE;
+		}
                 else if(!strcasecmp(argv[i], "--numoptions")) {
                         fprintf(stderr, "\nNumber of Options: %d\n", NumberOfOptions());
                         gMessage = TRUE;
