60e4d83fab2a9313a0d491b6edca7ab64c57f5a9
dmchan <dmchan@a668be5d-ef11-cb4c-8bfd-e08cbb24f2ba>
2007-12-05 03:02:46 +0000

    box around winner, removed some old code
    git-svn-id: https://gamescrafters.svn.sourceforge.net/svnroot/gamescrafters/GamesmanClassic/trunk@3538 a668be5d-ef11-cb4c-8bfd-e08cbb24f2ba

--- gamesman/tcl/printing.tcl
+++ gamesman/tcl/printing.tcl
@@ -1,46 +1,46 @@
-global outputs
-set outputs("ps_path") "/tmp/"
-set outputs("left") "/tmp/left.ps"
-set outputs("top") "/tmp/top.ps"
-set outputs("right") "/tmp/right.ps"
-set outputs("top_merge") "/tmp/top_merge.ps"
-set outputs("left_merge") "/tmp/left_merge.ps"
-set outputs("right_merge") "/tmp/right_merge.ps"
-set outputs("output") "/tmp/output.ps"
-set outputs("bot") "/tmp/bot.ps"
-set outputs("bot_merge") "/tmp/bot_merge.ps"
-set outputs("left_moves_merge") "/tmp/left_moves_merge.ps"
-set outputs("left_moves") "/tmp/left_moves.ps"
-set outputs("right_moves_merge") "/tmp/right_moves_merge.ps"
-set outputs("right_moves") "/tmp/right_moves.ps"
-set outputs("middle_moves_merge") "/tmp/middle_moves_merge.ps"
-set outputs("middle_moves") "/tmp/middle_moves.ps"
-set outputs("output_merge") "/tmp/output_merge.ps"
-set outputs("outputPDF") "pdf/output.pdf"
-set outputs("static_oxy") "../bitmaps/static_oxy.ps"
-set outputs("static_legend") "../bitmaps/static_legend.ps"
-set outputs("static_blank") "../bitmaps/static_blank.ps"
-set outputs("gs_str") "exec /usr/bin/gs -q -dNOPAUSE -dBATCH -sDEVICE=pswrite \
+global p_outputs p_canvas 
+set p_outputs("ps_path") "/tmp"
+set p_outputs("left") "/tmp/left.ps"
+set p_outputs("top") "/tmp/top.ps"
+set p_outputs("right") "/tmp/right.ps"
+set p_outputs("top_merge") "/tmp/top_merge.ps"
+set p_outputs("left_merge") "/tmp/left_merge.ps"
+set p_outputs("right_merge") "/tmp/right_merge.ps"
+set p_outputs("output") "/tmp/output.ps"
+set p_outputs("bot") "/tmp/bot.ps"
+set p_outputs("bot_merge") "/tmp/bot_merge.ps"
+set p_outputs("left_moves_merge") "/tmp/left_moves_merge.ps"
+set p_outputs("left_moves") "/tmp/left_moves.ps"
+set p_outputs("right_moves_merge") "/tmp/right_moves_merge.ps"
+set p_outputs("right_moves") "/tmp/right_moves.ps"
+set p_outputs("middle_moves_merge") "/tmp/middle_moves_merge.ps"
+set p_outputs("middle_moves") "/tmp/middle_moves.ps"
+set p_outputs("output_merge") "/tmp/output_merge.ps"
+set p_outputs("outputPDF") "pdf/output.pdf"
+set p_outputs("static_oxy") "../bitmaps/static_oxy.ps"
+set p_outputs("static_legend") "../bitmaps/static_legend.ps"
+set p_outputs("static_blank") "../bitmaps/static_blank.ps"
+set p_outputs("gs_str") "exec /usr/bin/gs -q -dNOPAUSE -dBATCH -sDEVICE=pswrite \
 	-sOutputFile="
-set outputs("left_name") "/tmp/left_name.ps"
-set outputs("right_name") "/tmp/right_name.ps"
-# if you ever need to find the font_str
+set p_outputs("left_name") "/tmp/left_name.ps"
+set p_outputs("right_name") "/tmp/right_name.ps"
+
+# if you ever need to find the  
 # to a search for the following array value
 # and replace the occurences with the new path
 # need to find a way to pass it to exec w/o the
 # substitutions
-set outputs("font_str") "(\\/usr\\/share\\/ghostscript\\/fonts\\/Vag Rounded BT.ttf)"
-canvas .printing -width 500 -height 500
+set p_font "(\\/usr\\/share\\/ghostscript\\/fonts\\/Vag Rounded BT.ttf)"
+set p_canvas .printing 
+canvas $p_canvas -width 500 -height 500 
 
 # do the printing
 proc doPrinting {c position winningSide} {
-	global outputs gFrameWidth
-
-	# disable caching
-	set path [makePath $position false]
+	global p_outputs gFrameWidth p_canvas
+  $p_canvas configure -width $gFrameWidth -height $gFrameWidth
 
 	# capture the last position
-	capture $c $position false $path
+	capture $c $position false [makePath $position false] 
 
   # create the PDF display widget
 	$c create rectangle 0 [expr $gFrameWidth/2 - 75] $gFrameWidth [expr $gFrameWidth/2 + 75] -fill gray -width 1 -outline black -tag PDF
@@ -64,18 +64,18 @@ proc doPrinting {c position winningSide} {
 	
 	$c itemconfigure PDFStep -text "Step 5 of 6"
 	update idletasks
-	exec /usr/bin/psnup -q -1 -b0.25in -pletter -Pletter $outputs("output") $outputs("output_merge")
+	exec /usr/bin/psnup -q -1 -b0.25in -pletter -Pletter $p_outputs("output") $p_outputs("output_merge")
 	
 	$c itemconfigure PDFStep -text "Step 6 of 6"
 	update idletasks
 	# use gs to generate a pdf...
 	# only needed for debugging
-	makePDF $outputs("output_merge") $outputs("outputPDF")
+	makePDF $p_outputs("output_merge") $p_outputs("outputPDF")
 	$c delete PDF
 }
 
 proc addFooter { } {
-	global outputs
+	global p_outputs 
 	# omg... at this command
 	# add titles for the bottom columns
 	# also add the gamescrafters website location
@@ -177,11 +177,11 @@ proc addFooter { } {
 		(H) dispCenter\\n \
 		7750 -3000 moveto\\n \
 		(T) dispCenter\\n \
-		-90 rotate\\n&/' $outputs("output")	
+		-90 rotate\\n&/' $p_outputs("output")	
 }
 
 proc addHeader { } {
-	global outputs kGameName
+	global p_outputs kGameName 
 	# add the gamescrafters logo
 	# center on 396 -75
 	# add the time and the date
@@ -247,29 +247,19 @@ proc addHeader { } {
 		($t) dispCenter\\n \
 		750 -60 moveto\\n \
 		($host) dispCenter\\n \
-		-90 rotate/' $outputs("output")
-}
-
-# replace references to /Courier with
-# the vag rounded bt font...
-proc replaceFont { } {
-	global outputs
-	# right name
-	exec /usr/bin/sed -i -r -e 's/\\/Courier/(\\/usr\\/share\\/ghostscript\\/fonts\\/Vag Rounded BT.ttf)/' $outputs("right_name")
-	# left name
-	exec /usr/bin/sed -i -r -e 's/\\/Courier/(\\/usr\\/share\\/ghostscript\\/fonts\\/Vag Rounded BT.ttf)/' $outputs("left_name") 
+		-90 rotate/' $p_outputs("output")
 }
 
 # setup the top part
 proc makeTop { c position winningSide} {
-	global outputs kGameName
+	global p_outputs kGameName
 	# make the name tags
 	makeTags $winningSide
 	set maxMoves 6
 	# disable caching
 	set winPath [makePath $position false]
 	set moves [makeMoveList [expr 3 * $maxMoves]]
-	set emptyStr "$outputs(\"static_blank\") $outputs(\"static_blank\")"
+	set emptyStr "$p_outputs(\"static_blank\") $p_outputs(\"static_blank\")"
 	set leftMoves ""
 	set middleMoves ""
 	set rightMoves ""
@@ -298,75 +288,90 @@ proc makeTop { c position winningSide} {
 	if {$leftMoves == "" } {
 		set leftMoves $emptyStr
 	}
-	eval "$outputs(\"gs_str\")$outputs(\"left_moves_merge\") $leftMoves"
-	exec /usr/bin/psnup -q -m0.65in -$maxMoves -H8.5in -W8.6in -pletter $outputs("left_moves_merge") $outputs("left_moves")
+	eval "$p_outputs(\"gs_str\")$p_outputs(\"left_moves_merge\") $leftMoves"
+	exec /usr/bin/psnup -q -m0.65in -$maxMoves -H8.5in -W8.6in -pletter $p_outputs("left_moves_merge") $p_outputs("left_moves")
 	
 	if {$middleMoves == "" } {
 		set middleMoves $emptyStr
 	}
-	eval "$outputs(\"gs_str\")$outputs(\"middle_moves_merge\") $middleMoves"
-	exec /usr/bin/psnup -q -m0.65in -$maxMoves -H8.5in -W8.6in -pletter $outputs("middle_moves_merge") $outputs("middle_moves")	
+	eval "$p_outputs(\"gs_str\")$p_outputs(\"middle_moves_merge\") $middleMoves"
+	exec /usr/bin/psnup -q -m0.65in -$maxMoves -H8.5in -W8.6in -pletter $p_outputs("middle_moves_merge") $p_outputs("middle_moves")	
 
 	if {$rightMoves == "" } {
 		set rightMoves $emptyStr
 	}
-	eval "$outputs(\"gs_str\")$outputs(\"right_moves_merge\") $rightMoves"
-	exec /usr/bin/psnup -q -m0.65in -$maxMoves -H8.5in -W8.6in -pletter $outputs("right_moves_merge") $outputs("right_moves")		
+	eval "$p_outputs(\"gs_str\")$p_outputs(\"right_moves_merge\") $rightMoves"
+	exec /usr/bin/psnup -q -m0.65in -$maxMoves -H8.5in -W8.6in -pletter $p_outputs("right_moves_merge") $p_outputs("right_moves")		
 		
 
 	# make top
-	
-	set topStr "$outputs(\"left_name\") \"$winPath\" $outputs(\"right_name\") "
-	set topStr "$topStr $outputs(\"left_moves\") $outputs(\"middle_moves\") $outputs(\"right_moves\")"
-	eval "$outputs(\"gs_str\")$outputs(\"top_merge\") $topStr"
-	exec /usr/bin/psnup -q -6 -Pletter -pletter $outputs("top_merge") $outputs("top") 
+	set topStr "$p_outputs(\"left_name\") \"$winPath\" $p_outputs(\"right_name\") "
+	set topStr "$topStr $p_outputs(\"left_moves\") $p_outputs(\"middle_moves\") $p_outputs(\"right_moves\")"
+	eval "$p_outputs(\"gs_str\")$p_outputs(\"top_merge\") $topStr"
+	exec /usr/bin/psnup -q -6 -Pletter -pletter $p_outputs("top_merge") $p_outputs("top") 
 }
 
 # make the pages for the names
 proc makeTags { winningSide } {
-	global gLeftName gRightName outputs gFrameWidth gLeftPiece gRightPiece
+	global gLeftName gRightName gFrameWidth gLeftPiece gRightPiece 
+  global p_outputs p_canvas 
+
 	set colors [GS_ColorOfPlayers]
 	set maxLen [max [max [string length $gLeftName] \
 	 					[string length $gRightName]] 1]
 	# don't want to divide by zero...
 	# leave a buffer
-	set yOffset 250
+	set yOffset [expr $gFrameWidth / 2] 
 	set maxPixels [expr [tk scaling] * 8.5 * 72 - 10]
+
 	# make sure fontsize is an int
 	# also don't make font too big
 	set fontSize [min 128 [expr {int($maxPixels / $maxLen)}]]
+
 	# pack the printing canvas
 	# do some creation and capturing
 	# courier is just used as a placeholder... we will replace it
 	# with the desired font
-	pack .printing
-	.printing create text [expr $gFrameWidth / 2] [expr $yOffset + 210] \
+	pack $p_canvas
+
+	$p_canvas create text [expr $gFrameWidth / 2] [expr 1.9 * $yOffset]  \
 		-justify center -text "WINNER" -font {Courier 128} \
 		-tag __winner -state hidden
-	.printing create text [expr $gFrameWidth / 2] $yOffset -justify center \
+
+	$p_canvas create text [expr $gFrameWidth / 2] $yOffset -justify center \
 		-text "LEFT\n$gLeftName" -font "Courier $fontSize" -tag __printing -fill [lindex $colors 0]
+
 	if { $winningSide == $gLeftPiece } {
-		.printing itemconfigure __winner -state normal
+		$p_canvas itemconfigure __winner -state normal
+    $p_canvas create rectangle 0 [expr $yOffset / 2] $gFrameWidth $gFrameWidth \
+        -outline black -width 5 -tags p_rectangle 
 	}
 	update idletasks
 	
 	# reconfigure the text and capture again
-	.printing postscript -file $outputs("left_name") -pagewidth 8.5i
+	$p_canvas postscript -file $p_outputs("left_name") -pagewidth 8.5i
 	if { $winningSide == $gRightPiece } {
-		.printing itemconfigure __winner -state normal
+		$p_canvas itemconfigure __winner -state normal
+    $p_canvas create rectangle 0 [expr $yOffset / 2] $gFrameWidth $gFrameWidth \
+        -outline black -width 5 -tags p_rectangle 
 	} else {
-		.printing itemconfigure __winner -state hidden
+		$p_canvas itemconfigure __winner -state hidden
+    $p_canvas delete p_rectangle
 	}
-	.printing itemconfigure __printing -text "RIGHT\n$gRightName" -fill [lindex $colors 1]
+
+	$p_canvas itemconfigure __printing -text "RIGHT\n$gRightName" -fill [lindex $colors 1]
 	update idletasks
+
 	# again
-	.printing postscript -file $outputs("right_name") -pagewidth 8.5i
-	pack forget .printing
-	.printing delete __printing __winner
+	$p_canvas postscript -file $p_outputs("right_name") -pagewidth 8.5i
+	pack forget $p_canvas
+	$p_canvas delete __printing __winner
 	update
+
 	# replace the Courier font with
 	# the vag rounded bt
-	replaceFont
+	exec /usr/bin/sed -i -r -e 's/\\/Courier/(\\/usr\\/share\\/ghostscript\\/fonts\\/Vag Rounded BT.ttf)/' $p_outputs("left_name")
+	exec /usr/bin/sed -i -r -e 's/\\/Courier/(\\/usr\\/share\\/ghostscript\\/fonts\\/Vag Rounded BT.ttf)/' $p_outputs("right_name")
 }
 
 proc makeMoveList { maxMoves } {
@@ -402,10 +407,10 @@ proc makeBottom {} {
 
 # combine bottom and top outputs
 proc combine {} {
-	global outputs
-	eval "$outputs(\"gs_str\")$outputs(\"output_merge\") $outputs(\"top\") \
-		$outputs(\"bot\")"
-	exec /usr/bin/psnup -q -2 -pletter -W8.25in $outputs("output_merge") $outputs("output")	
+	global p_outputs
+	eval "$p_outputs(\"gs_str\")$p_outputs(\"output_merge\") $p_outputs(\"top\") \
+		$p_outputs(\"bot\")"
+	exec /usr/bin/psnup -q -2 -pletter -W8.25in $p_outputs("output_merge") $p_outputs("output")	
 }
 
 # go through all the mistakes
@@ -425,7 +430,7 @@ proc setMistakesLists { } {
 
 # generate the mistake pages to display
 proc generateBottom { } {
-	global leftMistakes rightMistakes outputs
+	global leftMistakes rightMistakes p_outputs
 
 	set maxErrors 4
 	set leftExec [makeExec $leftMistakes $maxErrors]
@@ -436,37 +441,37 @@ proc generateBottom { } {
 	# then combine the pages, else we point to a blank page
 	# need to point to blank page to preserve ordering
 	if { $leftExec == "" } {
-		set leftExec "$outputs(\"static_blank\") $outputs(\"static_blank\")"
+		set leftExec "$p_outputs(\"static_blank\") $p_outputs(\"static_blank\")"
 	}
-	eval "$outputs(\"gs_str\")$outputs(\"left\") $leftExec"
+	eval "$p_outputs(\"gs_str\")$p_outputs(\"left\") $leftExec"
 	exec /usr/bin/psnup -q -[expr 2 * $maxErrors] -H8.5in -W8.5in -pletter \
-		$outputs("left") $outputs("left_merge")
-	set mergeStr "$mergeStr $outputs(\"left_merge\")"
+		$p_outputs("left") $p_outputs("left_merge")
+	set mergeStr "$mergeStr $p_outputs(\"left_merge\")"
 	
 	if { $rightExec == "" } {
-		set rightExec "$outputs(\"static_blank\") $outputs(\"static_blank\")"
+		set rightExec "$p_outputs(\"static_blank\") $p_outputs(\"static_blank\")"
 	}
-	eval "$outputs(\"gs_str\")$outputs(\"right\") $rightExec"
+	eval "$p_outputs(\"gs_str\")$p_outputs(\"right\") $rightExec"
 	exec /usr/bin/psnup -q -[expr 2 * $maxErrors] -H8.5in -W8.5in -pletter \
-			$outputs("right") $outputs("right_merge")
-	set mergeStr "$mergeStr $outputs(\"right_merge\")"
+			$p_outputs("right") $p_outputs("right_merge")
+	set mergeStr "$mergeStr $p_outputs(\"right_merge\")"
 	
 	# combine the left and right outputs to form the bottom
-	eval "$outputs(\"gs_str\")$outputs(\"bot_merge\") $mergeStr"
-	exec /usr/bin/psnup -q -2 -pletter $outputs("bot_merge") $outputs("bot")
+	eval "$p_outputs(\"gs_str\")$p_outputs(\"bot_merge\") $mergeStr"
+	exec /usr/bin/psnup -q -2 -pletter $p_outputs("bot_merge") $p_outputs("bot")
 }
 
 # go through the mistake lists
 # and add stuff to the psnup execute command
 proc makeExec { mistakeList maxErrors} {
-	global outputs
+	global p_outputs
 	set size [llength $mistakeList]
 	set ret ""
 	# trim mistakes to the worst ones
 	if { $size > $maxErrors } {
 		# get list of the worst mistakes
 		set mistakeList [findWorst $mistakeList]
-		# truncate to 4 mistakes
+		# truncate to 4 maxErrors
 		set mistakeList [lrange $mistakeList 0 [expr $maxErrors - 1]]
 	}
 
@@ -479,6 +484,12 @@ proc makeExec { mistakeList maxErrors} {
 		set badPos [C_DoMove $oldPos $badMove]
 		set old [makePath $oldPos true]
 		set new [makePath $badPos false]
+    
+    # position was not captured
+    # use blank place holder for now
+    if { [file exists $new] != true } {
+      set new $p_outputs("static_blank") 
+    }
 		set ret "$ret \"$old\" \"$new\""
 	}
 	
@@ -543,15 +554,16 @@ proc stripKey { lst } {
 # the position we need to capture hasn't
 # been capture yet
 proc doCapture { c moveType position theMoves value} {
-	# disable caching
 	set path [makePath $position $value]
 	if { $path != "" } {
 		set type "all"
 		if {$value == true} {
 			set type "value"
 		}
+
 		# hide old and display new
 		GS_HideMoves $c $moveType $position $theMoves
+
 		if {$value == true} {
 			GS_ShowMoves $c $type $position $theMoves		
 		}
@@ -564,6 +576,7 @@ proc doCapture { c moveType position theMoves value} {
 		if {$value == true} {
 			GS_HideMoves $c $type $position $theMoves
 		}
+
 		GS_ShowMoves $c $moveType $position $theMoves
 		update idletasks
 	}
@@ -582,29 +595,16 @@ proc capture { c pos value path} {
 	}
 }
 
-# will generate the path for the given arguments
-# but will only return if the path doesn't already
-# exist, if it does, then return ""
-proc makePathOnce { pos value} {
-	set path [makePath $pos $value]
-	
-	if { [file exists $path] } {
-		return ""
-	}
-	
-	return $path
-}
-
 # make path with given arguments
 # do the folder checking in here, since other
 # functions should call this function to make the
 # path
-proc makePath { pos value } {
-	global kGameName outputs
+proc makePath { pos valueMove } {
+	global kGameName p_outputs
 	checkFolders $kGameName
 	
-	set path "$outputs(\"ps_path\")/$kGameName/$kGameName\_$pos"
-	if { $value == true} {
+	set path "$p_outputs(\"ps_path\")/$kGameName/$kGameName\_$pos"
+	if { $valueMove == true} {
 		set path "$path\_v"
 	}
 	
@@ -614,7 +614,7 @@ proc makePath { pos value } {
 # make string to use for pdf generation
 # then call it
 proc makePDF { input output} {
-	global outputs
+	global p_outputs
 	if { ![file exists "pdf/"] } {
 		file mkdir "pdf/"
 	}
@@ -625,13 +625,13 @@ proc makePDF { input output} {
 # check for folders we need
 proc checkFolders { game } {
 	# check for directories
-	global outputs
-	if { ![file exists "$outputs(\"ps_path\")/"] } {
-		file mkdir "$outputs(\"ps_path\")"
+	global p_outputs
+	if { false == [file exists "$p_outputs(\"ps_path\")/"] } {
+		file mkdir "$p_outputs(\"ps_path\")"
 	}
 	
-	if { ![file exists "$outputs(\"ps_path\")/$game"] } {
-		file mkdir "$outputs(\"ps_path\")/$game"
+	if { false == [file exists "$p_outputs(\"ps_path\")/$game"] } {
+		file mkdir "$p_outputs(\"ps_path\")/$game"
 	}
 }
 
