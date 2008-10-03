e7fad7163a40f5fd6e5e1cd0c9fa601277f2265c
j-fly4 <j-fly4@a668be5d-ef11-cb4c-8bfd-e08cbb24f2ba>
2008-10-03 18:31:00 +0000

    Ethan and I (Jeremy) have rewritten the Rubik.py to represent the cube in a more natural state (permutation and orientation). The notation has been updated, and the output is beautiful.
    git-svn-id: https://gamescrafters.svn.sourceforge.net/svnroot/gamescrafters/GamesmanClassic/trunk@3631 a668be5d-ef11-cb4c-8bfd-e08cbb24f2ba

--- gamesman/src/Puzzle/Rubik.py
+++ gamesman/src/Puzzle/Rubik.py
@@ -13,68 +13,131 @@ import string
 #Level 8 : 869715  | 3.82
 #Level 9 : 1885639 | 2.16
 
-# cat = concatenate
-def cat(s1, s2): return s1 + s2
-
-# fact
-def fact(n):
-    if n < 1:
-        return 1
-    else:
-        return n * fact(n-1)
-
 class Rubik(Puzzle):
     """This is the 2x2x2 Rubiks Cube puzzle class"""
-
-    def __init__(self, T="WWW",R="BBB",L="RRR",BT="YYYY",BR="OOOO",BL="GGGG"):
-        # anchor on Top=white, Right=Blue, Left=Red, then flip around hand axis
-        # clockwise, always (T3,R3,L3,BT4,BR4,BL4)
-        self.T, self.R, self.L, self.BT, self.BR, self.BL = T,R,L,BT,BR,BL
-        Puzzle(True) 
+    
+    #We want to be color scheme independent, so we're going to identify colors
+    #from their face. Standard notation is F for front, U=up, R=right L=left, B=back, D=down
+    #Corners are numbered as in the following diagram
+    
+    #         +-----+
+    #         |3 U 2|
+    #         |1   0|
+    #         +-----+
+    # +-----+ +-----+ +-----+ +-----+
+    # |3 L 1| |1 F 0| |0 R 2| |2 B 3|
+    # |7   5| |5   4| |4   6| |6   7|
+    # +-----+ +-----+ +-----+ +-----+
+    #         +-----+
+    #         |5 D 4|
+    #         |7   6|
+    #         +-----+
+    
+    #The cube will be represented as an array of 8 numbers, where each number is N*3 + T,
+    #where N is the number of piece (as in the above diagram), and T is the
+    #number of turns clockwise necessary to orient the piece. A piece is oriented
+    #if it has either the U or D color on U or D.
+    def is_oriented(self, c):
+        return c == "U" or c == "D"
+    
+    #Each face's stickers are defined in the following order:
+    # +---+---+
+    # |_3_|_2_|
+    # | 1 | 0 |
+    # +---+---+
+    values = { "F" : 0, "U" : 0, "R" : 0, "L" : 1, "B" : 2, "D" : 4}
+    """TODO - deal with corner 7 not being correct"""
+    #This method exists because python does not provide a way to have multiple constructors
+    def initializeCubeWithStickers(self, F="FFFF", U="UUUU", R="RRRR", L="LLLL", B="BBBB", D="DDDD"):
+        self.pieces = []
+        orientation = 0
+        for c in range(8):
+            up = c < 4
+            s0 = eval("DU"[up])[c % 4]                  # this sticker is facing up or down
+            s2 = eval("RFBLFLRB"[c])[3 * up]            # this sticker is clockwise of s0
+            s1 = eval("RFBLFLRB"[(c + 4) % 8])[1 + up]  # this sticker is counter clockwise of s0
+            corner = [  ]
+            #TODO - check if is valid and unvisited corner
+            n = self.values[s0] + self.values[s1] + self.values[s2]
+            t = 1*self.is_oriented(s1) + 2*self.is_oriented(s2)
+            orientation += t
+            self.pieces.append(3*n + t)
+        
+        if orientation % 3 != 0:
+            raise "Invalid corner orientation"
+        
+    def __init__(self, pieces=[3*n for n in range(8)]):
+        self.pieces = pieces
+        Puzzle(True)
 
     def generate_solutions(self):
         return [Rubik()]   # Default _is_ solution; what else?
     
+    #We're anchoring the BLD corner, so legal moves are F, U, R (and their multiples)
+    #F means rotate front face clockwise 90 degrees, F' means 90 degrees counterclockwise, F2 means 180 degrees
     def generate_moves(self):
-        return ['B1','B2','B3','R1','R2','R3','L1','L2','L3']
+        return ["F", "F2", "F'", "U", "U'", "U2", "R", "R'", "R2"]
 
-    def do_move(self, move):
-        T,   R,  L = list(self.T), list(self.R), list(self.L)
-        BT, BR, BL = list(self.BT),list(self.BR),list(self.BL)
-        times = int(move[1])
-        for i in range(times):
-            if move[0] == 'B':
-                R[1],   R[2],  L[0],  L[1],  \
-                BT[0], BT[1], BT[2], BT[3],  \
-                BR[0], BR[1], BL[0], BL[3] = \
-                L[0],  L[1],  BL[3], BL[0],  \
-                BT[3], BT[0], BT[1], BT[2],  \
-                R[1],  R[2],  BR[1], BR[0]
-            elif move[0] == 'R':
-                T[1],   T[2],  R[0],   R[1], BT[0], BT[3], \
-                BR[0], BR[1], BR[2], BR[3],                \
-                BL[0], BL[1] =                             \
-                R[0],   R[1], BT[3], BT[0],  BL[1], BL[0], \
-                BR[3], BR[0], BR[1], BR[2],                \
-                T[1],   T[2]
-            elif move[0] == 'L':
-                T[0],   T[1],  L[1],  L[2],  \
-                BT[0], BT[1], BR[0], BR[3],  \
-                BL[0], BL[1], BL[2], BL[3] = \
-                BR[3], BR[0],  T[0],  T[1],  \
-                L[1],  L[2], BT[1], BT[0],   \
-                BL[3], BL[0], BL[1], BL[2] 
-            else:
-                raise 'BadMoveError', move + ' is not one of [BRL][123]'
-        return Rubik(reduce(cat,T),  reduce(cat,R),  reduce(cat,L),
-                     reduce(cat,BT), reduce(cat,BR), reduce(cat,BL))
-            
-    def reverse_move(self, move):
-        return move[0] + str(4-int(move[1])) 
-
-    def __str__(self):
-        return self.T+'|'+self.R+'|'+self.L+'|'+self.BT+'|'+self.BR+'|'+self.BL
-
-    def __hash__(self):
-        return hash(str(self))
+    #this returns the value of the corner after being twisted turnsCW
+    def twist_cw(self, corner, turnsCW):
+        return 3 * (corner / 3) + (corner - turnsCW) % 3
     
+    def do_move(self, move):
+        times = self.turns.index(move[1:])
+        pieces = list(self.pieces)
+        for i in range(times):
+            if move[0] == 'F':
+                pieces[0], pieces[4], pieces[5], pieces[1] = pieces[1], pieces[0], pieces[4], pieces[5]
+                pieces[0] = self.twist_cw(pieces[0], 1)
+                pieces[1] = self.twist_cw(pieces[1], 2)
+                pieces[4] = self.twist_cw(pieces[4], 2)
+                pieces[5] = self.twist_cw(pieces[5], 1)
+            elif move[0] == 'U':
+                pieces[0], pieces[1], pieces[3], pieces[2] = pieces[2], pieces[0], pieces[1], pieces[3]
+            elif move[0] == 'R':
+                pieces[0], pieces[2], pieces[6], pieces[4] = pieces[4], pieces[0], pieces[2], pieces[6]
+                pieces[0] = self.twist_cw(pieces[0], 2)
+                pieces[2] = self.twist_cw(pieces[2], 1)
+                pieces[4] = self.twist_cw(pieces[4], 1)
+                pieces[6] = self.twist_cw(pieces[6], 2)
+            else:
+                raise "BadMoveError", move + " is not one of [FUR][ '2]"
+        return Rubik(pieces)
+
+    turns = [ None, "", "2", "'" ]
+    def reverse_move(self, move):
+        return move[0] + self.turns[4 - self.turns.index(move[1:])]
+
+    """TODO: Ethan, this is all you"""
+    def __str__(self):
+        #return self.T+'|'+self.R+'|'+self.L+'|'+self.BT+'|'+self.BR+'|'+self.BL
+        solved_cube = (("U","R","F"),("U","F","L"),("U","B","R"),("U","L","B"),("D","F","R"),("D","L","F"),("D","R","B"),("D","B","L")) # solved pieces are how each cube (as represented in the picture above) would look going clockwise
+        pieces = list(self.pieces)
+        current_state = []
+        for x in range(len(pieces)):
+            current_chunk = []
+            real_piece = pieces[x] / 3
+            rotations_from_orientation = pieces[x] % 3
+            current_chunk.append(solved_cube[real_piece][(0-rotations_from_orientation)]) #top piece
+            current_chunk.append(solved_cube[real_piece][(1-rotations_from_orientation)]) #right piece
+            current_chunk.append(solved_cube[real_piece][(2-rotations_from_orientation)]) #left piece
+            current_state.append(current_chunk) # This is the stickers on piece[x] arranged clockwise
+        cube_string = "         +-----+\n"
+        cube_string += "         |"+current_state[3][0]+" U "+current_state[2][0]+"|\n"
+        cube_string += "         |"+current_state[1][0]+"   "+current_state[0][0]+"|\n"
+        cube_string += "         +-----+\n"
+        cube_string += " +-----+ +-----+ +-----+ +-----+\n"
+        cube_string += " |"+current_state[3][1]+" L "+current_state[1][2]+"| |"+current_state[1][1]+" F "+current_state[0][2]+"| |"+current_state[0][1]+" R "+current_state[2][2]+"| |"+current_state[2][1]+" B "+current_state[3][2]+"|\n"
+        cube_string += " |"+current_state[7][2]+"   "+current_state[5][1]+"| |"+current_state[5][2]+"   "+current_state[4][1]+"| |"+current_state[4][2]+"   "+current_state[6][1]+"| |"+current_state[6][2]+"   "+current_state[7][1]+"|\n"
+        cube_string += " +-----+ +-----+ +-----+ +-----+\n"
+        cube_string += "         +-----+\n"
+        cube_string += "         |"+current_state[5][0]+" D "+current_state[4][0]+"|\n"
+        cube_string += "         |"+current_state[7][0]+"   "+current_state[6][0]+"|\n"
+        cube_string += "         +-----+\n"
+        return cube_string
+    
+    def __hash__(self):
+        hash = 0
+        for i, piece in enumerate(self.pieces):
+            hash |= piece << (5 * i)
+        return hash
