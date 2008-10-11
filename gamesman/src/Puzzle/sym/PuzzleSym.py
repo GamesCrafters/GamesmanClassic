5bc36be86e355d9c07b8fb1a0bab6b8483a76b68
cp123127 <cp123127@a668be5d-ef11-cb4c-8bfd-e08cbb24f2ba>
2008-10-11 17:01:01 +0000

    Syms workcvs commit! Check it out.
    git-svn-id: https://gamescrafters.svn.sourceforge.net/svnroot/gamescrafters/GamesmanClassic/trunk@3633 a668be5d-ef11-cb4c-8bfd-e08cbb24f2ba

---
+++ gamesman/src/Puzzle/sym/PuzzleSym.py
@@ -0,0 +1,44 @@
+class Puzzle:
+    """This is the base class for a puzzle"""
+
+    def __init__(self,  one_solution = True):
+        self.one_solution = one_solution
+
+    def generate_start(self):      # By default, no starting point (like Rubik's cube)
+        return False
+
+    def generate_solutions(self):
+        return []
+    
+    def generate_moves(self):
+        return []
+
+    def do_move(self, move):
+        return []
+
+    def __add__(self, move):
+        return self.do_move(move).con_board()
+
+    def is_a_solution(self):
+        return self in self.generate_solutions()
+    
+    def con_board(self):
+        return self
+    
+    def is_illegal(self):
+        return False
+
+    def reverse_move(self, move):
+        return move
+
+	def get_permutations(self):
+		return 0
+
+    def __cmp__(self, other):
+        return cmp(hash(self), hash(other))
+
+    def __hash__(self):
+        return 0
+
+    def __str__(self):
+        return "Default Puzzle string"
