788f1a4d31af0c27af6475711a3852fa4dca5d99
l156steven <l156steven@a668be5d-ef11-cb4c-8bfd-e08cbb24f2ba>
2008-10-28 01:54:58 +0000

    Changes to Puzzle.py and Solver.py
    git-svn-id: https://gamescrafters.svn.sourceforge.net/svnroot/gamescrafters/GamesmanClassic/trunk@3637 a668be5d-ef11-cb4c-8bfd-e08cbb24f2ba

--- gamesman/src/Puzzle/Puzzle.py
+++ gamesman/src/Puzzle/Puzzle.py
@@ -9,6 +9,10 @@ class Puzzle:
 
     def generate_solutions(self):
         return []
+
+    #use self.is_a_solution() instead
+    #def is_solution(self):
+    #    return False
     
     def generate_moves(self):
         return []
@@ -16,20 +20,26 @@ class Puzzle:
     def do_move(self, move):
         return []
 
+    def undo_move(self, move): # new
+        return []
+
     def __add__(self, move):
         return self.do_move(move)
 
-    def is_a_solution(self):
+    def __sub__(self, move): # new
+        return self.undo_move(move)
+
+    def is_a_solution(self): # new
         return self in self.generate_solutions()
+
+    def is_deadend(self): # new
+        return False
+
+    def is_leaf(self): # new
+        return self.is_a_solution() or self.is_deadend()
 
     def is_illegal(self):
         return False
-
-    def reverse_move(self, move):
-        return move
-
-	def get_permutations(self):
-		return 0
 
     def __cmp__(self, other):
         return cmp(hash(self), hash(other))
@@ -37,5 +47,15 @@ class Puzzle:
     def __hash__(self):
         return 0
 
+    def unhash(self, number): #new
+        return self
+
     def __str__(self):
         return "Default Puzzle string"
+
+    def value(self):
+        return 1
+
+
+
+    
--- gamesman/src/Puzzle/Solver.py
+++ gamesman/src/Puzzle/Solver.py
@@ -1,13 +1,16 @@
+import cPickle
+
 class Solver:
     """This is a solver of a single solution"""
-
+    # cpickle not implemented
     def __init__(self):
-        self.levels = {}         # level_num : [position, position, ...]
+        self.levels = {}         # level_num : [hashed_position, hashed_position, ...]
         self.seen = {}           # hashed_position : level_num
         self.move = {}           # hashed_position : [reversed_move, reversed_move, ...]
         self.visited = {}        # hashed_position : True
         self.path_to_answer = {} # hashed_position : True
-	
+        self.puzzle = None
+
     def find_solutions(self, puzzle):
         if puzzle.is_illegal() or hash(puzzle) in self.visited:
             return []
@@ -21,27 +24,32 @@ class Solver:
                 solutions += self.find_solutions(child)
             return solutions
 
+
     def solve(self, puzzle, verbose=False, max_level=-1):
+        self.puzzle = puzzle
         solutions = puzzle.generate_solutions()
 
         if not solutions:
             solutions = self.find_solutions(puzzle)
 
-        self.levels[0] = solutions
+        #self.levels[0] = solutions
+        self.levels[0] = []
         level = 0
         for solution in solutions:
+            self.levels[0].append(hash(solution))
             self.seen[hash(solution)] = level
         if verbose:
             print "Level 0 : " + str(len(solutions))
         while self.levels[level] and (max_level==-1 or level<max_level):
             self.levels[level+1] = []
-            for position in self.levels[level]:
+            for h_position in self.levels[level]:
+                position = self.puzzle.unhash(h_position)
                 for move in position.generate_moves():
                     child = position + move
                     if not child.is_illegal():
                         if hash(child) not in self.seen:  # first time we've seen it
                             self.seen[hash(child)] = level+1
-                            self.levels[level+1].append(child)
+                            self.levels[level+1].append(hash(child))
                             self.move[hash(child)] = [position.reverse_move(move)]
                         elif self.seen[hash(child)] == level+1: # another sol path!
                             self.move[hash(child)].append(position.reverse_move(move))
@@ -69,6 +77,20 @@ class Solver:
             print "        LEVEL: " + str(self.seen[hash(puzzle)])
             print puzzle
 
+    def nextmove(self, puzzle):
+        if puzzle not in self.seen:
+            print "Sorry, position not in database"
+            return
+        else:
+            solutions = puzzle.generate_solutions()
+            if not solutions:
+                self.visited = {}
+                solutions = self.find_solutions(puzzle)
+            if puzzle not in solutions:
+                return (puzzle + self.move[hash(puzzle)][0])
+            else:
+                return
+    
     def mark_path_to_answer(self, puzzle):
         if puzzle.generate_start():
             start = puzzle.generate_start()
@@ -99,16 +121,17 @@ class Solver:
 
         print " node [fontname = \"Courier\"];",
 
-        start = self.levels[0][0].generate_start() # Ask 1st solution for its start
+        start = self.puzzle.unhash(self.levels[0][0]).generate_start() # Ask 1st solution for its start
         if start:                         # Make starting points inv triangles
-			print "  \""  + str(start) + "\" [shape=invtriangle]"
-			self.mark_path_to_answer(start)
-			
+            print "  \""  + str(start) + "\" [shape=invtriangle]"
+
+        self.mark_path_to_answer(start)
         for p in self.path_to_answer.keys():
             print "  \""  + str(p) + "\" [style=filled, color=\".7 .3 1.0\"];"
 
         rankstr = "  { rank=same; 0; "
-        for solution in self.levels[0]: # Make solutions triangles
+        for h_solution in self.levels[0]: # Make solutions triangles
+            solution = self.puzzle.unhash(h_solution)
             rankstr += "\"" + str(solution) + "\"; "
             print "  \""  + str(solution) + "\" [shape=triangle]"
         if print_levels:
@@ -117,7 +140,8 @@ class Solver:
         for level in self.levels.keys():          
             if level != 0:                              # don't include solutions
                 rankstr = "  { rank=same; " + str(level) + "; "
-                for p in self.levels[level]:
+                for h_p in self.levels[level]:
+                    p = self.puzzle.unhash(h_p)
                     for move in self.move[hash(p)]:
                         answer = p + move
                         rankstr += "\"" + str(p) + "\"; "
@@ -128,77 +152,60 @@ class Solver:
 
         print "}"
 
-    def get_max_level(self):
-		return max(self.levels.keys())
+    def save(self, fname):
+        f = open(fname, 'w')
+        cPickle.dump(self, f)
+        f.close()
 
-    def get_reachables(self):
-		return len(self.seen)
-
-    def get_permutations(self, puzzle):
-		return puzzle.get_permutations()
-
-    def get_ratio(self, puzzle):
-		return float(self.get_reachables())/float(self.get_permutations(puzzle))
-		
-class MSSolver:
-    
-    def __init__(self):
-        self.levels = {}         # level_num : [position, position, ...]
-        self.seen = {}           # hashed_position : level_num
-        self.move = {}           # hashed_position : [reversed_move, reversed_move, ...]
-        self.visited = {}        # hashed_position : True
-        self.path_to_answer = {} # hashed_position : True
-        self.solutions = []
-        self.init_puzzle = None
-    
-    def reset(self):
-        self.__init__()
+    def load(self, fname):
+        f = open(fname, 'w')
+        result = cPickle.load(f)
+        f.close()
+        return result
         
-    def solve(self, init_puzzle, verbose = False, stop_at_a_solution = False):
-        self.init_puzzle = init_puzzle
-        if init_puzzle.is_illegal():
-            pass
-        else:
-            level = 0
-            self.levels[0] = [init_puzzle]
-            self.seen[hash(init_puzzle)] = 0
-            solution_found = init_puzzle.is_a_solution()
-            if verbose:
-                print "Level 0 : 1"
-            while self.levels[level] and (not (self.solutions and stop_at_a_solution)):
-                self.levels[level+1] = []
-                for position in self.levels[level]:
-                    for move in position.generate_moves():
-                        child = position + move
-                        if not child.is_illegal():
-                            if hash(child) not in self.seen:
-                                if child.is_a_solution():
-                                        self.solutions.append(child)
-                                self.seen[hash(child)] = level + 1
-                                self.levels[level+1].append(child)
-                                self.move[hash(child)] = [position.reverse_move(move)]
-                            elif self.seen[hash(child)] == level+1:
-                                self.move[hash(child)].append(position.reverse_move(move))
-                            else:
-                                pass
-                if verbose:
-                    print "Level " + str(level + 1) + " : " + str(len(self.levels[level+1]))
-                level += 1
-        return self.solutions
+    '''
+    def save(self, fname):
+        f = open(fname, 'w')
+        f.write('levels\n')
+        for l in self.levels.keys():
+            f.write(str(l)+'\n')
+            for elt in self.levels[l]:
+                f.write(str(elt)+' ')
+            f.write('\n')
+        f.write('move\n')
+        for h_p in self.move.keys():
+            f.write(str(h_p)+'\n')
+            for elt in self.move[h_p]:
+                f.write(str(elt)+' ')
+            f.write('\n')
+        f.close()
 
-    def path(self, solution):
-        if solution not in solutions:
-            print "Sorry, this solution is not in the database"
-        else:
-            solutions = puzzle.generate_solutions()
-            if not solutions:
-                self.visited = {}
-                solutions = self.find_solutions(puzzle)
-            while puzzle not in solutions:
-                print "        LEVEL: " + str(self.seen[hash(puzzle)])
-                print puzzle
-                print ""
-                puzzle += self.move[hash(puzzle)][0] ### Walk left side of tree
-            print "        LEVEL: " + str(self.seen[hash(puzzle)])
-            print puzzle
-        
+    def load(self, fname, puzzle):
+        self.puzzle = puzzle
+        f = open(fname)
+        l = f.readline()
+        mode = None
+        while not (l == ''):
+            l = l[:-1]
+            if (l == 'levels') or (l == 'move'):
+                mode = l
+            elif mode == 'levels':
+                nextl = f.readline()
+                elts = nextl.split()
+                level = int(l)
+                index = 0
+                length = len(elts)
+                while index < length:
+                    h_p = int(elts[index])
+                    self.seen[h_p] = level
+                    elts[index] = h_p
+                    index += 1
+                self.levels[level] = elts[:]
+            else: # mode == 'move'
+                h_p = int(l)
+                nextl = f.readline()
+                elts = nextl.split()
+                self.move[h_p] = elts[:]
+            l = f.readline()
+        f.close()
+    '''
