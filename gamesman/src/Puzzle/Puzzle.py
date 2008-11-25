afefde7e91a464b125794fe68a6880890562e6b4
pathorn <pathorn@a668be5d-ef11-cb4c-8bfd-e08cbb24f2ba>
2008-11-25 05:04:55 +0000

    Unserialize should not take selv.
    git-svn-id: https://gamescrafters.svn.sourceforge.net/svnroot/gamescrafters/GamesmanClassic/trunk@3661 a668be5d-ef11-cb4c-8bfd-e08cbb24f2ba

--- gamesman/src/Puzzle/Puzzle.py
+++ gamesman/src/Puzzle/Puzzle.py
@@ -4,7 +4,7 @@ class Puzzle:
     def __init__(self,  one_solution = True):
         self.one_solution = one_solution
 
-    def generate_start(self):      # By default, no starting point (like Rubik's cube)
+    def generate_start(self):
         return False
 
     def generate_solutions(self):
@@ -16,17 +16,26 @@ class Puzzle:
     def do_move(self, move):
         return []
 
+    def undo_move(self, move):
+        return []
+
     def __add__(self, move):
         return self.do_move(move)
+
+    def __sub__(self, move):
+        return self.undo_move(move)
 
     def is_a_solution(self):
         return self in self.generate_solutions()
 
+    def is_deadend(self):
+        return (not self.is_a_solution()) and (self.is_leaf())
+
+    def is_leaf(self):
+        return (len(self.generate_moves) == 0)
+
     def is_illegal(self):
         return False
-
-    def reverse_move(self, move):
-        return move
 
     def __cmp__(self, other):
         return cmp(hash(self), hash(other))
@@ -34,15 +43,32 @@ class Puzzle:
     def __hash__(self):
         return 0
 
-    def unhash(self, num): # add this function to whatever you have
-        return self.generate_start()
+    def unhash(self, number):
+        return self
 
     def __str__(self):
         return "Default Puzzle string"
 
-    # static methods
-    def serialize(self):
-        return
+    def value(self):
+        return 1
 
-    def unserialize(self):
-        return
+    # Functions required for web framework:
+    def serialize(self):
+        """Returns a string representation of this object that can be unserialized.
+
+           Has a similar relation as __hash__ and unhash, but should be independent
+           of the hashing function used.  In some cases, __str__ can be used.
+           """
+        return str(self)
+    
+    # Dictionary containing the default options passed to constructor.
+    default_options = {}
+    
+    @staticmethod
+    def unserialize(options, boardstr):
+        """Takes the output of serialize() and returns a new Puzzle object.
+
+           If the board is invalid, returns None
+           'options' is a dictionary containing any other arguments for the constructor.
+           """
+        pass # return new object with passed options and parse board string
