936a1cf6b9e44bedc5763963c3142bca689de8ec
j-fly4 <j-fly4@a668be5d-ef11-cb4c-8bfd-e08cbb24f2ba>
2009-02-04 21:14:30 +0000

    Moved Puzzle directory to GamesmanWeb directory.
    git-svn-id: https://gamescrafters.svn.sourceforge.net/svnroot/gamescrafters/GamesmanClassic/trunk@3851 a668be5d-ef11-cb4c-8bfd-e08cbb24f2ba

--- src/Puzzle/SlideNTester.py
+++
@@ -1,7 +0,0 @@
-import SlideN
-import Solver
-
-n = SlideN.SlideN(4)
-s = Solver.Solver()
-s.solve(n)
-s.graph()
--- src/Puzzle/WheelTester.py
+++
@@ -1,7 +0,0 @@
-import Wheel
-import Solver
-
-w = Wheel.Wheel(8,7)
-s = Solver.Solver()
-s.solve(w)
-s.graph()
