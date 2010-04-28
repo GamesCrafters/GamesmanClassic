74ded7d337f7e564f816cc300234c33f33f1471a
james_ide <james_ide@a668be5d-ef11-cb4c-8bfd-e08cbb24f2ba>
2010-04-28 21:54:45 +0000

    Thrift Classic server
    git-svn-id: https://gamescrafters.svn.sourceforge.net/svnroot/gamescrafters/GamesmanClassic/trunk@4876 a668be5d-ef11-cb4c-8bfd-e08cbb24f2ba

--- thrift/server.py
+++ thrift/server.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python
+import re
 from subprocess import Popen, PIPE
 from thrift.Thrift import TException
 from thrift.transport import TSocket
@@ -13,20 +14,33 @@ class RequestHandler(GamestateRequestHandler.Iface):
     def __init__(self, path):
         self.path = path
     
-    def getMoveValue(self, gameName, configuration):
-        results = self.get_move_value_base(gameName, configuration,
+    def getMoveValue(self, game, configuration):
+        results = self.get_move_value_base(game, configuration,
                                            '--GetMoveValue')
-        raise TException('not implemented')
+        move_value = self.parse_move_value(results)
+        if move_value is None:
+            raise TException('failed to get move value for %s %s' % \
+                                 (game, configuration))
+        return GetMoveResponse(status='ok', response=move_value)
     
-    def getNextMoveValues(self, gameName, configuration):
-        results = self.get_move_value_base(gameName, configuration,
+    def getNextMoveValues(self, game, configuration):
+        results = self.get_move_value_base(game, configuration,
                                            '--GetNextMoveValues')
-        raise TException('not implemented')
+        if results is None:
+            raise TException('failed to get move values for %s %s' % \
+                                 (game, configuration))
+        pattern = r'\([^,]+, [^,]+, [^,]+\)'
+        move_values = []
+        for matched in re.findall(pattern, results):
+            move_values.append(self.parse_move_value(matched.strip()))
+        return GetNextMoveResponse(status='ok', response=move_values)
     
     def get_move_value_base(self, game, configuration, flag):
         self.verify_name(game)
         board, player, variant, extra = self.parse_configuration(configuration)
-        arguments = [self.path + '/' + game, board, player, variant, extra]
+        arguments = [self.path + '/m' + game, flag, board, player, variant,
+                     extra]
+        print arguments
         process = Popen(arguments, stdout=PIPE, stderr=PIPE, cwd=self.path)
         out, err = process.communicate()
         if err:
@@ -34,9 +48,35 @@ class RequestHandler(GamestateRequestHandler.Iface):
         return out
     
     def parse_configuration(self, configuration):
-        print configuration
-        return None
+        fields = configuration.strip(';').split(';')
+        board, player, variant, extra = '', '1', '1', []
+        for field in fields:
+            prop, value = field.split('=', 1)
+            if prop == 'board':
+                board = value
+            elif prop == 'player':
+                player = str(int(value))
+            elif prop == 'variant':
+                variant = str(int(value))
+            else:
+                extra.append('--' + prop + '=' + value)
+        extra = ' '.join(extra)
+        return board, player, variant, extra
     
+    def parse_move_value(self, result):
+        match = re.match(r'\(([^,]+), ([^,]+), ([^,]+)\)', result)
+        if match is None:
+            return None
+        board, move, value = match.group(1, 2, 3)
+        value = self.decode_move(int(move))
+        return GamestateResponse(board=board, move=move, value=value)
+    
+    def decode_move(self, move):
+        values = ('undecided', 'win', 'lose', 'tie')
+        if 0 <= move < len(values):
+            return values[move]
+        return 'undecided'
+
     def verify_name(self, name):
         if re.match(r'^[a-zA-Z\d]+$', name) is None:
             raise TException(name + ' is not a valid game name')
@@ -54,4 +94,5 @@ if __name__ == '__main__':
     protocol_factory = TBinaryProtocol.TBinaryProtocolFactory()
     server = TServer.TThreadedServer(processor, transport, transport_factory,
                                      protocol_factory)
+    print 'GamesmanClassic server listening on port %d...' % (port,)
     server.serve()
