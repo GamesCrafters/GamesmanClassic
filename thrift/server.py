dec3eb519d3e8cad15fc336b1d5a403085ed47df
james_ide <james_ide@a668be5d-ef11-cb4c-8bfd-e08cbb24f2ba>
2010-05-01 13:52:13 +0000

    Thrift server for Classic is much more secure and errs on the side of caution
    git-svn-id: https://gamescrafters.svn.sourceforge.net/svnroot/gamescrafters/GamesmanClassic/trunk@4884 a668be5d-ef11-cb4c-8bfd-e08cbb24f2ba

--- thrift/server.py
+++ thrift/server.py
@@ -11,6 +11,17 @@ from gamesman.ttypes import *
 
 class RequestHandler(GamestateRequestHandler.Iface):
     
+    GAMES = ['1210', '1ton', '369mm', '3spot', 'Lgame', 'abalone', 'achi',
+             'ago', 'asalto', 'ataxx', 'baghchal', 'blocking', 'cambio',
+             'change', 'cmass', 'con', 'ctoi', 'dao', 'dinododgem', 'dnb',
+             'dodgem', 'fandan', 'foxes', 'gobblet', 'graph', 'hex', 'horse',
+             'hshogi', 'iceblocks', 'igo', 'joust', 'kono', 'lewth', 'lite3',
+             'loa', 'mancala', 'nim', 'nuttt', 'ooe', 'othello', 'pylos',
+             'qland', 'quarto', 'quickchess', 'qx', 'rInfin2', 'rcheckers',
+             'rubix', 'seega', 'sim', 'slideN', 'snake', 'squaredance', 'stt',
+             'swans', 'tactix', 'tilechess', 'tootnotto', 'tore', 'ttc', 'ttt',
+             'tttier', 'win4', 'winkers', 'wuzhi', 'xigua']
+    
     def __init__(self, path):
         self.path = path
     
@@ -29,18 +40,27 @@ class RequestHandler(GamestateRequestHandler.Iface):
         if results is None:
             raise TException('failed to get move values for %s %s' % \
                                  (game, configuration))
-        pattern = r'\([^,]+, [^,]+, [^,]+\)'
+        pattern = r'\([^,]+,\s+[^,]+,\s+[^,]+\)'
         move_values = []
         for matched in re.findall(pattern, results):
             move_values.append(self.parse_move_value(matched.strip()))
         return GetNextMoveResponse(status='ok', response=move_values)
     
     def get_move_value_base(self, game, configuration, flag):
-        self.verify_name(game)
-        board, player, variant, extra = self.parse_configuration(configuration)
-        arguments = [self.path + '/m' + game, flag, board, player, variant,
-                     extra]
-        print arguments
+        '''Securely invokes the specified game binary with the given board
+        configuration data.
+        This method defends against shell injections by using Popen, which
+        doesn't go through the shell. However, an attacker may still specify
+        a vulnerable binary; to prevent this, we verify the game name against
+        a whitelist of valid games. Finally, we only permit a limited set of
+        configuration parameters and strip out any non-alphanumeric or "normal"
+        characters. This is a defensive measure that mitigates exploits in
+        the actual game binaries.
+        '''
+        
+        self.verify_game(game)
+        board, player, variant = self.parse_configuration(configuration)
+        arguments = [self.path + '/m' + game, flag, board, player, variant]
         process = Popen(arguments, stdout=PIPE, stderr=PIPE, cwd=self.path)
         out, err = process.communicate()
         if err:
@@ -49,22 +69,20 @@ class RequestHandler(GamestateRequestHandler.Iface):
     
     def parse_configuration(self, configuration):
         fields = configuration.strip(';').split(';')
-        board, player, variant, extra = '', '1', '1', []
+        board, player, variant = '', '1','1'
         for field in fields:
             prop, value = field.split('=', 1)
             if prop == 'board':
                 board = value
             elif prop == 'player':
                 player = str(int(value))
-            elif prop == 'variant':
+            elif prop == 'option':
                 variant = str(int(value))
-            else:
-                extra.append('--' + prop + '=' + value)
-        extra = ' '.join(extra)
-        return board, player, variant, extra
+            # Ignore arguments outside of our whitelist.
+        return [self.sanitize_argument(a) for a in board, player, variant]
     
     def parse_move_value(self, result):
-        match = re.match(r'\(([^,]+), ([^,]+), ([^,]+)\)', result)
+        match = re.match(r'\(([^,]+),\s+([^,]+),\s+([^,]+)\)', result)
         if match is None:
             return None
         board, move, value = match.group(1, 2, 3)
@@ -76,13 +94,17 @@ class RequestHandler(GamestateRequestHandler.Iface):
         if 0 <= move < len(values):
             return values[move]
         return 'undecided'
-
-    def verify_name(self, name):
-        if re.match(r'^[a-zA-Z\d]+$', name) is None:
+    
+    def verify_game(self, game):
+        if game not in RequestHandler.GAMES:
             raise TException(name + ' is not a valid game name')
     
-    def verify_arguments(self, *args):
-        pass
+    def sanitize_argument(self, argument):
+        '''Removes all characters that aren't common typeable ASCII characters
+        from the given program argument.
+        '''
+        # The only visible ASCII characters are Space through Tilde.
+        return re.sub('[^ -~]', '', argument)
 
 if __name__ == '__main__':
     port = 24444
