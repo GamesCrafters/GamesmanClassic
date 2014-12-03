ed87afde1fb3fe950e8f10086db43b42928979cc
Yihe Li <liyihe96@gmail.com>
2014-12-03 11:49:56 -0800

    Add LosingStallingAi.py

--- src/py/ai/LosingStallingAi.py
+++ src/py/ai/LosingStallingAi.py
@@ -2,9 +2,9 @@ from ai import AiRequest
 class PerfectAi(AiRequest):
     def play(self, moves):
         sort_by_value_dict = {
-            'lose' : 1,
+            'lose' : -1,
             'tie' : 0,
-            'win' : -1,
+            'win' : 1,
         }
         moves = sorted(moves, key = lambda move: -move['remoteness'])
         moves = sorted(moves, key = lambda move: sort_by_value_dict[move['value']])
