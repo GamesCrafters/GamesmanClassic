32cabcb698c57b80a5b14137407a2cd0f6d1c67d
Max Fierro <maxfierro@berkeley.edu>
2023-03-16 02:26:05 -0700

    solving games since before you were born

--- doc/generated/html/navtreedata.js
+++ doc/generated/html/navtreedata.js
@@ -24,7 +24,7 @@
 */
 var NAVTREE =
 [
-  [ "Gamesman", "index.html", [
+  [ "GamesmanClassic", "index.html", [
     [ "Data Structures", "annotated.html", [
       [ "Data Structures", "annotated.html", "annotated_dup" ],
       [ "Data Structure Index", "classes.html", null ],
