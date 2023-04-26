a92bf7230c0708251371337d75327a4057989bbe
Zachary Leete <zacharyleete@berkeley.edu>
2023-04-26 01:42:33 -0700

    Added Canonical Positions

--- src/msantorini.c
+++ src/msantorini.c
@@ -176,13 +176,186 @@ VALUE Primitive(POSITION position) {
 
 /* Symmetry Handling: Return the canonical position. */
 POSITION GetCanonicalPosition(POSITION position) {
-  /* YOUR CODE HERE */
-  return position;
+  int unhashedPosition[height][width][2]=decodePosition(position);
+  int P1location[2];
+  FindLocation(unhashedPosition, 1, &P1location[0], &P1location[1]);
+  int P2location[2];
+  FindLocation(unhashedPosition, 2, &P2location[0], &P2location[1]);
+  int lowestCanonicalPosition[height][width][2];
+  for (int i = 0; i < height; i++) {
+    for (int j = 0; j < width; j++) {
+      lowestCanonicalPosition[i][j][0] = unhashedPosition[i][j][0];
+      lowestCanonicalPosition[i][j][1] = unhashedPosition[i][j][1];
+    }
+  }
+  int lowestCanonicalSymmetry = 0;
+  int minCanonicalValue = INT_MAX;
+
+  for (int i = 0; i <= 7; i++) {
+    int canonicalValue = 0;
+    int P1Symmetric[2], P2Symmetric[2];
+
+    // Get symmetric pawn positions
+    P1Symmetric[0]=P1location[0];
+    P1Symmetric[1]=P1location[1];
+    P2Symmetric[0]=P2location[0];
+    P2Symmetric[1]=P2location[1];
+    getSymmetricPawnPositions(P1Symmetric, P2Symmetric, height, width, i);
+
+    // Calculate canonical value using symmetric pawn positions
+    canonicalValue = ((P1Symmetric[0] * width + P1Symmetric[1]) * boardsize + (P2Symmetric[0] * width + P2Symmetric[1]));
+
+    // Update minimum canonical value
+    if (canonicalValue < minCanonicalValue) {
+      minCanonicalValue = canonicalValue;
+      lowestCanonicalSymmetry = i;
+    }
+  }
+
+  // Create symmetric copies based on lowestCanonicalSymmetry
+  if (lowestCanonicalSymmetry == 1) {
+    // Rotate board state by 90 degrees and create second symmetric copy
+    for (int i = 0; i < width; i++) {
+      for (int j = 0; j < height; j++) {
+        lowestCanonicalPosition[i][j][0] = unhashedPosition[height-1-j][i][0];
+        lowestCanonicalPosition[i][j][1] = unhashedPosition[height-1-j][i][1];
+      }
+    }
+  } else if (lowestCanonicalSymmetry == 2) {
+    // Rotate board state by 180 degrees and create third symmetric copy
+    for (int i = 0; i < height; i++) {
+      for (int j = 0; j < width; j++) {
+        lowestCanonicalPosition[i][j][0] = unhashedPosition[height-1-i][width-1-j][0];
+        lowestCanonicalPosition[i][j][1] = unhashedPosition[height-1-i][width-1-j][1];
+      }
+    }
+  } else if (lowestCanonicalSymmetry == 3) {
+    // Rotate board state by 270 degrees and create fourth symmetric copy
+    for (int i = 0; i < width; i++) {
+      for (int j = 0; j < height; j++) {
+        lowestCanonicalPosition[i][j][0] = unhashedPosition[j][width-1-i][0];
+        lowestCanonicalPosition[i][j][1] = unhashedPosition[j][width-1-i][1];
+      }
+    }
+  } else if (lowestCanonicalSymmetry == 4) {
+    // Flip board horizontally and create fifth symmetric copy
+    for (int i = 0; i < height; i++) {
+      for (int j = 0; j < width; j++) {
+        lowestCanonicalPosition[i][j][0] = unhashedPosition[i][width-1-j][0];
+        lowestCanonicalPosition[i][j][1] = unhashedPosition[i][width-1-j][1];
+      }
+    }
+  } else if (lowestCanonicalSymmetry == 5) {
+    // Flip board vertically and create sixth symmetric copy
+    for (int i = 0; i < height; i++) {
+      for (int j = 0; j < width; j++) {
+        lowestCanonicalPosition[i][j][0] = unhashedPosition[height-1-i][j][0];
+        lowestCanonicalPosition[i][j][1] = unhashedPosition[height-1-i][j][1];
+      }
+    }
+  } else if (lowestCanonicalSymmetry == 6) {
+    // Flip board diagonally and create seventh symmetric copy
+    for (int i = 0; i < height; i++) {
+      for (int j = 0; j < width; j++) {
+        lowestCanonicalPosition[i][j][0] = unhashedPosition[j][i][0];
+        lowestCanonicalPosition[i][j][1] = unhashedPosition[j][i][1];
+      }
+    }
+  } else if (lowestCanonicalSymmetry == 7) {
+    // Flip board anti-diagonally and create eighth symmetric copy
+    for (int i = 0; i < height; i++) {
+      for (int j = 0; j < width; j++) {
+        lowestCanonicalPosition[i][j][0] = unhashedPosition[height-1-j][width-1-i][0];
+        lowestCanonicalPosition[i][j][1] = unhashedPosition[height-1-j][width-1-i][1];
+      }
+    }
+  }
+
+  return lowestCanonicalPosition;
 }
 
+TIERLIST GetTierChildren(TIER tier) {
+  /* YOUR CODE HERE */
+  return NULL;
+}
 /*********** END SOLVING FUNCTIONS ***********/
 
 /*********** START HELPER FUNCTIONS ***********/
+
+void getSymmetricPawnPositions(int* pawn1, int* pawn2, int height, int width, int symmetry) {
+  int temp[2];
+
+  switch(symmetry) {
+    case 0:
+      // Original board state, no transformation needed
+      break;
+
+    case 1:
+      // 90 degrees clockwise
+      temp[0] = pawn1[0];
+      pawn1[0] = pawn1[1];
+      pawn1[1] = width - temp[0] - 1;
+
+      temp[0] = pawn2[0];
+      pawn2[0] = pawn2[1];
+      pawn2[1] = width - temp[0] - 1;
+      break;
+
+    case 2:
+      // 180 degrees clockwise
+      pawn1[0] = height - pawn1[0] - 1;
+      pawn1[1] = width - pawn1[1] - 1;
+
+      pawn2[0] = height - pawn2[0] - 1;
+      pawn2[1] = width - pawn2[1] - 1;
+      break;
+
+    case 3:
+      // 270 degrees clockwise
+      temp[0] = pawn1[0];
+      pawn1[0] = height - pawn1[1] - 1;
+      pawn1[1] = temp[0];
+
+      temp[0] = pawn2[0];
+      pawn2[0] = height - pawn2[1] - 1;
+      pawn2[1] = temp[0];
+      break;
+
+    case 4:
+      // Horizontal reflection
+      pawn1[1] = width - pawn1[1] - 1;
+      pawn2[1] = width - pawn2[1] - 1;
+      break;
+
+    case 5:
+      // Vertical reflection
+      pawn1[0] = height - pawn1[0] - 1;
+      pawn2[0] = height - pawn2[0] - 1;
+      break;
+
+    case 6:
+      // Diagonal reflection
+      temp[0] = pawn1[0];
+      pawn1[0] = pawn1[1];
+      pawn1[1] = temp[0];
+
+      temp[0] = pawn2[0];
+      pawn2[0] = pawn2[1];
+      pawn2[1] = temp[0];
+      break;
+
+    case 7:
+      // Anti-diagonal reflection
+      temp[0] = pawn1[0];
+      pawn1[0] = height - pawn1[1] - 1;
+      pawn1[1] = width - temp[0] - 1;
+
+      temp[0] = pawn2[0];
+      pawn2[0] = height - pawn2[1] - 1;
+      pawn2[1] = width - temp[0] - 1;
+      break;
+  }
+}
 
 void MoveDirections(int position[height][width][2], int availableDirections[8][2]) {
   int activePlayer = FindActivePlayer(position);
