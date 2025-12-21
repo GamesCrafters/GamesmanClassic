f43da97f0c97b5b9b578080bb9374a0f06b5d888
Bruce <sykim28@berkeley.edu>
2025-12-21 14:21:02 -0800

    game init

---
+++ src/mccheckers.c
@@ -0,0 +1,352 @@
+/************************************************************************
+**
+** NAME:        mccheckers.c
+**
+** DESCRIPTION: Chinese Checkers
+**
+** AUTHORS:     Seungyou(Bruce) Kim - University of California Berkeley
+**
+**
+** DATE:        12/21/2025
+**
+**************************************************************************/
+
+/*************************************************************************
+**
+** Everything below here must be in every game file
+**
+**************************************************************************/
+
+#include "gamesman.h"
+
+POSITION gNumberOfPositions  = -1;
+POSITION kBadPosition        = -1;
+
+POSITION gInitialPosition    =  0;
+POSITION gMinimalPosition    =  0;
+
+CONST_STRING kAuthorName         = "Seungyou(Bruce) Kim";
+CONST_STRING kGameName           = "Chinese Checkers";
+CONST_STRING kDBName = "CCheckers";
+BOOLEAN kPartizan           = TRUE;
+BOOLEAN kDebugMenu          = False;
+BOOLEAN kGameSpecificMenu   = FALSE;
+BOOLEAN kTieIsPossible      = False;
+BOOLEAN kLoopy               = FALSE;
+BOOLEAN kDebugDetermineValue = FALSE;
+void*    gGameSpecificTclInit = NULL;
+
+CONST_STRING kHelpGraphicInterface =
+        "Not written yet";
+
+CONST_STRING kHelpTextInterface    =
+        "On your turn, choose the origin and target to make your move you desire and hit return. 
+Describe the origin and target coordinates by the corresponding row (numeric)\n\
+and column (alphabet). If at any point you have made a mistake,\n\
+you can type u and hit return and the system will revert back to your most recent position.";
+
+CONST_STRING kHelpOnYourTurn =
+        "You move one of your pieces to an empty spot. You can jump over other pieces,\n\
+        your own or your opponent's, if the space across the jumped piece is empty.\n\ 
+        You may chain as many jumps as possible.";
+
+CONST_STRING kHelpStandardObjective =
+        "All your pieces reach the opposite starting position";
+
+CONST_STRING kHelpReverseObjective =
+        ""                                                                                                                                                             ;
+
+CONST_STRING kHelpTieOccursWhen =   /* Should follow 'A Tie occurs when... */
+                            "No tie in this game";
+
+CONST_STRING kHelpExample =
+        ""                   
+
+/*************************************************************************
+**
+** Everything above here must be in every game file
+**
+**************************************************************************/
+
+/*************************************************************************
+**
+** Every variable declared here is only used in this file (game-specific)
+**
+**************************************************************************/
+
+#define BOARDSIZE     25           
+#define BOARDROWS     5
+#define BOARDCOLS     5
+
+/************************************************************************
+**
+** NAME:        InitializeGame
+**
+** DESCRIPTION: Initialize the gDatabase, a global variable. and the other
+**              local variables.
+**
+************************************************************************/
+
+void InitializeGame() {}
+
+/************************************************************************
+**
+** NAME:        DebugMenu
+**
+** DESCRIPTION: Menu used to debub internal problems. Does nothing if
+**              kDebugMenu == FALSE
+**
+************************************************************************/
+
+void DebugMenu()
+{}
+
+/************************************************************************
+**
+** NAME:        GameSpecificMenu
+**
+** DESCRIPTION: Menu used to change game-specific parmeters, such as
+**              the side of the board in an nxn Nim board, etc. Does
+**              nothing if kGameSpecificMenu == FALSE
+**
+************************************************************************/
+
+void GameSpecificMenu() {}
+
+/************************************************************************
+**
+** NAME:        SetTclCGameSpecificOptions
+**
+** DESCRIPTION: Set the C game-specific options (called from Tcl)
+**              Ignore if you don't care about Tcl for now.
+**
+************************************************************************/
+
+void SetTclCGameSpecificOptions (int options[]) {
+	(void)options;
+}
+
+/************************************************************************
+**
+** NAME:        DoMove
+**
+** DESCRIPTION: Apply the move to the position.
+**
+** INPUTS:      POSITION thePosition : The old position
+**              MOVE     theMove     : The move to apply.
+**
+** OUTPUTS:     (POSITION) : The position that results after the move.
+**
+** CALLS:       Hash ()
+**              Unhash ()
+**	            LIST OTHER CALLS HERE
+*************************************************************************/
+POSITION DoMove (POSITION thePosition, MOVE theMove) {}
+
+/************************************************************************
+**
+** NAME:        GetInitialPosition
+**
+** DESCRIPTION: Ask the user for an initial position for testing. Store
+**              it in the space pointed to by initialPosition;
+**
+** OUTPUTS:     POSITION initialPosition : The position to fill.
+**
+************************************************************************/
+
+POSITION GetInitialPosition() {}
+
+/************************************************************************
+**
+** NAME:        PrintComputersMove
+**
+** DESCRIPTION: Nicely format the computers move.
+**
+** INPUTS:      MOVE    computersMove : The computer's move.
+**              STRING  computersName : The computer's name.
+**
+************************************************************************/
+
+void PrintComputersMove(MOVE computersMove, STRING computersName) {}
+
+/************************************************************************
+**
+** NAME:        Primitive
+**
+** DESCRIPTION: Return the value of a position if it fulfills certain
+**              'primitive' constraints. Some examples of this is having
+**              three-in-a-row with Gobblet. Three in a row for the player
+**              whose turn it is a win, otherwise its a loss.
+**              Otherwise undecided.
+**
+** INPUTS:      POSITION position : The position to inspect.
+**
+** OUTPUTS:     (VALUE) an enum which is oneof: (win,lose,tie,undecided)
+**
+** CALLS:       LIST FUNCTION CALLS
+**
+**
+************************************************************************/
+
+VALUE Primitive(POSITION pos) {}
+
+/************************************************************************
+**
+** NAME:        PrintPosition
+**
+** DESCRIPTION: Print the position in a pretty format, including the
+**              prediction of the game's outcome.
+**
+** INPUTS:      POSITION position   : The position to pretty print.
+**              STRING   playerName : The name of the player.
+**              BOOLEAN  usersTurn  : TRUE <==> it's a user's turn.
+**
+** CALLS:       Unhash()
+**              GetPrediction()
+**              LIST OTHER CALLS HERE
+**
+************************************************************************/
+
+void PrintPosition (POSITION position, STRING playerName, BOOLEAN usersTurn) {}
+
+/************************************************************************
+**
+** NAME:        GenerateMoves
+**
+** DESCRIPTION: Create a linked list of every move that can be reached
+**              from this position. Return a pointer to the head of the
+**              linked list.
+**
+** INPUTS:      POSITION position : The position to branch off of.
+**
+** OUTPUTS:     (MOVELIST *), a pointer that points to the first item
+**              in the linked list of moves that can be generated.
+**
+** CALLS:       GENERIC_PTR SafeMalloc(int)
+**              LIST OTHER CALLS HERE
+**
+************************************************************************/
+MOVELIST *GenerateMoves(POSITION position) {}
+
+/************************************************************************
+**
+** NAME:        GetAndPrintPlayersMove
+**
+** DESCRIPTION: This finds out if the player wanted an undo or abort or not.
+**              If so, return Undo or Abort and don't change theMove.
+**              Otherwise get the new theMove and fill the pointer up.
+**
+** INPUTS:      POSITION *thePosition : The position the user is at.
+**              MOVE *theMove         : The move to fill with user's move.
+**              STRING playerName     : The name of the player whose turn it is
+**
+** OUTPUTS:     USERINPUT             : Oneof( Undo, Abort, Continue )
+**
+** CALLS:       ValidMove(MOVE, POSITION)
+**              BOOLEAN PrintPossibleMoves(POSITION) ...Always True!
+**
+************************************************************************/
+
+USERINPUT GetAndPrintPlayersMove(POSITION thePosition, MOVE *theMove, STRING playerName) {}
+
+/************************************************************************
+**
+** NAME:        ValidTextInput
+**
+** DESCRIPTION: Return TRUE iff the string input is of the right 'form'.
+**              For example, if the user is allowed to select one slot
+**              from the numbers 1-9, and the user chooses 0, it's not
+**              valid, but anything from 1-9 IS, regardless if the slot
+**              is filled or not. Whether the slot is filled is left up
+**              to another routine.
+**
+** INPUTS:      STRING input : The string input the user typed.
+**
+** OUTPUTS:     BOOLEAN : TRUE if the input is a valid text input.
+**
+************************************************************************/
+
+BOOLEAN ValidTextInput(STRING input) {}
+
+/************************************************************************
+**
+** NAME:        ConvertTextInputToMove
+**
+** DESCRIPTION: Convert the string input to the internal move representation.
+**              No checking if the input is valid is needed as it has
+**              already been checked!
+**
+** INPUTS:      STRING input : The string input the user typed.
+**
+** OUTPUTS:     MOVE : The move corresponding to the user's input.
+**
+************************************************************************/
+
+MOVE ConvertTextInputToMove(STRING input) {}
+
+/************************************************************************
+**
+** NAME:        MoveToString
+**
+** DESCRIPTION: Returns the move as a STRING
+**
+** INPUTS:      MOVE *theMove         : The move to put into a string.
+**              char *moveStringBuffer : Buffer to write movestring to
+**
+************************************************************************/
+
+void MoveToString(MOVE theMove, char *moveStringBuffer) {}
+
+/************************************************************************
+**
+** NAME:        NumberOfOptions
+**
+** DESCRIPTION: Calculates and returns the number of option combinations
+**				there are with all the game variations you program.
+**
+** OUTPUTS:     int : the number of option combination there are.
+**
+************************************************************************/
+
+int NumberOfOptions() {}
+
+/************************************************************************
+**
+** NAME:        getOption
+**
+** DESCRIPTION: A hash function to keep track of all the game variants.
+**				Should return a different number for each set of
+**				variants.
+**
+** OUTPUTS:     int : the number representation of the options.
+**
+************************************************************************/
+
+int getOption() {}
+
+/************************************************************************
+**
+** NAME:        setOption
+**
+** DESCRIPTION: The corresponding unhash for the game variants.
+**				Should take the input and set all the appropriate
+**				variants.
+**
+** INPUT:     int : the number representation of the options.
+**
+************************************************************************/
+
+void setOption(int option) {}
+
+/************************************************************************
+*************************************************************************
+**         EVERYTHING BELOW THESE LINES IS LOCAL TO THIS FILE
+*************************************************************************
+************************************************************************/
+
+
+/************************************************************************
+** This is where you can put any helper functions, including your
+** hash and unhash functions if you are not using one of the existing
+** ones.
+************************************************************************/
