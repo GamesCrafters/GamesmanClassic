161a94b13fad7003797d2f39c33ff53049a260bd
Abraham Hsu <hpghsu@berkeley.edu>
2026-02-25 00:27:10 -0800

    Default hooking file added

---
+++ src/core/blob_db_hooks_default.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "gamesman.h"
+
+__attribute__((weak))
+void GetBlobFileNameFromPosition(POSITION p, char *filename) {
+    (void)p;
+    if (filename) filename[0] = '\0';
+}
+
+__attribute__((weak))
+UINT64 GetInfoFromBlobFile(POSITION p, FILE *f) {
+    (void)p; (void)f;
+    return 0;
+}
+
+__attribute__((weak))
+VALUE GetPrimitiveFromInfo(UINT64 info) {
+    (void)info;
+    return 0;
+}
+
+__attribute__((weak))
+REMOTENESS GetRemotenessFromInfo(UINT64 info) {
+    (void)info;
+    return 0;
+}
