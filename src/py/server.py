555cd5906581db23b83e21c04dc3eece257496fd
Abraham Hsu <hpghsu@berkeley.edu>
2026-05-13 18:47:43 -0700

    Revert changes

--- src/py/server.py
+++ src/py/server.py
@@ -431,9 +431,6 @@ class GameProcess():
     # Parsing a result that looks like
     # result =>> {result}
     def parse_response(self, response: str) -> str:
-        print("-----------------------------------")
-        print(response.split('=>>'))
-        print("-----------------------------------")
         result = response.split('=>>')[1].strip()
         self.server.log.debug(f'Split off "result =>>"')
         parsed = json.loads(result)
